f4fd6f62496ba4bd14101c1d7c8f6a75b7ed0019
matheus <matheuschomem@hotmail.com>
2024-01-12 08:45:20 -0300

    Changes made to test the new functionalities.

--- codes/main.py
+++ codes/main.py
@@ -1,12 +1,15 @@
 from libs.reports import DailyReport
+from libs.mailing import send_email
 from config.settings import Config
+import os
 
 def main():
-    config = Config("dev")
+	config = Config("dev")
 
-    print(config.paths.directoryPdf)
-    print(config.paths.fileMorningData)
-    print(config.today.weekNumber)
+	#print(config.paths.filePdf)
+	send_email(config, subject="Relatório Diário", email_body="Teste de Relatório")
+
+	#attachment_path=os.path.join(config.paths.directoryPdf, "2024-01-11.pdf")
 
 if __name__ == "__main__":
-    main()
+	main()
