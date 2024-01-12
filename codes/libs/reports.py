07377c9ba39902b86c27294c90cfb8d16101b0db
matheus <matheuschomem@hotmail.com>
2024-01-12 08:46:55 -0300

    New test report generated.

--- codes/libs/reports.py
+++ codes/libs/reports.py
@@ -1,26 +1,35 @@
 from abc import ABC, abstractmethod
 
 class Report(ABC):
-    
-    @abstractmethod
-    def canvas_config():
-        pass
+	
+	@abstractmethod
+	def canvas_config():
+		pass
 
-    @abstractmethod
-    def add_header():
-        pass
+	@abstractmethod
+	def add_header():
+		pass
 
-    @abstractmethod
-    def generate_report():
-        pass
+	@abstractmethod
+	def generate_report():
+		pass
 
 class DailyReport(Report):
-    
-    def __init__(file_path):
-        pass
+	
+	def __init__(file_path):
+		pass
+
+	def canvas_config():
+		pass
+		
+	def add_header():
+		pass
+
+	def generate_report():
+		pass
 
 class WeeklyReport(Report):
-    pass
+	pass
 
 class MonthlyReport(Report):
-    pass
+	pass
