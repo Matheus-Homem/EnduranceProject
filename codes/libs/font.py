8f8aa35a447eccfd563e9aaf9779b1570b83b1b9
matheus <matheuschomem@hotmail.com>
2024-01-14 18:47:59 -0300

    created some font functions

--- codes/libs/font.py
+++ codes/libs/font.py
@@ -1,19 +1,34 @@
-from reportlab.lib import fonts
+import json
+import os
 
-class Font:
-	def __init__(self, family='Helvetica', size=12, style='Normal'):
-		self.family = family
-		self.size = size
-		self.style = style
+def load_font(font_type):
+	# Get the path to the parent directory of the current file
+	parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+	
+	# Create a variable with the relative path to the font patterns file
+	FONT_PATTERNS_FILE = os.path.join(parent_dir, "files", "misc", "font_patterns.json")
+	
+	# Read font patterns from the JSON file
+	with open(FONT_PATTERNS_FILE, 'r') as f:
+		font_patterns = json.load(f)
+	
+	# Extract font and font size based on the provided font_type
+	font = font_patterns[font_type]["font"]
+	font_size = font_patterns[font_type]["font_size"]
+	
+	# Return the selected font and font size
+	return font, font_size
 
-	def set_family(self, family):
-		self.family = family
+def translate_weekday(english_weekday):
+	# Get the path to the parent directory of the current file
+	parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+	
+	# Create a variable with the relative path to the font patterns file
+	WEEKDAY_NAME_FILE = os.path.join(parent_dir, "files", "misc", "ptbr_weekday_name.json")
+	
+	# Read font patterns from the JSON file
+	with open(WEEKDAY_NAME_FILE, 'r') as f:
+		translate_ptbr = json.load(f)
 
-	def set_size(self, size):
-		self.size = size
-
-	def set_style(self, style):
-		self.style = style
-
-	def get_reportlab_font(self):
-		return fonts.Font(self.family, self.size, self.style)
+	# Return the translated version of the weekday to PT-BR
+	return translate_ptbr[english_weekday]
