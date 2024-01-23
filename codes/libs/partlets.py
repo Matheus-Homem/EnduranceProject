fc9b05d259877bf151b7e1bb2552e831e3982cad
matheus <matheuschomem@hotmail.com>
2024-01-23 08:46:48 -0300

    Parlet class development using abc lib

--- codes/libs/partlets.py
+++ codes/libs/partlets.py
@@ -1,11 +1,25 @@
-import libs.write_patterns as wrt
+from abc import ABC, abstractmethod
 from libs.font import translate_weekday
+import libs.write_patterns as wrt
+
 
 class Partlet():
 	def __init__(self):
 		pass
 
-class Header():
+	@abstractmethod
+	def daily_generate(self):
+		pass
+	
+	@abstractmethod
+	def weekly_generate(self):
+		pass
+	
+	@abstractmethod
+	def monthly_generate(self):
+		pass
+
+class Header(Partlet):
 	def __init__(self, report, initial_height):
 		# Initialize Header with reference to the main report, configuration, canvas, and initial height
 		self.config = report.config  # Configuration object from the main report
@@ -29,10 +43,8 @@ class Header():
 		# Draw a line under the subtitle
 		self.c.line(100, self.up_start - 25, 500, self.up_start - 25)	
 
-class Goals():
-	def __init__(self, configuration, goal_date):
-		self.config = configuration
-		self.date = goal_date
+class Goals(Partlet):
+	def __init__(self):
 		pass
 
 class SleepDiary():
@@ -40,61 +52,82 @@ class SleepDiary():
 		pass
 
 class WeightDiary():
+	def __init__(self, report, initial_height):
+		# Initialize Header with reference to the main report, configuration, canvas, and initial height
+		self.config = report.config  # Configuration object from the main report
+		self.c = report.c  # Canvas object from the main report
+		self.up_start = initial_height  # Initial height for positioning elements
+
+	def daily_generate(self):
+		# Generate the header partlet with centralized text and a line
+		# Title
+		wrt.centralized_text(self.c,
+							 self.up_start,
+							 "Relatório Diário",
+							 "TITLE")
+
+		# Subtitle with date, weekday, and week number
+		wrt.centralized_text(self.c,
+							 self.up_start - 20,
+							 f"{self.config.today.date_fmtd} | {translate_weekday(self.config.today.week_day)} | Semana: {self.config.today.week_number}",
+							 "SUBTITLE")
+
+		# Draw a line under the subtitle
+		self.c.line(100, self.up_start - 25, 500, self.up_start - 25)
+		pass
+
+class ReadingDiary(Partlet):
 	def __init__(self):
 		pass
 
-class ReadingDiary():
+class LearningDiary(Partlet):
 	def __init__(self):
 		pass
 
-class LearningDiary():
+class PracticeDiary(Partlet):
 	def __init__(self):
 		pass
 
-class PracticeDiary():
+class WorkDiary(Partlet):
 	def __init__(self):
 		pass
 
-class WorkDiary():
+class StudyDiary(Partlet):
 	def __init__(self):
 		pass
 
-class StudyDiary():
+class SymptomDiary(Partlet):
 	def __init__(self):
 		pass
 
-class SymptomDiary():
+class UpperBodyDiary(Partlet):
 	def __init__(self):
 		pass
 
-class UpperBodyDiary():
+class LowerBodyDiary(Partlet):
 	def __init__(self):
 		pass
 
-class LowerBodyDiary():
+class NutritionDiary(Partlet):
 	def __init__(self):
 		pass
 
-class NutritionDiary():
+class SelfcareDiary(Partlet):
 	def __init__(self):
 		pass
 
-class SelfcareDiary():
+class VolunteeringDiary(Partlet):
 	def __init__(self):
 		pass
 
-class VolunteeringDiary():
+class RelaxationDiary(Partlet):
 	def __init__(self):
 		pass
 
-class RelaxationDiary():
+class ExcellenceDiary(Partlet):
 	def __init__(self):
 		pass
 
-class ExcellenceDiary():
-	def __init__(self):
-		pass
-
-class MeditationDiary():
+class MeditationDiary(Partlet):
 	def __init__(self):
 		pass
