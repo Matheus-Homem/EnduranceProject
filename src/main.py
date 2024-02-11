31a34e878f3b50c973b093e0392da07b265642fb
matheus <matheuschomem@hotmail.com>
2024-02-11 20:50:41 -0300

    change main.py directory to src

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
-from codes.libs.reports import Report
-from etl.orchestrator import execute_orchestration
+from etl.orchestrator import Orchestrator
+from libs.reports import Report
 
-import time
 import argparse
 
 def main():
@@ -10,24 +9,20 @@ def main():
 	parser = argparse.ArgumentParser(description="Script that executes main.py")
 
 	# Add arguments to the parser
-	parser.add_argument('--automate', type=bool, default=False, help='Argument identifying script automation')
+	parser.add_argument('--automated', type=bool, default=False, help='Argument identifying script automation')
 	parser.add_argument('--date', default=None, help='Argument identifying script date in the format "YYYYMMDD"')
 
 	# Parse command line arguments
 	args = parser.parse_args()
 
 	# Acesse os argumentos
-	automate_execution = args.automate
+	automated = args.automated
 	script_date = args.date
 
-	while Report()._config_instance.date_validation(automate_execution) :
-		print("Databases not updated. Testing again in 1 minute.")
-		time.sleep(60) 
-
-	execute_orchestration()
+	Orchestrator(date_param=script_date).run_pipeline(automated=automated)
 
 	# Everyday generate Daily Report
-	Report().daily_publish(date_str=script_date)
+	Report(date_param=script_date).daily_publish()
 
 if __name__ == "__main__":
 	main()
