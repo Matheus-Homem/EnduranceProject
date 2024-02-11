b80ee945f82b29dbe77d86c08cf7219b84acb0bf
matheus <matheuschomem@hotmail.com>
2024-02-11 20:49:47 -0300

    created header.py for Header partlet class

---
+++ src/libs/topics/header.py
@@ -0,0 +1,26 @@
+from .partlet import Partlet
+
+class Header(Partlet):
+	def __init__(self):
+		super().__init__()
+
+		self.set_order()
+
+	def exe_title(self):
+		# Title
+		title = lambda: self.draw.centered_text(text="Relatório Diário", pattern="TITLE")
+		self._elements.append(title)
+
+	def exe_subtitle(self):
+		# Subtitle with date, weekday, and week number
+		subtitle_text = f"{self.config.dt.dt_fmtd} | {self.draw.translate_weekday(self.config.dt.week_day)} | Semana: {self.config.dt.week_number}"
+		subtitle = lambda: self.draw.centered_text(text=subtitle_text, pattern="SUBTITLE")
+		self._elements.append(subtitle)
+		
+	def set_order(self):
+		self.exe_title() # Create a Title for the Header Partlet
+		self.exe_subtraction(20) # Subtract 20 from initial height
+		self.exe_subtitle() # Create a Subitle for the Header Partlet
+		self.exe_subtraction(10) # Subtract 10 from initial height
+		self.exe_line()  # Create a Horizontal Line for the Header Partlet
+		self.exe_subtraction(40) # Subtract 40 from initial height
