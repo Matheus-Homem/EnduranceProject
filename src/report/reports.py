af0154d038196e453e2975b56319cbff16322e65
matheus <matheuschomem@hotmail.com>
2024-02-14 12:24:59 -0300

    Calendar() turned to a attribute of Globals

--- src/report/reports.py
+++ src/report/reports.py
@@ -1,45 +1,38 @@
 import src.report.partlets as ptt
-from src.env.helpers import Calendar
-from src.report.singletons import CanvasSingleton
+from src.env.globals import Global
 from src.report.email.manager import EmailManager
 
 from typing import List
 
 class Report:
 
-	def __init__(self, exec_date):
+	def __init__(self):
 		
 		# Create a sections attribute to list all Partlets inside Report
 		self._sections: List[ptt.Partlet] = []
 
-		self.calendar = Calendar(exec_date)
+		# Obter instâncias únicas de Calendar e Canvas
+		self.canvas = Global().get_canvas()
 
-		self.file_path = self.calendar.get_partitioned_file_path(fmt="pdf")
-
-		self.canvas = CanvasSingleton(filename=self.file_path).get_canvas()
-
-	def add_partlet(self, partlet: ptt.Partlet):
+	def add_partlet(self, partlet):
 		self._sections.append(partlet)
-
-	def skip_page(self):
-		pass
 
 	def save_file(self):
 		for partlet in self._sections:
-			partlet.daily()
+			partlet.exe_daily()
 		self.canvas.save()
+		print("PDF was successfully saved.")
 
-	def send(self):
+	def send_file(self):
 		email_manager = EmailManager()
 		email_manager.set_frequency("0 5 * * *")
-		email_manager.set_calendar(self.calendar)
 		email_manager.dispatch()
 
 	def daily_publish(self, send_email:bool=False):
 
 		active_partlets = [
 			ptt.Header(), 
-			ptt.WeightDiary()
+			ptt.WeightPartlet()
 		]
 		[self.add_partlet(partlets) for partlets in active_partlets]
 
@@ -49,3 +42,5 @@ class Report:
 		if send_email:
 			## Report Sending
 			self.send_file()
+
+			
