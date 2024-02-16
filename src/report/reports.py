5bc24ef9805c6cd57de95eb05dd37268e741c6af
matheus <matheuschomem@hotmail.com>
2024-02-16 08:27:45 -0300

    improved Report readbility

--- src/report/reports.py
+++ src/report/reports.py
@@ -2,45 +2,29 @@ import src.report.partlets as ptt
 from src.env.globals import Global
 from src.report.email.manager import EmailManager
 
-from typing import List
-
 class Report:
 
 	def __init__(self):
-		
-		# Create a sections attribute to list all Partlets inside Report
-		self._sections: List[ptt.Partlet] = []
 
-		# Obter instâncias únicas de Calendar e Canvas
 		self.canvas = Global().get_canvas()
 
-	def add_partlet(self, partlet):
-		self._sections.append(partlet)
-
-	def save_file(self):
-		for partlet in self._sections:
-			partlet.exe_daily()
+	def _save_file(self):
 		self.canvas.save()
 		print("PDF was successfully saved.")
 
-	def send_file(self):
+	def _send_file(self):
 		email_manager = EmailManager()
 		email_manager.set_frequency("0 5 * * *")
 		email_manager.dispatch()
 
 	def daily_publish(self, send_email:bool=False):
-
-		active_partlets = [
-			ptt.Header(), 
-			ptt.WeightPartlet()
-		]
-		[self.add_partlet(partlets) for partlets in active_partlets]
+		ptt.Header()
+		ptt.WeightPartlet()
+		ptt.Partlet.exe_daily()
 
 		## Report Saving
-		self.save_file()
+		self._save_file()
 
 		if send_email:
 			## Report Sending
-			self.send_file()
-
-			
+			self._send_file()
