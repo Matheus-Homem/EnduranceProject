8b6b3b187cbfdca6f232a8001c28aa339630201e
matheus <matheuschomem@hotmail.com>
2024-02-16 08:26:53 -0300

    order_elements method does not exist anymore

--- src/report/partlets/weight.py
+++ src/report/partlets/weight.py
@@ -8,20 +8,17 @@ class WeightPartlet(Partlet):
 	def __init__(self):
 		super().__init__()
 
-		self.order_elements(section_name="WEIGHT")
 		exec()
+		#self.add_plot()
+		Partlet.add_name_section(section_name="WEIGHT")
+		Partlet.add_chapter_header("Pesagem")
+		Partlet.sub_height(20)
+		Partlet.add_line()
+		Partlet.sub_height(20)	
 
 	def add_plot(self):
 		# Add a png plot to the PDF
 		plot1_path = self.calendar.get_partitioned_file_path(prefix="WM", fmt="png")
 		weight_plot = lambda: self.canvas.drawImage(plot1_path, inch, inch/4, width=6*inch, height=9.5*inch)
 		weight_comment = "Add multiple Weight plots"
-		self.add_custom_element(comment=weight_comment, function=weight_plot)
-
-	def order_elements(self, section_name: str):
-		super().order_elements(section_name)
-		self.add_chapter_header("Pesagem")
-		self.sub_height(20)
-		self.add_line()
-		self.sub_height(20)
-		self.add_plot()
+		Partlet.add_custom_element(comment=weight_comment, function=weight_plot)
