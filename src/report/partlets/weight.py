d77fe6e9500459e18df6a1bb327e889dc16a0236
matheus <matheuschomem@hotmail.com>
2024-02-17 15:53:35 -0300

    updated to working version

--- src/report/partlets/weight.py
+++ src/report/partlets/weight.py
@@ -9,16 +9,18 @@ class WeightPartlet(Partlet):
 		super().__init__()
 
 		exec()
-		#self.add_plot()
+		
 		Partlet.add_name_section(section_name="WEIGHT")
 		Partlet.add_chapter_header("Pesagem")
 		Partlet.sub_height(20)
 		Partlet.add_line()
 		Partlet.sub_height(20)	
 
+		self.add_plot()
+
 	def add_plot(self):
 		# Add a png plot to the PDF
 		plot1_path = self.calendar.get_partitioned_file_path(prefix="WM", fmt="png")
 		weight_plot = lambda: self.canvas.drawImage(plot1_path, inch, inch/4, width=6*inch, height=9.5*inch)
 		weight_comment = "Add multiple Weight plots"
-		Partlet.add_custom_element(comment=weight_comment, function=weight_plot)
+		Partlet.add_custom_function(function_description=weight_comment, custom_function=weight_plot)
