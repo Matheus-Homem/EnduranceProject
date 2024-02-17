e3518cf0862e2b333535f466c9a8d88abc3d63c4
matheus <matheuschomem@hotmail.com>
2024-02-17 15:53:11 -0300

    improved version to add doc details

--- src/report/partlets/partlet.py
+++ src/report/partlets/partlet.py
@@ -1,6 +1,7 @@
 from src.env.globals import Global
 from src.report.partlets.typing.scribe import PDFArtist
 
+import uuid
 from abc import ABC
 from typing import Callable
 
@@ -13,6 +14,7 @@ class Partlet(ABC):
 	_height: int = 800
 
 	draw = PDFArtist()
+	
 
 	def __init__(self):
 
@@ -25,94 +27,163 @@ class Partlet(ABC):
 		self.exec_date = self.global_instance.get_date()
 
 	@classmethod
-	def add_name_section(cls, section_name: str):
+	def add_to_elements(
+		cls, 
+		description: str, 
+		function: Callable
+		):
+		"""
+		Adds an element to the _elements dictionary.
+
+		Args:
+			description (str): The description of the element.
+			function (Callable): The lambda function to be added.
+		"""
+		unique_key = uuid.uuid4()
+		cls._elements[f"{description}__{unique_key}"] = function
+
+	@classmethod
+	def add_name_section(
+		cls, 
+		section_name: str
+		):
 		"""
 		Abstract method to add a name to the partlet.
 
 		Args:
 			section_name (str): The name of the section.
 		"""
-		null_lambda = lambda: None
-		cls._elements[f"Starting the execution of section: {section_name}"] = null_lambda
+		cls.add_to_elements(
+			description = f"Starting the execution of section: {section_name}", 
+			function = lambda: None
+		)
 
 	@classmethod
-	def add_custom_element(cls, comment: str, function: Callable):
+	def add_custom_function(
+		cls, 
+		function_description: str, 
+		custom_function: Callable
+		):
 		"""
 		Adds a custom function to the partlet.
 
 		Args:
 			comment (str): A comment describing the function.
-			function (Callable): The function to add.
+			custom_function (Callable): The function to add.
 		"""
-		cls._elements[comment] = function
+		cls.add_to_elements(
+			description = function_description, 
+			function = custom_function
+		)
 
 	@classmethod
-	def add_chapter_header(cls, text: str):
+	def add_chapter_header(
+		cls, 
+		chapter_name: str
+		):
 		"""
 		Adds a chapter header to the partlet.
 
 		Args:
-			text (str): The text of the chapter header.
+			chapter_name (str): The text of the chapter header.
 		"""
-		chapter_header = lambda: cls.draw.centered_text(text=text, pattern="CHAPTER_HEADER", height=cls._height)
-		cls._elements[f"Add a Header for the Chapter '{text}'"] = chapter_header
+		cls.add_to_elements(
+			description = f"Add a Header for the Chapter '{chapter_name}'", 
+			function = lambda: cls.draw.centered_text(text=chapter_name, 
+											 		  pattern="CHAPTER_HEADER", 
+													  height=cls._height
+													  )
+		)
 
 	@classmethod
-	def add_title(cls, text: str):
+	def add_title(
+		cls, 
+		title_text: str
+		):
 		"""
 		Adds a title to the partlet.
 
 		Args:
-			text (str): The text of the title.
+			title_text (str): The text of the title.
 		"""
-		title = lambda: cls.draw.centered_text(text=text, pattern="TITLE", height=cls._height)
-		cls._elements[f"Add a title"] = title
+		cls.add_to_elements(
+			description = "Add a title", 
+			function = lambda: cls.draw.centered_text(text=title_text, 
+													  pattern="TITLE", 
+													  height=cls._height
+													  )
+		)
 
 	@classmethod
-	def add_subtitle(cls, text: str):
+	def add_subtitle(
+		cls, 
+		subtitle_text: str
+		):
 		"""
 		Adds a subtitle to the partlet.
 
 		Args:
-			text (str): The text of the subtitle.
+			subtitle_text (str): The text of the subtitle.
 		"""
-		sub_title = lambda: cls.draw.centered_text(text=text, pattern="SUBTITLE", height=cls._height)
-		cls._elements[f"Add a formal subtitle"] = sub_title
+		cls.add_to_elements(
+			description = "Add a formal subtitle", 
+			function = lambda: cls.draw.centered_text(text=subtitle_text, 
+													  pattern="SUBTITLE", 
+													  height=cls._height
+													  )
+		)
 
 	@classmethod
-	def add_line(cls):
+	def add_line(
+		cls
+		):
 		"""
-		Adds a horizontal line to the partlet.
+		Add a horizontal line to the partlet.
 		"""
-		h_line = lambda: cls.draw.horizontal_line(cls._height)
-		cls._elements["Add a Horizontal Line"] = h_line
+		cls.add_to_elements(
+			description = "Add a Horizontal Line", 
+			function = lambda: cls.draw.horizontal_line(cls._height)
+		)
 
 	@classmethod
-	def sub_height(cls, value: int):
+	def sub_height(
+		cls, 
+		value: int
+		):
 		"""
 		Subtracts a specified value from the height instance variable.
 		
 		Args:
 			value (int): The value to subtract from the height.
 		"""
-		height_deduction = lambda: setattr(cls, '_height', cls._height - value)
-		cls._elements[f"Reducing {value} from Height"] = height_deduction
+		cls.add_to_elements(
+			description = f"Reducing {value} from Height",
+			function = lambda: setattr(cls, '_height', cls._height - value)
+		)
 
 	@classmethod
-	def add_page(cls):
+	def add_page(
+		cls
+		):
 		"""
 		Adds a new page to the report and reset height.
 		"""
-		skipper = lambda: cls.draw.blank_page()
-		reset = lambda: setattr(cls, "_height", 800)
-		cls._elements[f"Add a new page"] = skipper, reset
+		cls.add_to_elements(
+			description = f"Skipping page",
+			function = lambda: cls.draw.blank_page()
+		)
+		cls.add_to_elements(
+			description = f"Reseting Height",
+			function = lambda: setattr(cls, "_height", 800)
+		)
 
 	@classmethod
-	def exe_daily(cls):
+	def exe_daily(
+		cls
+		):
 		"""
 		Executes the functions within the partlet for daily reporting.
 		"""
-		#print(cls._elements.keys())
 		for key, function in cls._elements.items():
-			print(f"Executing a function that: {key} | Height: {cls._height}")
+			print(f"Height: {cls._height} || Executing : {key.split('__')[0]}")
 			function()
