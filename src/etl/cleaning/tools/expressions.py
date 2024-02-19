eeff0b94048069dde8263bd4364cf7a5fc17c5b1
matheus <matheuschomem@hotmail.com>
2024-02-18 21:14:33 -0300

    improved class methods

--- src/etl/cleaning/tools/expressions.py
+++ src/etl/cleaning/tools/expressions.py
@@ -4,38 +4,31 @@ class Expressions:
 
 	def __init__(
 		self, 
-		table_id:str, 
-		dtype_dict:dict, 
-		data_schema:dict, 
-		df:pl.DataFrame
+		data_schema:dict,
 		):
 	
-		self.table_id = table_id 
-		self.dtype_dict = dtype_dict 
 		self.data_schema = data_schema
-		self.df = df
 
-		self._generate_rename_expressions()
-		self._generate_dtype_expressions()
+		# Dict to translate dtype in STRING to dtype in POLARS
+		self.dtype_dict = {
+			"list": pl.List(pl.Utf8),
+			"string": pl.Utf8,
+			"date": pl.Date,
+			"float": pl.Float64,
+			"int": pl.Int64,
+			"timestamp": pl.Datetime,
+			"timedelta": pl.Duration
+		}
 
-	def _generate_rename_expressions(self):
-		if self.table_id == "morning_v2":
-			self.rename_expression = [
-				pl.col(column_id).alias(column_config["name"])
-				for column_id, column_config in self.data_schema[self.table_id].items()
-				]
-		elif self.table_id == "morning_v3":
-			full_name_dict = {coluna[:3]: coluna for coluna in self.df.columns}
-			self.rename_expression = [
-				pl.col(full_name_dict.get(column_id[:3])).alias(column_config["name"])
-				for column_id, column_config in self.data_schema[self.table_id].items()
-				]
+	def generate_rename_expressions(self, table_id, columns_list):
+		if table_id == "morning_v2":
+			for column_id, column_config in self.data_schema[table_id].items():
+				return pl.col(column_id).alias(column_config["name"])
+		elif table_id == "morning_v3":
+			full_name_dict = {coluna[:3]+coluna[-4:]: coluna for coluna in columns_list}
+			for column_id, column_config in self.data_schema[table_id].items():
+				return pl.col(full_name_dict.get(column_id[:3])).alias(column_config["name"])
 
-	def _generate_dtype_expressions(self):
-		self.dtype_expression = [
-			pl.col(column_config["name"]).cast(self.dtype_dict[column_config["dtype"]])
-			for column_id, column_config in self.data_schema[self.table_id].items()
-			]
-
-	def get_expressions(self):
-		return self.rename_expression, self.dtype_expression
+	def generate_dtype_expressions(self, table_id):
+		for column_id, column_config in self.data_schema[table_id].items():
+			return pl.col(column_config["name"]).cast(self.dtype_dict[column_config["dtype"]])
