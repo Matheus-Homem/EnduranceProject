1543c47f9b2bc6a88b04de3dce3c590886ea12f2
matheus <matheuschomem@hotmail.com>
2024-02-23 19:55:07 -0300

    renamed class Orchestrator and change folder

---
+++ src/etl/process/orchestrator.py
@@ -0,0 +1,38 @@
+from src.etl.patterns import Orchestrator
+from src.etl.cleaning.orchestrator import CleanerOrchestrator
+from src.etl.refining.orchestrator import RefinerOrchestrator
+
+import time
+
+class ProcessOrchestrator(Orchestrator):
+
+	def execute_etl(self):
+		self.logger.info("")
+		self.logger.info("ETL Process Started")
+		self.logger.info("Performing data cleaning from ingestion layer to cleaned layer")
+		CleanerOrchestrator().execute()
+		self.logger.info("Performing data refining from cleaned layer to refining layer")
+		RefinerOrchestrator().execute()
+		self.logger.info("")
+		self.logger.info("ETL Process Finished.")
+		self.logger.info("")
+
+	def _block_pipeline(self):
+		if self.validate_last_date("morning_routine_v2.xlsx") and self.validate_last_date("night_routine_v2.xlsx"):
+			self.logger.info("Pipeline Unblocked")
+			return False
+		else:
+			self.logger.info("Pipeline Blocked")
+			return True 
+
+	def execute(self, automated: bool = False):
+		if automated:
+			self.logger.info("Automation detected. Initializing validation.")
+			while self._block_pipeline():
+				self.logger.info("Databases not updated. Retesting in 1 minute.")
+				time.sleep(60)
+			self.logger.info("Validation finished.")
+			self.execute_etl()
+		else:
+			self.logger.info("Manual process detected. Skipping validation.")
+			self.execute_etl()
