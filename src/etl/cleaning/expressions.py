06fcaaac45d3255e7f3b1b6ce5fbdcac2f47152f
matheus <matheuschomem@hotmail.com>
2024-02-24 15:48:20 -0300

    expressions now is a part of CleanerEngine

--- src/etl/cleaning/expressions.py
+++ src/etl/cleaning/expressions.py
@@ -1,15 +1,12 @@
+from src.etl.cleaning.formater import FormatConverter
+
 import polars as pl
 
-class Expressions:
+class ExpressionsGenerator:
 
-	def __init__(
-		self, 
-		data_schema:dict,
-		):
-	
+	def __init__(self, data_schema):
 		self.data_schema = data_schema
 
-		# Dict to translate dtype in STRING to dtype in POLARS
 		self.dtype_dict = {
 			"list": pl.List(pl.Utf8),
 			"string": pl.Utf8,
@@ -21,21 +18,47 @@ class Expressions:
 			"bool": pl.Boolean
 		}
 
-	def generate_rename_expressions(self, table_id, columns_list):
-		if table_id == "morning_v2":
-			return [
-				pl.col(column_id).alias(column_config["name"])
-				for column_id, column_config in self.data_schema[table_id].items()
-			]
-		else:
-			column_dict = {index: column for index, column in enumerate(columns_list)}
-			return [
-				pl.col(column_dict[int(column_id)]).alias(column_config["name"])
-				for column_id, column_config in self.data_schema[table_id].items()
-			]
-			
-	def generate_dtype_expressions(self, table_id):
+		self.conversion_functions = {
+			"date": FormatConverter._to_date,
+			"timedelta": FormatConverter._to_timedelta,
+			"timestamp": FormatConverter._to_timedelta,
+			"bool": FormatConverter._to_boolean
+		}
+
+	def rename_expressions(self, identifier, columns_list):
+		column_dict = {index: column for index, column in enumerate(columns_list)}
 		return [
-			pl.col(column_config["name"]).cast(self.dtype_dict[column_config["dtype"]])
-			for column_id, column_config in self.data_schema[table_id].items()
+			pl.col(column_dict[int(column_id)]).alias(column_config["name"])
+			for column_id, column_config in self.data_schema[identifier].items()
 		]
+
+	def init_dtype_expressions(self, identifier):
+		dtype_expressions = []
+
+		for column_id, column_config in self.data_schema[identifier].items():
+			conversion_function = self.conversion_functions.get(column_config["dtype"])
+			if conversion_function:
+				expression = (
+					pl.col(column_config["name"])
+					.map_elements(conversion_function)
+					.cast(self.dtype_dict[column_config["dtype"]])
+					.alias(column_config["name"])
+				)
+				dtype_expressions.append(expression)
+
+		return dtype_expressions
+	
+	def inal_dtype_expressions(self, identifier):
+		dtype_expressions = []
+		
+		for column_id, column_config in self.data_schema[identifier].items():
+			if column_config["dtype"] == "timestamp":
+				expression = FormatConverter._to_timestamp(column_config=column_config)
+			else:
+				expression = pl.col(column_config["name"])
+			
+			dtype_expressions.append(
+				expression.cast(self.dtype_dict[column_config["dtype"]])
+						  .alias(column_config["name"]))
+		
+		return dtype_expressions
