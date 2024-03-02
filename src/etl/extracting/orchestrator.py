935248a1ced74b0dcb67c2eb95c907463e746ea6
matheus <matheuschomem@hotmail.com>
2024-03-02 18:34:15 -0300

    created a Orchestrator for extraction

---
+++ src/etl/extracting/orchestrator.py
@@ -0,0 +1,21 @@
+from src.etl.patterns.orchestrator import Orchestrator
+from src.etl.extracting.engine import ExtractorEngine
+
+
+
+class ExtractorOrchestrator(Orchestrator):
+	
+	def __init__(self):
+		super().__init__()
+
+		self.process = "EXTRACTING"
+
+		self.engine = ExtractorEngine()
+
+	def execute(self):
+		self.logger = Orchestrator.logger
+		self.logger.info("*********************************************************")
+		self.logger.info(f"///////////// STARTING EXTRACTING PROCESS //////////////")
+		self.logger.info("*********************************************************")
+		self.engine.execute()
+
