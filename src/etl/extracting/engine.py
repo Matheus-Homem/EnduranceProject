4b925b95b36d3a56ffab7d9d243396ea310caa98
matheus <matheuschomem@hotmail.com>
2024-03-02 18:34:03 -0300

    created a Engine for extraction

---
+++ src/etl/extracting/engine.py
@@ -0,0 +1,59 @@
+
+from src.etl.patterns.engine import Engine
+from src.etl.extracting.connection import IMAPConnection
+
+import email
+import json
+import hashlib
+
+
+class ExtractorEngine(Engine):
+
+	def __init__(self):
+		super().__init__()
+
+		self.connection = IMAPConnection()
+		self.mail = self.connection.get_mail()
+		self.data = self.connection.get_data()
+
+	def _generate_subject(self, email_subject):
+		form_type, date_id, timestamp = email_subject.split("_")
+		form_id = form_type.split("-")[0]
+		hash_obj = hashlib.sha256(timestamp.encode('utf-8'))
+		hash_hex = hash_obj.hexdigest()
+		unique_id = hash_hex[:8]
+		return f"{form_id}_{date_id}({unique_id})"
+
+	def _fetch_raw_email(self, num):
+		result, email_data = self.mail.fetch(num, '(RFC822)')
+		return email_data[0][1]
+
+	def _parse_email(self, raw_email):
+		return email.message_from_bytes(raw_email)
+
+	def _parse_email_body(self, msg):
+		body = msg.get_payload(decode=True).decode().replace("\r", "")
+		ask_dict = {line.split("| ", 1)[0].strip(): line.split("| ", 1)[1].strip() 
+					for line in body.split("\n")[:-1]}
+		return ask_dict
+
+	def _get_json_path(self, msg):
+		subject = self._generate_subject(msg['Subject'])
+		return self.paths.get_file_path("ingestion", f"{subject}.json")
+
+	def _write_json_file(self, ask_dict, json_path):
+		with open(json_path, "w", encoding="utf-8") as arquivo:
+			json.dump(ask_dict, arquivo, indent=4, ensure_ascii=False)
+	
+	def _logout(self):
+		self.mail.close()
+		self.mail.logout()
+
+	def execute(self):
+		for num in self.data[0].split():
+			raw_email = self._fetch_raw_email(num)
+			msg = self._parse_email(raw_email)
+			ask_dict = self._parse_email_body(msg)
+			json_path = self._get_json_path(msg)
+			self._write_json_file(ask_dict, json_path)
+		self._logout()
