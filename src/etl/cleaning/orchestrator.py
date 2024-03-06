503072315c4be95be1a7965e3ed9c5b29872f5a6
matheus <matheuschomem@hotmail.com>
2024-03-06 07:54:54 -0300

    old files are no longer cleaned and CleaningOrchestrator now focus on json cleaning

--- src/etl/cleaning/orchestrator.py
+++ src/etl/cleaning/orchestrator.py
@@ -2,6 +2,7 @@ from src.etl.patterns.orchestrator import Orchestrator
 from src.etl.cleaning.engine import CleanerEngine
 
 import yaml
+import glob
 
 class CleanerOrchestrator(Orchestrator):
 	
@@ -10,55 +11,56 @@ class CleanerOrchestrator(Orchestrator):
 		
 		self.process = "CLEANING"
 
-		## Raw Morning Paths
-		self.raw_sun_path_v2	= self.paths.get_file_path("ingestion", "morning_routine_v2.xlsx")
-		self.raw_sun_path_v3	= self.paths.get_file_path("ingestion", "morning_data_02_24.xlsx")
-		
-		## Raw Night Paths
-		self.raw_moon_path_v2	= self.paths.get_file_path("ingestion", "night_routine_v2.xlsx")
-		self.raw_moon_path_v3	= self.paths.get_file_path("ingestion", "night_data_02_24.xlsx")
-		self.raw_moon_path_v4	= self.paths.get_file_path("ingestion", "night_compass_03_24.xlsx")
+		## Ingestion Path
+		self.ingestion_paths_morning	= glob.glob(f'{self.paths.ingestion}/morning*.json')
+		self.ingestion_paths_night		= glob.glob(f'{self.paths.ingestion}/night*.json')
 
 		## Cleaned Paths
-		self.clnd_mrn_cold_path	= self.paths.get_file_path("cleaned",   "mrn_cleaned_cold.parquet")
-		self.clnd_mrn_hot_path	= self.paths.get_file_path("cleaned",   "mrn_cleaned_hot.parquet")
-		self.clnd_night_path	= self.paths.get_file_path("cleaned",   "ngt_cleaned.parquet")
+		self.cleaned_path_morning	= self.paths.get_file_path("cleaned", "morning.parquet")
+		self.cleaned_path_night	= self.paths.get_file_path("cleaned", "ngt_cleaned.parquet")
 
-		self.yaml_path = self.paths.get_file_path("yaml", "data_schema.yaml")
+		self.yaml_path = self.paths.get_file_path("yaml", "cleaning_config.yaml")
 
 		with open(self.yaml_path, 'r', encoding='utf-8') as file:
-			self.data_schema = yaml.safe_load(file)
+			self.cleaning_config = yaml.safe_load(file)
 
-		self.engine = CleanerEngine(self.data_schema)
+		self.engine = CleanerEngine(cleaning_schema=self.cleaning_config)
 
-		self.tables_relation = [
-			["morning_v2", self.raw_sun_path_v2, self.clnd_mrn_cold_path, "morning"],
-			["morning_v3", self.raw_sun_path_v3, self.clnd_mrn_hot_path, "morning"],
-			["night_0324", self.raw_moon_path_v4, self.clnd_night_path, "night"]
-		]
-
-	def cleaning(self, df_raw, table_id, mrng_or_night):
+	def cleaning(self, df_raw, table_id):
 		self.logger.info(f"{self.process} Engine: EXECUTION Started")
 		df_cleaned = self.engine.execute(
 			df_to_execute=df_raw,
 			table_id=table_id,
-			day_column="day_date",
-			mrng_or_night=mrng_or_night
+			day_column="day_date"
 		)
 		self.logger.info(f"{self.process} Engine: EXECUTION Finished")
 		return df_cleaned
 	
 	def execute(self):
 		self.logger = Orchestrator.logger
-		for table_id, raw_path, cleaned_path, mrng_or_night in self.tables_relation:
-			self.logger.info("*********************************************************")
-			self.logger.info(f"///////// STARTING {table_id} CLEANING PROCESS /////////")
-			self.logger.info("*********************************************************")
+		self.logger.info("*********************************************************")
+		self.logger.info(f"////////////// STARTING CLEANING PROCESS ///////////////")
+		self.logger.info("*********************************************************")
 
-			raw_data = self.reading(file_format="xlsx", file_path=raw_path)
+		# TODO= Create a loop for cleaning morning data and cleaning night data
+		self.logger.info(f"//////////////// CLEANING MORNING DATA /////////////////")
+		self.logger.info("*********************************************************")	
+		for ingestion_path in self.ingestion_paths_morning:		
 			
-			cleaned_data = self.cleaning(df_raw=raw_data, table_id=table_id, mrng_or_night=mrng_or_night)
+			ingestion_dt_is_stored = self.validate_ingestion(
+				ingestion_path=ingestion_path, 
+				cleaned_path=self.cleaned_path_morning
+				)
 
-			validated_data = self.validating(df_to_validate=cleaned_data)
-
-			self.writing(df_to_write=validated_data, file_path=cleaned_path)
+			if ingestion_dt_is_stored:
+				pass
+			else:
+				file_name = ingestion_path.split("ingestion")[1]
+				processing_date = file_name[1:].split("_")[1].split("(")[0]
+				self.logger.info(f" Date to be Cleaned: {processing_date} ")
+				df_raw = self.reading(file_format="json", file_path=ingestion_path)
+				df_stored = self.reading(file_format="parquet", file_path=self.cleaned_path_morning)
+				
+				df_cleaned = self.cleaning(df_raw=df_raw, table_id="morning")
+				df_append = df_stored.vstack(df_cleaned)
+				self.writing(df_to_write=df_append, file_path=self.cleaned_path_morning)
