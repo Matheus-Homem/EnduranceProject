9d7fe3eedec885c0420c791ff03ff0da27095209
matheus <matheuschomem@hotmail.com>
2024-03-30 06:35:40 -0300

    contatin a main module to import all other functions

---
+++ src/web/js/main.js
@@ -0,0 +1,26 @@
+import { validatePercentage } from './validator.js';
+import {
+    createList,
+    dragStart,
+    dragEnter,
+    dragLeave,
+    dragOver,
+    dragDrop,
+    swapItems,
+    checkOrder,
+    addEventListeners
+}  from './draggable.js';
+
+
+
+
+// let isValidPercentage = validatePercentage();
+// let createList = createList();
+// let dragStart = dragStart();
+// let dragEnter = dragEnter();
+// let dragLeave = dragLeave();
+// let dragOver = dragOver();
+// let dragDrop = dragDrop();
+// let swapItems = swapItems();
+// let checkOrder = checkOrder();
+// let addEventListeners = addEventListeners();
