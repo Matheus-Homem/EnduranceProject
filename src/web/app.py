9200499a1161e39ec059af8a1b3f982118b8df2d
matheus <matheuschomem@hotmail.com>
2024-04-15 09:52:18 -0300

    Refactor app.py to use MySQL instead of MongoDB

--- src/web/app.py
+++ src/web/app.py
@@ -1,29 +1,47 @@
-from src.env.credentials import Credentials
+from src.shared.connections.builder import build_connection
+from src.shared.definition.statements import DataManipulationLanguage
+from src.shared.definition.tables import MorningRawTable, NightRawTable
+
 from flask import Flask, render_template, request
-from flask_pymongo import PyMongo
+import mysql.connector
+import sshtunnel
 import os
+
+sshtunnel.SSH_TIMEOUT = 5.0
+sshtunnel.TUNNEL_TIMEOUT = 5.0
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.urandom(24)
 
-# Use environment variable for MONGO_URI if it"s set, otherwise use the local URI
-app.config["MONGO_URI"] = Credentials.MONGO_URI
 
-mongo = PyMongo(app)
+ssh_connection = build_connection(
+    connection_type="ssh",
+    instance=sshtunnel.SSHTunnelForwarder,
+)
+mysql_connection = build_connection(
+    connection_type="mysql",
+    instance=mysql.connector.connect,
+    tunnel=ssh_connection,
+)
+
+DML = DataManipulationLanguage(connection=mysql_connection)
 
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
 
-@app.route("/form/night/", methods=["GET", "POST"])
-def form_night():
-    return render_template("form_night_new.html")
+# @app.route("/form/night/", methods=["GET", "POST"])
+# def form_night():
+#     return render_template("form_night_new.html")
 
 @app.route("/form/morning/", methods=["GET", "POST"])
 def form_morning():
     if request.method == "POST":
         result = request.form
-        mongo.db.morning_form.insert_one(result.to_dict())
+        DML.insert(
+            table=MorningRawTable,
+            values=result.to_dict()
+        )
         return render_template("index.html")
     else:
         return render_template("form_morning_new.html")
