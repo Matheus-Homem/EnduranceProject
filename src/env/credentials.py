768fe486fdea6cf2be2a178bf6d63cff9165a906
matheus <matheuschomem@hotmail.com>
2024-04-14 21:05:20 -0300

    Update email recipient variable name in credentials.py

--- src/env/credentials.py
+++ src/env/credentials.py
@@ -6,11 +6,11 @@ from dataclasses import dataclass
 class Credentials:
     SMTP_PORT: int = 587
     SMTP_SERVER: str = "smtp.gmail.com"
-    SMTP_USERNAME: str = os.getenv("SMTP_USERNAME")
-    SMTP_PASSWORD: str = os.getenv("SMTP_PASSWORD")
-    SMTP_RECIPIENT: str = os.getenv("SMTP_RECIPIENT")
+    SMTP_USERNAME: str = os.getenv("SMTP_SENDER_EMAIL")
+    SMTP_PASSWORD: str = os.getenv("SMTP_SENDER_PASSWORD")
+    SMTP_RECIEVER: str = os.getenv("SMTP_RECIEVER_EMAIL")
     IMAP_SENDER: str = os.getenv("IMAP_SENDER")
     IMAP_PASSWORD: str = os.getenv("IMAP_PASSWORD")
-    IMAP_RECIPIENT: str = os.getenv("IMAP_RECIPIENT")
+    IMAP_RECIEVER: str = os.getenv("IMAP_RECIEVER")
     VALIDATED_EMAIL: str = os.getenv("VERIFIED_EMAIL")
     MONGO_URI: str = os.getenv("MONGO_URI", "mongodb://localhost:27017/test_db")
