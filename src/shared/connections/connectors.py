8f58c7b5a55f176e2fd6320ac5179edad3738b11
matheus <matheuschomem@hotmail.com>
2024-04-15 09:51:56 -0300

    Refactor email connectors and add SSH connector

--- src/shared/connections/connectors.py
+++ src/shared/connections/connectors.py
@@ -1,5 +1,8 @@
+from src.shared.definition.exceptions import ConnectionNotEstablished
 from src.shared.connections.credentials import Credential
+
 from typing import Protocol
+
 
 class Connector(Protocol):
 
@@ -8,6 +11,7 @@ class Connector(Protocol):
 
     def build_connection(self):
         ...
+
 
 class SmtpConnector(Connector):
 
@@ -18,35 +22,92 @@ class SmtpConnector(Connector):
         return self.credential
 
     def build_connection(self, instance):
-        self.instance = instance
-        return self.instance(
+        self.connection = instance(
             host=self.credential.get_server(),
             port=self.credential.get_port()
         )
-    
+        return self.connection
+
     def account_login(self):
-        self.instance.starttls()
-        return self.instance.login(
-            self.credential.get_username(),
+        self.connection.starttls()
+        return self.connection.login(
+            self.credential.get_sender(),
             self.credential.get_password()
         )
-    
+
     def address_message(self, message):
-        message["From"] = self.credential.get_username()
+        message["From"] = self.credential.get_sender()
         message["To"] = self.credential.get_reciever()
         return message
-    
+
     def send_email(self, message):
-        return self.instance.sendmail(
-            from_addr = self.credential.get_username(),
+        return self.connection.sendmail(
+            from_addr = self.credential.get_sender(),
             to_addrs = self.credential.get_reciever(),
             msg = message.as_bytes()
         )
 
-class MySqlConnector(Connector):
-    def build_connection(self, credential: Credential):
-        return super().build(credential)
-    
+
 class SshConnector(Connector):
-    def build_connection(self, credential: Credential):
-        return super().build(credential)
+
+    def __init__(self, credential: Credential):
+        self.credential = credential
+
+    def build_connection(self, instance):
+        self.tunnel = instance(
+            ssh_address_or_host=self.credential.get_host(),
+            ssh_username=self.credential.get_username(),
+            ssh_password=self.credential.get_password(),
+            remote_bind_address=(
+                self.credential.get_hostname(), 
+                self.credential.get_port()
+            )
+        )
+        return self.tunnel
+
+    def start_tunnel(self):
+        self.tunnel.start()
+
+    def close_tunnel(self):
+        self.tunnel.close()
+
+
+class MySqlConnector(Connector):
+
+    def __init__(self, credential: Credential):
+        self.credential = credential
+        self.connection = None
+        self.cursor = None
+
+    def build_connection(self, instance, tunnel):
+        tunnel.start_tunnel()
+        self.connection = instance(
+            user=self.credential.get_host(),
+            password=self.credential.get_password(),
+            host=self.credential.get_username(),
+            port=tunnel.local_bind_port,
+            db=self.credential.get_database()
+        )
+        return self.connection
+
+    def get_cursor(self):
+        try:
+            self.cursor = self.connection.cursor()
+        except AttributeError:
+            raise ConnectionNotEstablished("Call build_connection first.")
+        return self.cursor
+
+    def commit(self):
+        try:
+            self.connection.commit()
+        except AttributeError:
+            raise ConnectionNotEstablished("Call build_connection first.")
+
+    def close(self, tunnel):
+        try:
+            if self.cursor is not None:
+                self.cursor.close()
+            self.connection.close()
+            tunnel.close_tunnel()
+        except AttributeError:
+            raise ConnectionNotEstablished("Call build_connection first.")
