c961795fae94ad27f23f6a6c6b949662fd1fff7b
matheus <matheuschomem@hotmail.com>
2024-04-16 08:48:27 -0300

    Refactor connection builder to support multiple libraries and add new connection types

--- src/shared/connections/builder.py
+++ src/shared/connections/builder.py
@@ -1,16 +1,28 @@
-from typing import NewType, Type, Tuple, Dict
 from src.shared.connections.connectors import Connector, SmtpConnector, SshConnector, MySqlConnector
 from src.shared.connections.credentials import Credential, SmtpCredential, SshCredential, MySqlCredential
 
-# Define a new type for the connection keys
+from typing import NewType, Type, Tuple, Union, Dict
+from enum import Enum
+import mysql.connector
+import sshtunnel
+import smtplib
+
+sshtunnel.SSH_TIMEOUT = 5.0
+sshtunnel.TUNNEL_TIMEOUT = 5.0
+
 ConnectionType = NewType('ConnectionType', str)
 
-# Mapping of ConnectionType keys to Connector and Credential classes
-CONNECTION_CLASSES: Dict[ConnectionType, Type[Tuple[Connector, Credential]]] = {
-    ConnectionType("smtp"): (SmtpConnector, SmtpCredential),
-    ConnectionType("ssh"): (SshConnector, SshCredential),
-    ConnectionType("mysql"): (MySqlConnector, MySqlCredential),
+CONNECTION_CLASSES: Dict[ConnectionType, Type[Tuple[Connector, Credential, Union[mysql.connector.connect, sshtunnel.SSHTunnelForwarder, smtplib.SMTP]]]] = {
+    ConnectionType("smtp"): (SmtpConnector, SmtpCredential, smtplib.SMTP),
+    ConnectionType("ssh"): (SshConnector, SshCredential, sshtunnel.SSHTunnelForwarder),
+    ConnectionType("mysql"): (MySqlConnector, MySqlCredential, mysql.connector.connect),
 }
+
+class Connector(Enum):
+    SMTP = ConnectionType("smtp")
+    SSH = ConnectionType("ssh")
+    MYSQL = ConnectionType("mysql")
+
 
 def build_connection(connection_type: ConnectionType, **kwargs):
     """
@@ -26,13 +38,13 @@ def build_connection(connection_type: ConnectionType, **kwargs):
     Raises:
         ValueError: If the connection_type is not recognized.
     """
-    classes = CONNECTION_CLASSES.get(connection_type)
+    classes = CONNECTION_CLASSES.get(connection_type.value)
 
     if classes is None:
         raise ValueError(f"Unrecognized connection_type '{connection_type}'")
 
-    connector_class, credential_class = classes
+    connector_class, credential_class, lib_class = classes
     credential = credential_class()
     connector = connector_class(credential)
 
-    return connector.build_connection(**kwargs)
+    return connector.build_connection(lib=lib_class, **kwargs)
