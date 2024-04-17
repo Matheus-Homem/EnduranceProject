473b2c1b2b8e5e8ede5888d75684ddc099617785
matheus <matheuschomem@hotmail.com>
2024-04-17 15:25:11 -0300

    Refactor build_connection function to accept additional arguments and handle unrecognized connection_type in builder.py

--- src/shared/connections/builder.py
+++ src/shared/connections/builder.py
@@ -1,53 +1,24 @@
-from src.shared.connections.connectors import Connector, SmtpConnector, SshConnector, MySqlConnector
+from src.shared.connections.connectors import Connector, SmtpConnector, SshConnector, MySqlConnector, ConnectionType
 from src.shared.connections.credentials import Credential, SmtpCredential, SshCredential, MySqlCredential
 
-from typing import NewType, Type, Tuple, Union, Dict
-from enum import Enum
-import mysql.connector
-import sshtunnel
-import smtplib
+from typing import Type, Tuple, Dict
 
-sshtunnel.SSH_TIMEOUT = 5.0
-sshtunnel.TUNNEL_TIMEOUT = 5.0
 
-ConnectionType = NewType('ConnectionType', str)
-
-CONNECTION_CLASSES: Dict[ConnectionType, Type[Tuple[Connector, Credential, Union[mysql.connector.connect, sshtunnel.SSHTunnelForwarder, smtplib.SMTP]]]] = {
-    ConnectionType("smtp"): (SmtpConnector, SmtpCredential, smtplib.SMTP),
-    ConnectionType("ssh"): (SshConnector, SshCredential, sshtunnel.SSHTunnelForwarder),
-    ConnectionType("mysql"): (MySqlConnector, MySqlCredential, mysql.connector.connect),
+CONNECTION_CLASSES: Dict[ConnectionType, Type[Tuple[Connector, Credential]]] = {
+    ConnectionType("smtp"): (SmtpConnector, SmtpCredential),
+    ConnectionType("ssh"): (SshConnector, SshCredential),
+    ConnectionType("mysql"): (MySqlConnector, MySqlCredential),
 }
 
-class Connector(Enum):
-    SMTP = ConnectionType("smtp")
-    SSH = ConnectionType("ssh")
-    MYSQL = ConnectionType("mysql")
-
-
-def build_connection(
-        connection_type: ConnectionType,
-        **kwargs
-    ) -> Connector:
-    """
-    Builds a connection using the specified connection type.
-
-    Args:
-        connection_type (ConnectionType): The key for the connector and credential classes to use.
-        **kwargs: Arbitrary keyword arguments to be passed to the connector's build method.
-
-    Returns:
-        The built connection object.
-
-    Raises:
-        AttributeError: If the connection_type is not recognized.
-    """
-    classes = CONNECTION_CLASSES.get(connection_type.value)
-
-    if classes is None:
-        raise AttributeError(f"Unrecognized connection_type '{connection_type}'")
-
-    connector_class, credential_class, lib_class = classes
-    credential = credential_class()
-    connector = connector_class(credential)
-
-    return connector.build_connection(lib=lib_class, **kwargs)
+def build_connection(connection_type: ConnectionType, **kwargs) -> Connector:
+    
+    try:
+        classes = CONNECTION_CLASSES.get(connection_type.value)
+    except AttributeError:
+        raise AttributeError(f"Unrecognized connection_type '{connection_type.value}'")
+    else:
+        classes = CONNECTION_CLASSES.get(connection_type.value)
+        connector_class, credential_class = classes
+        credential = credential_class()
+        connector = connector_class(credential)
+        return connector.build_connection(**kwargs)
