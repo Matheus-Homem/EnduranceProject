ce79bc93b3abfc97117ae99b3c558ad0e3df2887
matheus <matheuschomem@hotmail.com>
2024-04-17 08:48:48 -0300

    Refactor test_credentials.py to use environment variables for SMTP and IMAP receiver emails

--- tests/shared/connections/test_credentials.py
+++ tests/shared/connections/test_credentials.py
@@ -1,14 +1,15 @@
 import unittest
 from unittest.mock import patch
+from src.shared.connections.credentials import SmtpCredential, SshCredential, MySqlCredential
 
 
 OS_ENVIRON_DICT = {
     "SMTP_SENDER_EMAIL" : "smtp_sender_email",
     "SMTP_SENDER_PASSWORD" : "smtp_sender_password",
-    "SMTP_RECIEVER_EMAIL" : "smtp_reciever_email",
+    "SMTP_RECEIVER_EMAIL" : "smtp_reciever_email",
     "IMAP_SENDER_EMAIL" : "imap_sender_email",
     "IMAP_SENDER_PASSWORD" : "imap_sender_password",
-    "IMAP_RECIEVER_EMAIL" : "imap_reciever_email",
+    "IMAP_RECEIVER_EMAIL" : "imap_reciever_email",
     "SSH_USERNAME": "ssh_username",
     "SSH_PASSWORD": "ssh_password",
     "MYSQL_USERNAME": "mysql_username",
@@ -19,29 +20,27 @@ OS_ENVIRON_DICT = {
 
 class TestCredentials(unittest.TestCase):
 
-    @patch.dict('os.environ', OS_ENVIRON_DICT, clear=True)
-    def test_all_credentials(self):
-
-        from src.shared.connections.credentials import SmtpCredential, SshCredential, MySqlCredential
-        
-        
-        smtp_credentials = SmtpCredential()
+    @patch("src.shared.connections.credentials.os.getenv", side_effect=lambda x: OS_ENVIRON_DICT.get(x))
+    def test_smtp_credentials(self, mock_getenv):
+        smtp_credentials = SmtpCredential(getenv=mock_getenv)
         self.assertEqual(smtp_credentials.get_port(), 587)
         self.assertEqual(smtp_credentials.get_server(), "smtp.gmail.com")
         self.assertEqual(smtp_credentials.get_sender(), 'smtp_sender_email')
         self.assertEqual(smtp_credentials.get_password(), 'smtp_sender_password')
-        self.assertEqual(smtp_credentials.get_reciever(), 'smtp_reciever_email')
-        
-        
-        ssh_credentials = SshCredential()
+        self.assertEqual(smtp_credentials.get_receiver(), 'smtp_reciever_email')
+
+    @patch("src.shared.connections.credentials.os.getenv", side_effect=lambda x: OS_ENVIRON_DICT.get(x))
+    def test_ssh_credentials(self, mock_getenv):
+        ssh_credentials = SshCredential(getenv=mock_getenv)
         self.assertEqual(ssh_credentials.get_host(), "ssh.pythonanywhere.com")
         self.assertEqual(ssh_credentials.get_port(), 3306)
         self.assertEqual(ssh_credentials.get_username(), 'ssh_username')
         self.assertEqual(ssh_credentials.get_password(), 'ssh_password')
         self.assertEqual(ssh_credentials.get_hostname(), 'ssh_username.mysql.pythonanywhere-services.com')
 
-
-        mysql_credentials = MySqlCredential()
+    @patch("src.shared.connections.credentials.os.getenv", side_effect=lambda x: OS_ENVIRON_DICT.get(x))
+    def test_mysql_credentials(self, mock_getenv):
+        mysql_credentials = MySqlCredential(getenv=mock_getenv)
         self.assertEqual(mysql_credentials.get_host(), "127.0.0.1")
         self.assertEqual(mysql_credentials.get_username(), 'mysql_username')
         self.assertEqual(mysql_credentials.get_password(), 'mysql_password')
