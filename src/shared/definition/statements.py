1656769b3885482126fbf13d5676582cddd4bbe0
matheus <matheuschomem@hotmail.com>
2024-04-20 09:12:20 -0300

    Refactor DataManipulationLanguage class to handle exceptions and print error messages

--- src/shared/definition/statements.py
+++ src/shared/definition/statements.py
@@ -7,72 +7,92 @@ class DataManipulationLanguage:
 
     def __init__(
         self,
-        connection
+        cursor
     ):
-        self.connection = connection
-        self.cursor = self.connection.get_cursor()
+        self.cursor = cursor
 
     def select(
             self,
             table: Table,
             condition: str = None,
         ) -> List[str]:
-        sql_select_query = f"SELECT * FROM {table}"
-        if condition is not None:
-            sql_select_query += f" WHERE {condition}"
-        self.cursor.execute(sql_select_query)
-        rows = self.cursor.fetchall()
-        return rows
+        try:
+            sql_select_query = f"SELECT * FROM {table}"
+            if condition is not None:
+                sql_select_query += f" WHERE {condition}"
+            self.cursor.execute(sql_select_query)
+            rows = self.cursor.fetchall()
+            for row in rows:
+                print(row)
+        except Exception as e:
+            print(f"An error occurred when selecting from {table}: {e}")
 
-    def select_columns(
+    def select_cols(
             self,
             table: Table,
             columns: List[str],
             condition: str = None
         ) -> List[str]:
-        sql_select_columns_query = f"SELECT {', '.join(columns)} FROM {table}"
-        if condition is not None:
-            sql_select_columns_query += f" WHERE {condition}"
-        self.cursor.execute(sql_select_columns_query)
-        rows = self.cursor.fetchall()
-        return rows
+        try:
+            sql_select_columns_query = f"SELECT {', '.join(columns)} FROM {table}"
+            if condition is not None:
+                sql_select_columns_query += f" WHERE {condition}"
+            self.cursor.execute(sql_select_columns_query)
+            rows = self.cursor.fetchall()
+            for row in rows:
+                print(row)
+        except Exception as e:
+            print(f"An error occurred when selecting cols from {table}: {e}")
 
     def top(
             self,
             table: Table,
             n: int
         ) -> List[str]:
-        sql_top_query = f"SELECT * FROM {table} LIMIT {n}"
-        self.cursor.execute(sql_top_query)
-        rows = self.cursor.fetchall()
-        return rows
+        try:
+            sql_top_query = f"SELECT * FROM {table} LIMIT {n}"
+            self.cursor.execute(sql_top_query)
+            rows = self.cursor.fetchall()
+            for row in rows:
+                print(row)
+        except Exception as e:
+            print(f"An error occurred when top selecting from {table}: {e}")
 
     def count(
             self,
             table: Table
         ) -> int:
-        sql_count_query = f"SELECT COUNT(*) FROM {table}"
-        self.cursor.execute(sql_count_query)
-        count = self.cursor.fetchone()[0]
-        return count
+        try:
+            sql_count_query = f"SELECT COUNT(*) FROM {table}"
+            self.cursor.execute(sql_count_query)
+            count = self.cursor.fetchone()[0]
+            print(f"A tabela {table} possui {count} linhas.")
+        except Exception as e:
+            print(f"An error occurred when counting from {table}: {e}")
 
     def insert(
         self,
         table: Table,
         values: List[str]
     ):
-        sql_insert_query = f"INSERT INTO {table} ({Table.COLUMNS}) VALUES ({', '.join(['%s' for _ in values])})"
-        self.cursor.execute(sql_insert_query, values)
-        self.connection.commit()
+        try:
+            sql_insert_query = f"INSERT INTO {table} ({Table.COLUMNS}) VALUES ({', '.join(['%s' for _ in values])})"
+            self.cursor.execute(sql_insert_query, values)
+            self.cursor.commit()
+        except Exception as e:
+            print(f"An error occurred when inserting into {table}: {e}")
 
     def delete(
             self,
             table: Table,
             condition: str
         ):
-        sql_delete_query = f"DELETE FROM {table} WHERE {condition}"
-        self.cursor.execute(sql_delete_query)
-        self.connection.commit()
+        try:
+            sql_delete_query = f"DELETE FROM {table} WHERE {condition}"
+            self.cursor.execute(sql_delete_query)
+            self.cursor.commit()
+        except Exception as e:
+            print(f"An error occurred when deleting from {table}: {e}")
 
     def update(
             self,
@@ -81,11 +101,14 @@ class DataManipulationLanguage:
             new_value: str,
             condition: str
         ):
-        if not table.validate_existence(column):
-            raise ValueError(f"Column {column} does not exist in table {table}")
-        sql_update_query = f"UPDATE {table} SET {column} = %s WHERE {condition}"
-        self.cursor.execute(sql_update_query, (new_value,))
-        self.connection.commit()
+        try:
+            if not table.validate_existence(column):
+                raise ValueError(f"Column {column} does not exist in table {table}")
+            sql_update_query = f"UPDATE {table} SET {column} = %s WHERE {condition}"
+            self.cursor.execute(sql_update_query, (new_value,))
+            self.cursor.commit()
+        except Exception as e:
+            print(f"An error occurred when updating {table}: {e}")
 
     def merge(
             self,
@@ -93,6 +116,9 @@ class DataManipulationLanguage:
             values: List[str],
             condition: str
         ):
-        sql_merge_query = f"MERGE INTO {table} ({table.COLUMNS}) VALUES ({', '.join(['%s' for _ in values])}) WHERE {condition}"
-        self.cursor.execute(sql_merge_query, values)
-        self.connection.commit()
+        try:
+            sql_merge_query = f"MERGE INTO {table} ({table.COLUMNS}) VALUES ({', '.join(['%s' for _ in values])}) WHERE {condition}"
+            self.cursor.execute(sql_merge_query, values)
+            self.cursor.commit()
+        except Exception as e:
+            print(f"An error occurred when merging {table}: {e}")
