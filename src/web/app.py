8675ed60ad44b0511cab6161240a82709764b05d
matheus <matheuschomem@hotmail.com>
2024-04-23 08:31:57 -0300

    Refactor app.py to use establish_mysql_connection decorator for form_morning route and handle database insertion retries

--- src/web/app.py
+++ src/web/app.py
@@ -1,13 +1,15 @@
 from src.web.utils import establish_mysql_connection
 
 from flask import Flask, render_template, request
-from datetime import datetime
 import json
 import os
+import time
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.urandom(24)
+
+MAX_RETRIES = 2
 
 @app.route("/", methods=["GET"])
 def index():
@@ -17,13 +19,24 @@ def index():
 @establish_mysql_connection
 def form_morning(cursor):
     if request.method == "POST":
-        date_id = datetime.now().strftime("%Y%m%d")
         data = request.form.to_dict()
         data_json = json.dumps(data)
-        cursor.execute(f"INSERT INTO RawMorning (id, data) VALUES ('{date_id}', '{data_json}')")
-        return render_template("index.html")
+        retry_count = 0
+        while retry_count < MAX_RETRIES:
+            try:
+                cursor.execute(f"INSERT INTO RawMorning (data) VALUES ('{data_json}');")
+                break
+            except Exception as e:
+                print(f"Erro ao inserir no banco de dados: {e}")
+                retry_count += 1
+                time.sleep(1)
+        if retry_count == MAX_RETRIES:
+            return "Erro ao inserir no banco de dados após várias tentativas", 500
+        else:
+            return render_template("index.html")
     else:
         return render_template("form_morning_new.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
