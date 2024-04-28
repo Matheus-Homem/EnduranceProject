ad460b2752239823d774ee9ad86915a3432e7c9e
matheus <matheuschomem@hotmail.com>
2024-04-28 12:30:52 -0300

    Refactor app.py to use establish_mysql_connection decorator for form_night route and handle database insertion retries

--- src/web/app.py
+++ src/web/app.py
@@ -38,10 +38,27 @@ def form_morning(cursor):
     else:
         return render_template("form_morning_new.html")
 
-
+@app.route("/form/night/", methods=["GET", "POST"])
+@establish_mysql_connection
+def form_night(cursor):
+    if request.method == "POST":
+        data = request.form.to_dict()
+        data_json = json.dumps(data)
+        retry_count = 0
+        while retry_count < MAX_RETRIES:
+            try:
+                cursor.execute(f"INSERT INTO RawNight (data) VALUES ('{data_json}');")
+                break
+            except Exception as e:
+                print(f"Erro ao inserir no banco de dados: {e}")
+                retry_count += 1
+                time.sleep(1)
+        if retry_count == MAX_RETRIES:
+            return "Erro ao inserir no banco de dados após várias tentativas", 500
+        else:
+            return render_template("index.html")
+    else:
+        return render_template("form_night_new.html")
+    
 if __name__ == "__main__":
     app.run(debug=True)
-
-# @app.route("/form/night/", methods=["GET", "POST"])
-# def form_night():
-#     return render_template("form_night_new.html") 
