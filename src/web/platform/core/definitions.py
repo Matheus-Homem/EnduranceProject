0be88c5b2e4821c83792aa4ffb140c1ea45c2157
matheus <matheuschomem@hotmail.com>
2024-06-16 06:54:39 -0300

    feat: Add optional columns and rows to InputDefinition
    This commit adds the optional columns and rows attributes to the InputDefinition class, allowing for more flexible input field configurations.

--- src/web/platform/core/definitions.py
+++ src/web/platform/core/definitions.py
@@ -1,4 +1,4 @@
-from dataclasses import asdict, dataclass, field
+from dataclasses import asdict, dataclass, field, default_factory
 from typing import Callable, Dict, List, Optional, Union
 
 from src.web.platform.core.types import FormattingType, InputType
@@ -6,10 +6,26 @@ from src.web.platform.core.types import FormattingType, InputType
 
 @dataclass
 class InputDefinition:
+    """
+    Define the structure of an input field in the form
+    - private_name: str: The input field name used on HTML and send to system.
+    - type: InputType: The type of the input field.
+    - options: Optional[Dict]: The options dict where keys are string send to system (private) and value is strings used on HTML (public).
+    - placeholder: Optional[str]: The placeholder of the input field.
+    - min_length: Optional[int]: The minimum length of the input field.
+    - max_length: Optional[int]: The maximum length of the input field.
+    - label: Optional[str]: The label of the input field.
+    - public_name: Optional[str]: The public name of the input field.
+    - output_formatting: Optional[FormattingType]: The output formatting of the input field.
+    - persona: Optional[str]: The persona of the input field.
+    - pillar: Optional[str]: The pillar of the input field.
+    - input_fullname: Optional[str]: The input fullname of the input field.
+    """
+
     private_name: str
     type: InputType
+    options: Optional[Dict] = None
     placeholder: Optional[str] = None
-    options: Optional[List[Dict]] = None
     min_length: Optional[int] = None
     max_length: Optional[int] = None
     label: Optional[str] = None
@@ -18,8 +34,17 @@ class InputDefinition:
     persona: Optional[str] = None
     pillar: Optional[str] = None
     input_fullname: Optional[str] = None
+    optional: Optional[bool] = False
+    columns: Optional[Dict] = None
+    rows: Optional[Dict] = None
 
-    def asdict(self):
+    def asdict(self) -> dict:
+        """
+        Convert the InputDefinition object to a dictionary.
+
+        Returns:
+        - dict: The InputDefinition object as a dictionary.
+        """
         return asdict(self)
 
 
