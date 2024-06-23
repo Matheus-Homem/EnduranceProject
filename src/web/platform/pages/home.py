75980248a72cc67fabd544a568cb3a70d78abdf6
matheus <matheuschomem@hotmail.com>
2024-06-23 19:38:07 -0300

    feat: Add home page definition and basic section to home.py

--- src/web/platform/pages/home.py
+++ src/web/platform/pages/home.py
@@ -1,3 +1,18 @@
-from src.web.platform.core.definitions import PageDefinition, SectionDefinition
+from src.web.platform.core.definitions import (
+    BoxDefinition,
+    PageDefinition,
+    SectionDefinition,
+)
+from src.web.platform.core.enums import SectionType
 
-
+home_page = PageDefinition(
+    private_name="home_page",
+    title="Home",
+    header="Maarga Project",
+    sections=[
+        SectionDefinition(
+            type=SectionType.BASIC,
+            boxes=[],
+        ),
+    ],
+)
