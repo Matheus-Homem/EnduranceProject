88a9d2b133b5a457fae81f9e3ca9e666f7f0b980
matheus <matheuschomem@hotmail.com>
2024-06-23 19:37:49 -0300

    feat: refactor enums.py for consistency and readability

--- src/web/platform/core/enums.py
+++ src/web/platform/core/enums.py
@@ -20,6 +20,27 @@ class FormattingType:
     PERCENTAGE = "percentage"
 
 
+class SectionType:
+    BASIC = "basic"
+    WISDOM = "wisdom"
+    STABILITY = "stability"
+    STRENGTH = "strength"
+    KINDNESS = "kindness"
+    DEVOTION = "devotion"
+
+
+class DefinitionType:
+    PAGE = "page"
+    SECTION = "section"
+    BOX = "box"
+    INPUT = "input"
+    BUTTON = "button"
+
+    @classmethod
+    def get_all(cls):
+        return [value for name, value in vars(cls).items() if not name.startswith("__")]
+
+
 class Icon:
     PILLS = "fa-solid fa-pills"
     NOTES = "fa-solid fa-notes"
