aef3776db39378a0dc3ffb8786f88b756bb02db2
matheus <matheuschomem@hotmail.com>
2024-06-23 19:37:11 -0300

    feat: Refactor Builder classes for consistency and readability

--- src/web/platform/core/builders.py
+++ src/web/platform/core/builders.py
@@ -1,52 +1,26 @@
-from typing import Protocol
+from src.web.platform.core.enums import DefinitionType
+from src.web.platform.core.protocols import Definition
 
 
-class Builder(Protocol):
+class Builder:
 
-    def get_definition(self, definition):
-        pass
-
-    def parse_definition(self):
-        pass
-
-    def build(self):
-        pass
-
-
-class InputBuilder(Builder):
-
-    def get_definition(self, definition):
+    def set_definition(self, definition: Definition):
         self.definition = definition
 
-    def parse_definition(self):
+    # def _validate_definition(self):
+    #     if not isinstance(self.definition, Definition):
+    #         raise TypeError(f"{self.definition.private_name} is not of type 'Definition'")
+
+    def _validate_definition(self):
+        valid_definitions = DefinitionType.get_all()
+        try:
+            definition_type = self.definition.get_definition()
+            definition_type in valid_definitions
+        except AttributeError:
+            raise AttributeError(f"{type(self.definition)} is not a Definition")
+
+    def _parse_definition(self):
         pass
 
     def build(self):
-        pass
-
-
-class PersonaBuilder(Builder):
-
-    def get_definition(self, definition):
-        pass
-
-    def build(self):
-        pass
-
-
-class PillarBuilder(Builder):
-
-    def get_definition(self, definition):
-        pass
-
-    def build(self):
-        pass
-
-
-class FormBuilder(Builder):
-
-    def get_definition(self, definition):
-        pass
-
-    def build(self):
-        pass
+        self._validate_definition()
