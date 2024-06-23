089c65791898b9891e53ee2f7dff9b2c1d1bbfa8
matheus <matheuschomem@hotmail.com>
2024-06-23 19:38:44 -0300

    feat: Refactor night page to use new pillar boxes

--- src/web/platform/pages/night.py
+++ src/web/platform/pages/night.py
@@ -1,9 +1,9 @@
 from src.web.platform.core.definitions import FormDefinition, SectionDefinition
-from src.web.platform.pillars.wisdom import WisdomPersonas
-from src.web.platform.pillars.stability import StabilityPersonas
-from src.web.platform.pillars.strength import StrengthPersonas
-from src.web.platform.pillars.kindness import KindnessPersonas
-from src.web.platform.pillars.devotion import DevotionPersonas
+from src.web.platform.pillars.devotion import DevotionBoxes
+from src.web.platform.pillars.kindness import KindnessBoxes
+from src.web.platform.pillars.stability import StabilityBoxes
+from src.web.platform.pillars.strength import StrengthBoxes
+from src.web.platform.pillars.wisdom import WisdomBoxes
 
 night_form = FormDefinition(
     form_name="night_form",
@@ -12,38 +12,38 @@ night_form = FormDefinition(
             public_name="Sabedoria",
             private_name="wisdom",
             description="A Sabedoria é...",
-            personas=[
-                WisdomPersonas.NAVIGATOR,
-                WisdomPersonas.ALCHEMIST,
+            boxes=[
+                WisdomBoxes.NAVIGATOR,
+                WisdomBoxes.ALCHEMIST,
             ],
         ),
         SectionDefinition(
             public_name="Estabilidade",
             private_name="stability",
             description="A Estabilidade é...",
-            personas=[
-                StabilityPersonas.SPONSOR,
-                StabilityPersonas.PATRON,
+            boxes=[
+                StabilityBoxes.SPONSOR,
+                StabilityBoxes.PATRON,
             ],
         ),
         SectionDefinition(
             public_name="Força",
             private_name="strength",
             description="A Força é...",
-            personas=[
-                StrengthPersonas.VANGUARD,
-                StrengthPersonas.SCOUT,
-                StrengthPersonas.NIGHT_COOK,
+            boxes=[
+                StrengthBoxes.VANGUARD,
+                StrengthBoxes.SCOUT,
+                StrengthBoxes.NIGHT_COOK,
             ],
         ),
         SectionDefinition(
             public_name="Gentiliza",
             private_name="kindness",
             description="A Gentiliza é...",
-            personas=[
-                KindnessPersonas.CARETAKER,
-                KindnessPersonas.DIPLOMAT,
-                KindnessPersonas.CITIZEN,
+            boxes=[
+                KindnessBoxes.CARETAKER,
+                KindnessBoxes.DIPLOMAT,
+                KindnessBoxes.CITIZEN,
             ],
         ),
     ],
