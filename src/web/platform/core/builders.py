a85746608310b7733e54c6263a5d1d6f71a0f13b
matheus <matheuschomem@hotmail.com>
2024-06-24 08:45:17 -0300

    feat: add build_definition function

--- src/web/platform/core/builders.py
+++ src/web/platform/core/builders.py
@@ -6,21 +6,30 @@ class Builder:
 
     def set_definition(self, definition: Definition):
         self.definition = definition
+        self.validated = False
 
-    # def _validate_definition(self):
-    #     if not isinstance(self.definition, Definition):
-    #         raise TypeError(f"{self.definition.private_name} is not of type 'Definition'")
-
-    def _validate_definition(self):
+    def validate_definition(self):
         valid_definitions = DefinitionType.get_all()
         try:
             definition_type = self.definition.get_definition()
             definition_type in valid_definitions
+            self.validated = True
         except AttributeError:
             raise AttributeError(f"{type(self.definition)} is not a Definition")
 
-    def _parse_definition(self):
+    def parse_definition(self):
         pass
 
     def build(self):
-        self._validate_definition()
+        if self.validated:
+            self.definition.build_definition()
+        else:
+            raise AttributeError("Definition has not been validated")
+
+
+def build_definition(definition: Definition):
+    builder = Builder()
+    builder.set_definition(definition=definition)
+    builder.validate_definition()
+    builder.parse_definition()
+    builder.build()
