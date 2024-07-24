54a36be5e96f3bca5f0c3ed89fc4e6030e63c177
matheus <matheuschomem@hotmail.com>
2024-07-24 07:28:20 -0300

    chore: divide clean_form_data in two functions for clarity

--- src/web/utils.py
+++ src/web/utils.py
@@ -1,9 +1,14 @@
 import json
 import re
 
-def clean_form_data(data: dict) -> str:
-    def regex_remove(input: str) -> str:
-        return re.sub(r'[\r\n]', '', str(input))
-    cleaned_data = {regex_remove(key): regex_remove(str(value)) for key, value in data.items()}
+
+def clean_string(input: str) -> str:
+    return re.sub(r"[\r\n]", "", input)
+
+
+def prepare_form_data(data: dict) -> str:
+    cleaned_data = {
+        clean_string(key): clean_string(str(value)) for key, value in data.items()
+    }
     seriealized_data = json.dumps(cleaned_data)
     return seriealized_data
