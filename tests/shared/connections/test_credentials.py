c146f947919d59904e7235f0c9464dfc3c76c403
matheus <matheuschomem@hotmail.com>
2024-07-24 07:57:24 -0300

    fix: fix tests for credentials

--- tests/shared/connections/test_credentials.py
+++ tests/shared/connections/test_credentials.py
@@ -1,50 +1,41 @@
+from src.shared.credentials import SshCredential, MySqlCredential
+
 import unittest
 from unittest.mock import patch
-from src.shared.connections.credentials import SmtpCredential, SshCredential, MySqlCredential
-
-
-OS_ENVIRON_DICT = {
-    "SMTP_SENDER_EMAIL" : "smtp_sender_email",
-    "SMTP_SENDER_PASSWORD" : "smtp_sender_password",
-    "SMTP_RECEIVER_EMAIL" : "smtp_reciever_email",
-    "IMAP_SENDER_EMAIL" : "imap_sender_email",
-    "IMAP_SENDER_PASSWORD" : "imap_sender_password",
-    "IMAP_RECEIVER_EMAIL" : "imap_reciever_email",
-    "SSH_USERNAME": "ssh_username",
-    "SSH_PASSWORD": "ssh_password",
-    "MYSQL_USERNAME": "mysql_username",
-    "MYSQL_PASSWORD": "mysql_password",
-    "MYSQL_DATABASE": "mysql_database",
-}
+from os import getenv
 
 
 class TestCredentials(unittest.TestCase):
 
-    @patch("src.shared.connections.credentials.os.getenv", side_effect=lambda x: OS_ENVIRON_DICT.get(x))
-    def test_smtp_credentials(self, mock_getenv):
-        smtp_credentials = SmtpCredential(getenv=mock_getenv)
-        self.assertEqual(smtp_credentials.get_port(), 587)
-        self.assertEqual(smtp_credentials.get_server(), "smtp.gmail.com")
-        self.assertEqual(smtp_credentials.get_sender(), 'smtp_sender_email')
-        self.assertEqual(smtp_credentials.get_password(), 'smtp_sender_password')
-        self.assertEqual(smtp_credentials.get_receiver(), 'smtp_reciever_email')
+    def test_ssh_credentials(self):
+        ssh_credentials = SshCredential().get_all_credentials()
+        ssh_username = getenv("SSH_USERNAME")
 
-    @patch("src.shared.connections.credentials.os.getenv", side_effect=lambda x: OS_ENVIRON_DICT.get(x))
-    def test_ssh_credentials(self, mock_getenv):
-        ssh_credentials = SshCredential(getenv=mock_getenv)
-        self.assertEqual(ssh_credentials.get_host(), "ssh.pythonanywhere.com")
-        self.assertEqual(ssh_credentials.get_port(), 3306)
-        self.assertEqual(ssh_credentials.get_username(), 'ssh_username')
-        self.assertEqual(ssh_credentials.get_password(), 'ssh_password')
-        self.assertEqual(ssh_credentials.get_hostname(), 'ssh_username.mysql.pythonanywhere-services.com')
+        self.assertEqual(ssh_credentials["host"], "ssh.pythonanywhere.com")
+        self.assertEqual(ssh_credentials["port"], 3306)
+        self.assertEqual(ssh_credentials["username"], getenv("SSH_USERNAME"))
+        self.assertEqual(ssh_credentials["password"], getenv("SSH_PASSWORD"))
+        self.assertEqual(
+            ssh_credentials["hostname"],
+            f"{ssh_username}.mysql.pythonanywhere-services.com",
+        )
 
-    @patch("src.shared.connections.credentials.os.getenv", side_effect=lambda x: OS_ENVIRON_DICT.get(x))
-    def test_mysql_credentials(self, mock_getenv):
-        mysql_credentials = MySqlCredential(getenv=mock_getenv)
-        self.assertEqual(mysql_credentials.get_host(), "127.0.0.1")
-        self.assertEqual(mysql_credentials.get_username(), 'mysql_username')
-        self.assertEqual(mysql_credentials.get_password(), 'mysql_password')
-        self.assertEqual(mysql_credentials.get_database(), 'mysql_username$mysql_database')
+    def test_mysql_credentials(self):
+        mysql_credentials = MySqlCredential().get_all_credentials()
+        mysql_username = getenv("MYSQL_USERNAME")
+        mysql_database = getenv("MYSQL_DATABASE")
 
-if __name__ == '__main__':
+        self.assertEqual(mysql_credentials["host"], "127.0.0.1")
+        self.assertEqual(mysql_credentials["username"], getenv("MYSQL_USERNAME"))
+        self.assertEqual(mysql_credentials["password"], getenv("MYSQL_PASSWORD"))
+        self.assertEqual(
+            mysql_credentials["database"], f"{mysql_username}${mysql_database}"
+        )
+        self.assertEqual(
+            mysql_credentials["hostname"],
+            f"{mysql_username}.mysql.pythonanywhere-services.com",
+        )
+
+
+if __name__ == "__main__":
     unittest.main()
