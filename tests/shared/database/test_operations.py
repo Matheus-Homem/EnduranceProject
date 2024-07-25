89cdce9ada95068a320acaed9b0c484ec513138e
matheus <matheuschomem@hotmail.com>
2024-07-25 14:28:14 -0300

    develop tests for decorators and operations

--- tests/shared/database/test_operations.py
+++ tests/shared/database/test_operations.py
@@ -1,49 +1,45 @@
 import unittest
 from unittest.mock import MagicMock, patch
+from src.shared.database.operations import DatabaseOperations
 
 
 class TestDatabaseOperations(unittest.TestCase):
 
-    @patch("src.shared.decorators.establish_mysql_connection", lambda func: func)
+    def setUp(self):
+        self.cursor_mock = MagicMock()
+
+    @patch("src.shared.database.operations.establish_mysql_connection", lambda func: func)
     def test_execute_command(self):
-        cursor_mock = MagicMock()
         command = 'INSERT INTO local_test (data) VALUES (\'{"key1": "value1", "key2": "value2"}\');'
-        from src.shared.database.operations import DatabaseOperations
 
-        DatabaseOperations.execute_command(cursor_mock, command)
+        @canc
+        DatabaseOperations.execute_command(self.cursor_mock, command)
 
-        cursor_mock.execute.assert_called_once_with(command)
-        print("test_execute_command passed")
+        self.cursor_mock.execute.assert_called_once_with(command)
 
-    @patch("src.shared.decorators.establish_mysql_connection", lambda func: func)
+    @patch("src.shared.database.operations.establish_mysql_connection", lambda func: func)
     def test_select_data(self):
-        cursor_mock = MagicMock()
         command = "SELECT * FROM local_test;"
         expected_result = [("row1",), ("row2",)]
-        cursor_mock.fetchall.return_value = expected_result
-        from src.shared.database.operations import DatabaseOperations
+        self.cursor_mock.fetchall.return_value = expected_result
 
-        result = DatabaseOperations.select_data(cursor_mock, command)
+        result = DatabaseOperations.select_data(self.cursor_mock, command)
 
-        cursor_mock.execute.assert_called_once_with(command)
-        cursor_mock.fetchall.assert_called_once()
+        self.cursor_mock.execute.assert_called_once_with(command)
+        self.cursor_mock.fetchall.assert_called_once()
         self.assertEqual(result, expected_result)
-        print("test_select_data passed")
 
-    @patch("src.shared.decorators.establish_mysql_connection", lambda func: func)
+    @patch("src.shared.database.operations.establish_mysql_connection", lambda func: func)
     def test_count_rows(self):
-        cursor_mock = MagicMock()
         table = "local_test"
         expected_result = (5,)
-        cursor_mock.fetchone.return_value = expected_result
-        from src.shared.database.operations import DatabaseOperations
+        self.cursor_mock.fetchone.return_value = expected_result
 
-        result = DatabaseOperations.count_rows(cursor_mock, table)
+        result = DatabaseOperations.count_rows(self.cursor_mock, table)
 
-        cursor_mock.execute.assert_called_once_with(f"SELECT COUNT(*) FROM {table}")
-        cursor_mock.fetchone.assert_called_once()
+        self.cursor_mock.execute.assert_called_once_with(f"SELECT COUNT(*) FROM {table}")
+        self.cursor_mock.fetchone.assert_called_once()
         self.assertEqual(result, expected_result[0])
-        print("test_count_rows passed")
 
 
 if __name__ == "__main__":
