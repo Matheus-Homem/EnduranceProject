c8d72a22c245af0a097343530dd5412116b336cf
matheus <matheuschomem@hotmail.com>
2024-08-01 07:54:30 -0300

    chore: isort black

--- src/main.py
+++ src/main.py
@@ -1,26 +1,37 @@
+import argparse
+
+from src.env.globals import Global
 from src.etl.process.orchestrator import ProcessOrchestrator
 from src.report.reports import Report
-from src.env.globals import Global
 
-import argparse
 
 def main():
 
-	parser = argparse.ArgumentParser(description="Script that executes main.py")
+    parser = argparse.ArgumentParser(description="Script that executes main.py")
 
-	parser.add_argument('--automated', type=bool, default=False, help='Argument identifying script automation')
-	parser.add_argument('--date', default=None, help='Argument identifying script date in the format "YYYYMMDD"')
+    parser.add_argument(
+        "--automated",
+        type=bool,
+        default=False,
+        help="Argument identifying script automation",
+    )
+    parser.add_argument(
+        "--date",
+        default=None,
+        help='Argument identifying script date in the format "YYYYMMDD"',
+    )
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	automated = args.automated
-	script_date = args.date
+    automated = args.automated
+    script_date = args.date
 
-	Global(exec_date=script_date)
+    Global(exec_date=script_date)
 
-	ProcessOrchestrator().execute(automated=automated)
+    ProcessOrchestrator().execute(automated=automated)
 
-	Report().daily_publish(send_email=automated)
+    Report().daily_publish(send_email=automated)
+
 
 if __name__ == "__main__":
-	main()
+    main()
