c6cbf2b1ece408594a7f4696ab35ba77faf2e4c2
matheus <matheuschomem@hotmail.com>
2024-08-01 13:24:19 -0300

    feat: improved logger class

--- src/shared/logger.py
+++ src/shared/logger.py
@@ -2,6 +2,21 @@ import logging
 import os
 import colorlog
 from datetime import datetime
+import inspect    
+
+class CustomFormatter(logging.Formatter):
+    def __init__(self, fmt, class_name):
+        super().__init__(fmt)
+        self.class_name = class_name
+
+    def format(self, record):
+        src_index = record.pathname.find("src")
+        if src_index != -1:
+            record.pathname = record.pathname[src_index:]
+        if isinstance(self.class_name,str):
+            record.funcName = f"{self.class_name}.{record.funcName}"
+        return super().format(record)
+
 
 class Logger:
     _instance = None
@@ -11,35 +26,51 @@ class Logger:
             cls._instance = super(Logger, cls).__new__(cls)
         return cls._instance
 
-    def __init__(self, log_level=logging.DEBUG, log_file=None):
+    def __init__(self, log_level=logging.DEBUG, log_file=None, class_name=None):
         if not hasattr(self, 'logger'):
-            self.logger = logging.getLogger(__name__)
-            self.logger.setLevel(log_level)
+            self.logger = self._initialize_logger(log_level, log_file, class_name)
 
-            formatter = colorlog.ColoredFormatter(
-                '%(log_color)s%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                log_colors={
-                    'DEBUG': 'blue',
-                    'INFO': 'green',
-                    'WARNING': 'yellow',
-                    'ERROR': 'red',
-                }
-            )
+    def _initialize_logger(self, log_level, log_file, class_name):
+        caller_frame = inspect.stack()[1]
+        frame_info = inspect.getframeinfo(caller_frame[0])
+        caller_filename = frame_info.filename
+        caller_function = frame_info.function
 
-            date_str = datetime.now().strftime('%Y-%m-%d')
-            log_dir = os.path.join('outputs', 'logs', date_str)
-            os.makedirs(log_dir, exist_ok=True)
+        logger_name = f"{caller_filename}.{class_name}.{caller_function}"
+        logger = logging.getLogger(logger_name)
+        logger.setLevel(log_level)
 
-            if log_file is None:
-                timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-                log_file = os.path.join(log_dir, f'logs_{timestamp}.log')
-            file_handler = logging.FileHandler(log_file)
-            file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-            self.logger.addHandler(file_handler)
+        self._add_file_handler(logger, log_file, class_name)
+        self._add_console_handler(logger)
 
-            console_handler = colorlog.StreamHandler()
-            console_handler.setFormatter(formatter)
-            self.logger.addHandler(console_handler)
+        return logger
+
+    def _add_file_handler(self, logger, log_file, class_name):
+        date_str = datetime.now().strftime('%Y-%m-%d')
+        log_dir = os.path.join('outputs', 'logs', date_str)
+        os.makedirs(log_dir, exist_ok=True)
+
+        if log_file is None:
+            timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+            log_file = os.path.join(log_dir, f'logs_{timestamp}.log')
+
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setFormatter(CustomFormatter('%(asctime)s - %(pathname)s - %(funcName)s - %(levelname)s - %(message)s', class_name))
+        logger.addHandler(file_handler)
+
+    def _add_console_handler(self, logger):
+        formatter = colorlog.ColoredFormatter(
+            '%(log_color)s%(asctime)s - %(pathname)s - %(funcName)s - %(levelname)s - %(message)s',
+            log_colors={
+                'DEBUG': 'purple',
+                'INFO': 'cyan',
+                'WARNING': 'yellow',
+                'ERROR': 'red',
+            }
+        )
+        console_handler = colorlog.StreamHandler()
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
 
     def get_logger(self):
         return self.logger
