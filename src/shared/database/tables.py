8ad03b67a66d9c0d79fc99ebfab3a988ff45dae9
matheus <matheuschomem@hotmail.com>
2024-08-06 09:18:38 -0300

    feat: improve Table usage and fix columns

--- src/shared/database/tables.py
+++ src/shared/database/tables.py
@@ -1,28 +1,37 @@
-from sqlalchemy import JSON, Column, Integer
+from sqlalchemy import Column, Integer, String, JSON, DateTime, MetaData
 from sqlalchemy.orm import declarative_base
+from sqlalchemy.sql import func
 
-Base = declarative_base()
+metadata = MetaData()
+
+Base = declarative_base(metadata=metadata)
+
 
 class Table(Base):
     __abstract__ = True
 
-
 class LocalTest(Table):
     __tablename__ = "local_test"
 
-    id: int = Column(Integer, primary_key=True, autoincrement=True)
-    data: dict = Column(JSON, nullable=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    data = Column(JSON, nullable=False, default={})
+    profile = Column(String(255), nullable=False)
+    created_at = Column(DateTime, nullable=False, default=func.now())
 
 
 class MorningData(Table):
     __tablename__ = "morning_data"
 
-    id: int = Column(Integer, primary_key=True, autoincrement=True)
-    data: dict = Column(JSON, nullable=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    data = Column(JSON, nullable=False, default={})
+    profile = Column(String(255), nullable=False)
+    created_at = Column(DateTime, nullable=False, default=func.now())
 
 
 class NightData(Table):
     __tablename__ = "night_data"
 
-    id: int = Column(Integer, primary_key=True, autoincrement=True)
-    data: dict = Column(JSON, nullable=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    data = Column(JSON, nullable=False, default={})
+    profile = Column(String(255), nullable=False)
+    created_at = Column(DateTime, nullable=False, default=func.now())
