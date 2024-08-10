20605d5e44c5076d3989a4c323ad182976fb2153
matheus <matheuschomem@hotmail.com>
2024-08-10 17:22:08 -0300

    feat: add Table ABC class

--- src/etl/definitions.py
+++ src/etl/definitions.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+from src.etl.pipeline.execute import execute_pipeline
+from src.etl.pipeline.properties import PipelineProperties
 from src.shared.logger import LoggingManager
 
 
@@ -19,6 +21,29 @@ class Writer(ABC):
     @abstractmethod
     def write(self):
         pass
+
+
+class Table(ABC):
+    READER: Reader
+    WRITER: Writer
+
+    def __init__(self):
+        self.pipeline_properties = self.generate_pipeline_properties()
+
+    @abstractmethod
+    def generate_pipeline_properties(self) -> PipelineProperties:
+        pass
+
+    def update(self):
+        execute_pipeline(pipeline_properties=self.pipeline_properties)
+
+
+class SilverTable(Table):
+    LAYER: str = "SILVER"
+
+
+class GoldTable(Table):
+    LAYER: str = "GOLD"
 
 
 class ProccessingType:
