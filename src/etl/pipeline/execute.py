689f98974ccaa9bcb1a3ed4b4b21be34a6824a3f
matheus <matheuschomem@hotmail.com>
2024-08-13 13:01:51 -0300

    refactor: Remove unused code in execute_pipeline function

--- src/etl/pipeline/execute.py
+++ src/etl/pipeline/execute.py
@@ -1,9 +1,9 @@
-from src.etl.pipeline.properties import PipelineProperties
+# from src.etl.pipeline.properties import PipelineProperties
 
 
-def execute_pipeline(pipeline_properties: PipelineProperties):
-    reader = pipeline_properties.get_reader()
-    writer = pipeline_properties.get_writer()
+# def execute_pipeline(pipeline_properties: PipelineProperties):
+#     reader = pipeline_properties.get_reader()
+#     writer = pipeline_properties.get_writer()
 
-    df = reader.read()
-    writer.write(df)
+#     df = reader.read_data()
+#     writer.write_data(df)
