777bdb3827879bb178b9f1e4d0652fda7f546972
matheus <matheuschomem@hotmail.com>
2024-08-13 13:04:26 -0300

    refactor: Remove unused JSON reader and writer classes

--- src/etl/readers/json.py
+++
@@ -1,8 +0,0 @@
-from src.etl.definitions import Reader
-
-
-class JsonReader(Reader):
-
-    def read(self):
-        print("Reading from JSON file")
-        return None
--- src/etl/writers/json.py
+++
@@ -1,7 +0,0 @@
-from src.etl.definitions import Writer
-
-
-class JsonWriter(Writer):
-
-    def write(self, df):
-        print("Writing to JSON file")
