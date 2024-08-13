b44b4b3e3f1130e1e62c56adc059c01a37237fd3
matheus <matheuschomem@hotmail.com>
2024-08-13 13:02:59 -0300

    refactor: Update DatabaseReader to log source and table information when reading data

--- src/etl/readers/database.py
+++ src/etl/readers/database.py
@@ -4,8 +4,8 @@ from src.etl.definitions import Reader
 from src.shared.database.builder import DatabaseExecutorBuilder
 from src.shared.database.tables import MySqlTable
 
-
 class DatabaseReader(Reader):
+    SOURCE: str = "DATABASE"
 
     def __init__(self, table: MySqlTable) -> None:
         super().__init__()
@@ -13,9 +13,10 @@ class DatabaseReader(Reader):
         self.logger = self.logger_manager.get_logger()
         self.table = table
 
-    def read(self) -> pl.DataFrame:
+    def read_data(self) -> pl.DataFrame:
+        self.logger.info(f"Starting reading process. Source: {self.SOURCE} | Table: {self.table.__tablename__}")
         with DatabaseExecutorBuilder() as executor:
             database_table_data = executor.select(self.table)
         df = pl.DataFrame(database_table_data)
-        self.logger.info("Data read successfully")
+        self.logger.info(f"Data read successfully from {self.table.__tablename__} with {df.shape[0]} records")
         return df
