f35f80b66001d9888011aa44b2c6332413022d62
matheus <matheuschomem@hotmail.com>
2024-08-14 09:28:40 -0300

    refactor: Update ParquetWriter to write data using DataFrame

--- src/etl/writers/parquet.py
+++ src/etl/writers/parquet.py
@@ -1,14 +1,15 @@
 from src.etl.definitions import Writer, Table
+from polars import DataFrame
 
 
 class ParquetWriter(Writer):
-    TARGET: str = "RAW"
 
-    def __init__(self, table: Table) -> None:
-        super().__init__()
-        self.logger_manager.set_class_name(__class__.__name__)
-        self.logger = self.logger_manager.get_logger()
-        self.table = table
+    def __init__(self, target: Table) -> None:
+        super().__init__(target=target)
 
-    def write_data(self):
-        print("Writing to Parquet table")
+    def write_data(self, dataframe: DataFrame) -> None:
+        self.logger.info(f"Starting writing process. PATH: {self.target}")
+        dataframe.write_parquet(self.target)
+        self.logger.info(f"Data written successfully with {dataframe.shape[0]} records")
+
+        
