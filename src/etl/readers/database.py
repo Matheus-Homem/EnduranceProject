7fe61a94fa4e2147a9e59b4e7d871b9b5e37f60e
matheus <matheuschomem@hotmail.com>
2024-08-14 09:28:51 -0300

    refactor: Update DatabaseReader to use DataFrame instead of polars

--- src/etl/readers/database.py
+++ src/etl/readers/database.py
@@ -1,22 +1,19 @@
-import polars as pl
+from polars import DataFrame
 
 from src.etl.definitions import Reader
 from src.shared.database.builder import DatabaseExecutorBuilder
 from src.shared.database.tables import MySqlTable
 
 class DatabaseReader(Reader):
-    SOURCE: str = "DATABASE"
+    SOURCE: str = "Database"
 
-    def __init__(self, table: MySqlTable) -> None:
-        super().__init__()
-        self.logger_manager.set_class_name(__class__.__name__)
-        self.logger = self.logger_manager.get_logger()
-        self.table = table
+    def __init__(self, source:MySqlTable) -> None:
+        super().__init__(source=source)
 
-    def read_data(self) -> pl.DataFrame:
-        self.logger.info(f"Starting reading process. Source: {self.SOURCE} | Table: {self.table.__tablename__}")
+    def read_data(self) -> DataFrame:
+        self.logger.info(f"Starting reading process. SOURCE: {self.SOURCE} | TABLE: {self.source.__tablename__}")
         with DatabaseExecutorBuilder() as executor:
-            database_table_data = executor.select(self.table)
-        df = pl.DataFrame(database_table_data)
-        self.logger.info(f"Data read successfully from {self.table.__tablename__} with {df.shape[0]} records")
+            database_table_data = executor.select(self.source)
+        df = DataFrame(database_table_data)
+        self.logger.info(f"Data read successfully from {self.source.__tablename__} with {df.shape[0]} records")
         return df
