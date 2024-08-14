3b72d2300ce854352c762a63c8d5a14e9747e4a8
matheus <matheuschomem@hotmail.com>
2024-08-14 16:48:50 -0300

    refactor: Update ParquetWriter to use target path with file format extension

--- src/etl/writers/parquet.py
+++ src/etl/writers/parquet.py
@@ -1,15 +1,17 @@
-from src.etl.definitions import Writer, Table
+from src.etl.definitions import Writer
 from polars import DataFrame
 
 
 class ParquetWriter(Writer):
+    FORMAT: str = "parquet"
 
-    def __init__(self, target: Table) -> None:
+    def __init__(self, target: str) -> None:
         super().__init__(target=target)
+        self.path = f"{target}.{self.FORMAT}"
 
     def write_data(self, dataframe: DataFrame) -> None:
-        self.logger.info(f"Starting writing process. PATH: {self.target}")
-        dataframe.write_parquet(self.target)
+        self.logger.info(f"Starting writing process. PATH: {self.path}")
+        dataframe.write_parquet(f"{self.path}")
         self.logger.info(f"Data written successfully with {dataframe.shape[0]} records")
 
         
