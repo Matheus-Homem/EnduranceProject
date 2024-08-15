2503b38cb6bb1fd6289cbedd220167d72a3ece4b
matheus <matheuschomem@hotmail.com>
2024-08-15 08:24:41 -0300

    Refactor DeltaWriter to use polars and deltalake libraries for writing data

--- src/etl/writers/delta.py
+++ src/etl/writers/delta.py
@@ -1,7 +1,15 @@
-from src.etl.definitions import Writer
+from polars import DataFrame
+from deltalake import write_deltalake
+
+from src.etl.definitions import Writer, Path
 
 
 class DeltaWriter(Writer):
 
-    def write_data(self, df):
-        print("Writing to Delta table")
+    def __init__(self, path: Path) -> None:
+        super().__init__(path=path)
+
+    def write_data(self, dataframe: DataFrame) -> None:
+        self.logger.info(f"Starting writing process. FORMAT: Delta | PATH: {self.path}")
+        write_deltalake(self.path, dataframe.to_pandas(), mode="overwrite")
+        self.logger.info(f"Data written successfully with {dataframe.shape[0]} records")
