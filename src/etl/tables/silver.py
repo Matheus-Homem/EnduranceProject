1cc6ff02eef1ee29aea9866a603190b48a13c3a3
matheus <matheuschomem@hotmail.com>
2024-08-15 08:24:34 -0300

    Refactor SilverTable SOURCE_TABLE to SOURCE and add NavigatorTable

--- src/etl/tables/silver.py
+++ src/etl/tables/silver.py
@@ -1,11 +1,27 @@
 from src.etl.definitions import PipelineDefinition, Table
 from src.etl.readers.parquet import ParquetReader
-from src.etl.tables.bronze import BronzeTable
+from src.etl.tables.bronze import BronzeTable, MorningTable, NightTable
 from src.etl.writers.delta import DeltaWriter
+
 
 
 class SilverTable(Table):
     LAYER: str = "silver"
+    FORMAT: str = None
     READER = ParquetReader
     WRITER = DeltaWriter
-    SOURCE_TABLE: BronzeTable
+    SOURCE: BronzeTable
+
+
+class NavigatorTable(SilverTable):
+    SOURCE = NightTable
+    TARGET: str = "navigator"
+
+    def __init__(self):
+        super().__init__()
+
+    def generate_pipeline_properties(self) -> PipelineDefinition:
+        return PipelineDefinition(
+            reader=self.READER(source=self.SOURCE().get_path()),
+            writer=self.WRITER(path=self.get_path()),
+        )
