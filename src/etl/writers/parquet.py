19dad16724b8bcd11768850c1ffe2082a0cca840
matheus <matheuschomem@hotmail.com>
2024-08-15 08:24:49 -0300

    Refactor ParquetWriter class to use Path object for file path

--- src/etl/writers/parquet.py
+++ src/etl/writers/parquet.py
@@ -1,16 +1,14 @@
 from polars import DataFrame
 
-from src.etl.definitions import Writer
+from src.etl.definitions import Writer, Path
 
 
 class ParquetWriter(Writer):
-    FORMAT: str = "parquet"
 
-    def __init__(self, target: str) -> None:
-        super().__init__(target=target)
-        self.path = f"{target}.{self.FORMAT}"
+    def __init__(self, path: Path) -> None:
+        super().__init__(path=path)
 
     def write_data(self, dataframe: DataFrame) -> None:
-        self.logger.info(f"Starting writing process. PATH: {self.path}")
+        self.logger.info(f"Starting writing process. FORMAT: Parquet | PATH: {self.path}")
         dataframe.write_parquet(f"{self.path}")
         self.logger.info(f"Data written successfully with {dataframe.shape[0]} records")
