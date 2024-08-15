3ae4ebdd171b5746cc33dda0b5b7932b2c3a1375
matheus <matheuschomem@hotmail.com>
2024-08-15 08:23:39 -0300

    feat: Add Path type hint to Reader and Writer classes

--- src/etl/definitions.py
+++ src/etl/definitions.py
@@ -1,15 +1,16 @@
 import os
 from abc import ABC, abstractmethod
-from typing import Union
+from typing import Union, NewType
 
 from src.shared.database.tables import MySqlTable
 from src.shared.logger import LoggingManager
 
+Path = NewType('Path', str)
 
 class Reader(ABC):
     def __init__(
         self,
-        source: Union[str, MySqlTable],
+        source: Union[Path, MySqlTable],
         logger_manager=LoggingManager(),
     ) -> None:
         self.source = source
@@ -26,10 +27,10 @@ class Writer(ABC):
 
     def __init__(
         self,
-        target: str,
+        path: Path,
         logger_manager=LoggingManager(),
     ) -> None:
-        self.target = target
+        self.path = path
         self.logger_manager = logger_manager
         self.logger_manager.set_class_name(self.__class__.__name__)
         self.logger = self.logger_manager.get_logger()
@@ -62,7 +63,7 @@ class PipelineDefinition:
         Reader: {type(self.reader).__name__}
         Writer: {type(self.writer).__name__}
         Source: {str(self.reader.source).split('.')[-1][:-2]}
-        Target: {self.writer.target}
+        Target: {self.writer.path}
         """
         )
 
@@ -71,6 +72,8 @@ class Table(ABC):
     FOLDER: str = "data"
     LAYER: str
     TARGET: str
+    FORMAT: str
+    SOURCE: Union["Table", MySqlTable]
     READER: Reader
     WRITER: Writer
 
@@ -81,5 +84,6 @@ class Table(ABC):
     def generate_pipeline_properties(self) -> PipelineDefinition:
         pass
 
-    def get_target_path(self) -> str:
-        return os.path.join(self.FOLDER, self.LAYER, self.TARGET)
+    def get_path(self) -> Path:
+        suffix = f".{self.FORMAT}" if self.FORMAT else ""
+        return Path(os.path.join(self.FOLDER, self.LAYER, self.TARGET) + suffix)
