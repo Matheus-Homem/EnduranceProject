0c6d98e0c81dc546eef3826d2b0ee790bf854b97
matheus <matheuschomem@hotmail.com>
2024-08-17 19:29:31 -0300

    feat: deleted unsued files

--- src/etl/tables/gold.py
+++
@@ -1,7 +0,0 @@
-# from src.etl.definitions import Table
-# from src.etl.readers.parquet import ParquetReader
-# from src.etl.writers.delta import DeltaWriter
-
-
-# class GoldTable(Table):
-#     pass
