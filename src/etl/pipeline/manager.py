a9a42e60763f77449d7c02f54ddaf020837b3519
matheus <matheuschomem@hotmail.com>
2024-08-17 19:36:42 -0300

    refactor: Improve PipelineManager table processing logic

--- src/etl/pipeline/manager.py
+++ src/etl/pipeline/manager.py
@@ -1,5 +1,5 @@
 import os
-from typing import Any
+from typing import List, Tuple, Type
 
 import yaml
 
@@ -7,7 +7,6 @@ from src.etl.definitions import BronzeTable, GoldTable, SilverTable, Table
 from src.etl.pipeline.models import CleanerPipeline, ExtractorPipeline, RefinerPipeline
 from src.shared.database.tables import MySqlMorningTable, MySqlNightTable
 from src.shared.logger import LoggingManager
-from src.shared.utilities.functions import get_class_by_name, log_and_raise_error
 
 
 class PipelineManager:
@@ -22,6 +21,7 @@ class PipelineManager:
             "GoldTable": gold,
         }
         self.has_enabled_layers = any([bronze, silver, gold])
+        self.active_layers = ", ".join([k.replace("Table", "") for k, v in self.table_layer_flags.items() if v])
 
     @staticmethod
     def process_table(table: Table) -> None:
@@ -35,58 +35,77 @@ class PipelineManager:
         if isinstance(table, GoldTable):
             RefinerPipeline.execute(table=table)
 
-    def get_class_or_raise(self, class_name: str, object_list: list, error_message: str) -> Any:
-        retrieved_class = get_class_by_name(class_name, object_list)
-        if retrieved_class is None:
-            log_and_raise_error(
-                error_message=error_message,
-                logger=self.logger,
-                exception=ValueError,
-            )
-        return retrieved_class
-
-    def load_table_data(self) -> dict:
+    @staticmethod
+    def load_table_data() -> dict:
         with open(os.path.join("src", "etl", "tables.yaml"), "r") as file:
             return yaml.safe_load(file)
+
+    def check_enabled_layers(self) -> None:
+        if not self.has_enabled_layers:
+            error_message = "At least one of the parameters 'bronze', 'silver', or 'gold' must be set to True"
+            self.logger.error(error_message)
+            raise ValueError(error_message)
+
+    def get_class_or_raise(self, class_name: str, object_list: List[Type], error_message: str) -> Type:
+        retrieved_class = get_class_by_name(class_name, object_list)
+        if retrieved_class is None:
+            self.logger.error(error_message)
+            raise ValueError(error_message)
+        return retrieved_class
+
+    def identify_table_and_source(self, table_type_str: str, source_str: str) -> Tuple:
+
+        table_type = self.get_class_or_raise(
+            table_type_str,
+            [BronzeTable, SilverTable, GoldTable],
+            f"Name {table_type_str} must be 'BronzeTable', 'SilverTable' or 'GoldTable'",
+        )
+
+        if table_type == BronzeTable:
+            source = self.get_class_or_raise(
+                source_str,
+                [MySqlMorningTable, MySqlNightTable],
+                f"Name {source_str} must be a valid source name",
+            )
+        elif table_type == SilverTable:
+            source = BronzeTable(name=source_str, source=None)
+        elif table_type == GoldTable:
+            source = SilverTable(name=source_str, source=None)
+
+        return table_type, source
 
     def run_pipeline(self) -> None:
 
         processed_tables = []
 
-        if not self.has_enabled_layers:
-            log_and_raise_error(
-                error_message="At least one of the parameters 'bronze', 'silver', or 'gold' must be set to True",
-                logger=self.logger,
-                exception=ValueError,
-            )
+        self.check_enabled_layers()
 
-        data = self.load_table_data()
+        data = PipelineManager.load_table_data()
 
-        self.logger.info("Starting pipeline execution for layers: " + ", ".join([k for k, v in self.table_layer_flags.items() if v]))
+        self.logger.info("Starting pipeline execution for layers: " + self.active_layers)
 
         for table in data["tables"]:
-            self.logger.info(f"Processing table {table['name']} as {table['table_class']} with source {table['source']}")
-            table_class_name, source_name, name = (
-                table["table_class"],
+            table_type_str, source_str, name = (
+                table["table_type"],
                 table["source"],
                 table["name"],
             )
 
-            table_class = self.get_class_or_raise(
-                table_class_name,
-                [BronzeTable, SilverTable, GoldTable],
-                f"Name {table_class_name} must be 'BronzeTable', 'SilverTable' or 'GoldTable'",
-            )
-            source = self.get_class_or_raise(
-                source_name,
-                [MySqlMorningTable, MySqlNightTable],
-                f"Name {source_name} must be a valid source name",
-            )
+            self.logger.info(f"Trying to process table '{name}' as {table_type_str} with source {source_str}")
 
-            if self.table_layer_flags.get(table_class_name, False):
+            if self.table_layer_flags.get(table_type_str, False):
 
-                PipelineManager.process_table(table_class(source=source, name=name))
-                self.logger.info(f"Table {table['name']} processed successfully.")
+                table_type, source = self.identify_table_and_source(table_type_str, source_str)
+
+                self.logger.info(f"Starting '{name}' table execution.")
+                PipelineManager.process_table(table_type(source=source, name=name))
+                self.logger.info(f"Finished '{name}' table execution.")
                 processed_tables.append(table["name"])
+            else:
+                self.logger.info(f"Table '{name}' was not processed because layer {table_type_str} is not enabled.")
 
         self.logger.info(f"Pipeline execution completed. Executed {len(processed_tables)} tables.")
+
+
+def get_class_by_name(class_name: str, object_list: list):
+    return next((cls for cls in object_list if cls.__name__ == class_name), None)
