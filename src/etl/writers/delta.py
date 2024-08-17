f7deef31d342a00e4e86accda4b1b6b369f12dd9
matheus <matheuschomem@hotmail.com>
2024-08-16 22:07:55 -0300

    refactor: Update DeltaWriter to use write_dataframe instead of write_data

--- src/etl/writers/delta.py
+++ src/etl/writers/delta.py
@@ -1,7 +1,7 @@
-from polars import DataFrame
 from deltalake import write_deltalake
+from polars import DataFrame
 
-from src.etl.definitions import Writer, Path
+from src.etl.definitions import Path, Writer
 
 
 class DeltaWriter(Writer):
@@ -9,7 +9,7 @@ class DeltaWriter(Writer):
     def __init__(self, path: Path) -> None:
         super().__init__(path=path)
 
-    def write_data(self, dataframe: DataFrame) -> None:
+    def write_dataframe(self, dataframe: DataFrame) -> None:
         self.logger.info(f"Starting writing process. FORMAT: Delta | PATH: {self.path}")
         write_deltalake(self.path, dataframe.to_pandas(), mode="overwrite")
         self.logger.info(f"Data written successfully with {dataframe.shape[0]} records")
