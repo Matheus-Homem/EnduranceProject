1df327ecbff8cc77e215c29866ebe8bca8624541
matheus <matheuschomem@hotmail.com>
2024-08-16 22:05:06 -0300

    Refactor execute_pipeline function to use Reader and Writer classes

--- src/etl/execute.py
+++
@@ -1,9 +0,0 @@
-from src.etl.definitions import PipelineDefinition
-
-
-def execute_pipeline(pipeline_definitions: PipelineDefinition):
-    reader = pipeline_definitions.get_reader()
-    writer = pipeline_definitions.get_writer()
-
-    df = reader.read_data()
-    writer.write_data(df)
