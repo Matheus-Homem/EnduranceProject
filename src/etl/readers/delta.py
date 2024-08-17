63b6c96cccea6e822c9ac30c1b6d09bad837387c
matheus <matheuschomem@hotmail.com>
2024-08-16 22:07:22 -0300

    refactor: DeltaReader to rename read_data method to read_dataframe

--- src/etl/readers/delta.py
+++ src/etl/readers/delta.py
@@ -3,6 +3,6 @@ from src.etl.definitions import Reader
 
 class DeltaReader(Reader):
 
-    def read_data(self):
+    def read_dataframe(self):
         print("Reading from Delta table")
         return None
