225b00c3522835fe6ea74045a1ab0376010c2b0d
matheus <matheuschomem@hotmail.com>
2024-08-17 19:37:15 -0300

    refactor: Update DeltaWriter to accept path as parameter in write_dataframe method

--- src/etl/writers/delta.py
+++ src/etl/writers/delta.py
@@ -6,10 +6,10 @@ from src.etl.definitions import Path, Writer
 
 class DeltaWriter(Writer):
 
-    def __init__(self, path: Path) -> None:
-        super().__init__(path=path)
+    def __init__(self) -> None:
+        super().__init__()
 
-    def write_dataframe(self, dataframe: DataFrame) -> None:
-        self.logger.info(f"Starting writing process. FORMAT: Delta | PATH: {self.path}")
-        write_deltalake(self.path, dataframe.to_pandas(), mode="overwrite")
+    def write_dataframe(self, dataframe: DataFrame, path: Path) -> None:
+        self.logger.info(f"Starting writing process. FORMAT: Delta | PATH: {path}")
+        write_deltalake(path, dataframe.to_pandas(), mode="overwrite")
         self.logger.info(f"Data written successfully with {dataframe.shape[0]} records")
