781bd314307ceb7ce997dba1f34e3a352d18614d
matheus <matheuschomem@hotmail.com>
2024-08-17 18:25:19 -0300

    feat: add log_and_raise_error function

--- src/shared/utilities/functions.py
+++ src/shared/utilities/functions.py
@@ -1,2 +1,10 @@
-def get_class_by_name(name, class_list):
-    return next((cls for cls in class_list if cls.__name__ == name), None)
+from logging import Logger
+
+
+def get_class_by_name(class_name: str, object_list: list):
+    return next((cls for cls in object_list if cls.__name__ == class_name), None)
+
+
+def log_and_raise_error(logger: Logger, error_message: str, exception: Exception):
+    logger.error(error_message)
+    raise exception(error_message)
