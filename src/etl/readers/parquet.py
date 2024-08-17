bea28c79c4cc2010c22f47ebdd5335417157c68e
matheus <matheuschomem@hotmail.com>
2024-08-17 19:37:09 -0300

    refactor: Update ParquetReader to accept source path as parameter

--- src/etl/readers/parquet.py
+++ src/etl/readers/parquet.py
@@ -5,11 +5,11 @@ from src.etl.definitions import Path, Reader
 
 class ParquetReader(Reader):
 
-    def __init__(self, source: Path) -> None:
-        super().__init__(source=source)
+    def __init__(self) -> None:
+        super().__init__()
 
-    def read_dataframe(self) -> DataFrame:
-        self.logger.info(f"Starting reading process. FORMAT: Parquet | TARGET: {self.source}")
-        df = read_parquet(self.source)
-        self.logger.info(f"Data read successfully from {self.source} with {df.shape[0]} records")
+    def read_dataframe(self, source: Path) -> DataFrame:
+        self.logger.info(f"Starting reading process. FORMAT: Parquet | TARGET: {source}")
+        df = read_parquet(source)
+        self.logger.info(f"Data read successfully from {source} with {df.shape[0]} records")
         return df
