0bd1e16e80b144473385bde4837b9d3287cb6001
matheus <matheuschomem@hotmail.com>
2024-08-16 22:07:42 -0300

    refactor: Simplify GoldTable class by removing unused attributes and imports

--- src/etl/tables/gold.py
+++ src/etl/tables/gold.py
@@ -1,10 +1,7 @@
-from src.etl.definitions import PipelineDefinition, Table
+from src.etl.definitions import Table
 from src.etl.readers.parquet import ParquetReader
 from src.etl.writers.delta import DeltaWriter
 
 
 class GoldTable(Table):
-    LAYER: str = "gold"
-    READER = ParquetReader
-    WRITER = DeltaWriter
-    SOURCE: Table
+    pass
