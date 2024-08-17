523383038844721f4000f51c16fc5bd32b7c4cd5
matheus <matheuschomem@hotmail.com>
2024-08-16 22:07:03 -0300

    refactor: DatabaseReader to remove unnecessary source parameter in read_dataframe method

--- src/etl/readers/database.py
+++ src/etl/readers/database.py
@@ -7,17 +7,17 @@ from src.shared.database.tables import MySqlTable
 
 class DatabaseReader(Reader):
 
-    def __init__(self, source: MySqlTable) -> None:
-        super().__init__(source=source)
+    def __init__(self) -> None:
+        super().__init__()
 
-    def read_data(self) -> DataFrame:
+    def read_dataframe(self, source: MySqlTable) -> DataFrame:
         self.logger.info(
-            f"Starting reading process. SOURCE: Database | TABLE: {self.source.__tablename__}"
+            f"Starting reading process. SOURCE: Database | TABLE: {source.__tablename__}"
         )
         with DatabaseExecutorBuilder() as executor:
-            database_table_data = executor.select(self.source)
+            database_table_data = executor.select(source)
         df = DataFrame(database_table_data)
         self.logger.info(
-            f"Data read successfully from {self.source.__tablename__} with {df.shape[0]} records"
+            f"Data read successfully from {source.__tablename__} with {df.shape[0]} records"
         )
         return df
