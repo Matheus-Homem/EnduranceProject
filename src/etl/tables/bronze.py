f4eda704f4049b450fac0cf5c611e9acab89eb6e
matheus <matheuschomem@hotmail.com>
2024-08-16 22:07:36 -0300

    refactor: Update BronzeTable to use source instead of SOURCE

--- src/etl/tables/bronze.py
+++ src/etl/tables/bronze.py
@@ -1,40 +1,18 @@
-from src.etl.definitions import PipelineDefinition, Table
-from src.etl.readers.database import DatabaseReader
-from src.etl.writers.parquet import ParquetWriter
+from src.etl.definitions import Table
 from src.shared.database.tables import MySqlMorningTable, MySqlNightTable, MySqlTable
 
 
 class BronzeTable(Table):
-    LAYER: str = "bronze"
-    FORMAT: str = "parquet"
-    READER = DatabaseReader
-    WRITER = ParquetWriter
-    SOURCE: MySqlTable
+    source: MySqlTable
+    layer: str = "bronze"
+    format: str = "parquet"
 
 
-class MorningTable(BronzeTable):
-    SOURCE = MySqlMorningTable
-    TARGET: str = "morning_raw"
-
-    def __init__(self):
-        super().__init__()
-
-    def generate_pipeline_properties(self) -> PipelineDefinition:
-        return PipelineDefinition(
-            reader=self.READER(source=self.SOURCE),
-            writer=self.WRITER(path=self.get_path()),
-        )
+class MorningBronzeTable(BronzeTable):
+    source = MySqlMorningTable
+    name = "morning_raw"
 
 
-class NightTable(BronzeTable):
-    SOURCE = MySqlNightTable
-    TARGET: str = "night_raw"
-
-    def __init__(self):
-        super().__init__()
-
-    def generate_pipeline_properties(self) -> PipelineDefinition:
-        return PipelineDefinition(
-            reader=self.READER(source=self.SOURCE),
-            writer=self.WRITER(path=self.get_path()),
-        )
+class NightBronzeTable(BronzeTable):
+    source = MySqlNightTable
+    name = "night_raw"
