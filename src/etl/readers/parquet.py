e3bd16c357c9857a0fb714dc036e16c975257c2d
matheus <matheuschomem@hotmail.com>
2024-08-25 20:47:23 -0300

    refactor: improve logging message in ParquetReader

--- src/etl/readers/parquet.py
+++ src/etl/readers/parquet.py
@@ -9,7 +9,6 @@ class ParquetReader(Reader):
         super().__init__()
 
     def read_dataframe(self, source: Path) -> DataFrame:
-        self.logger.info(f"Starting reading process. FORMAT: Parquet | TARGET: {source}")
+        self.logger.info(f"Reading data from {source} using Parquet format")
         df = read_parquet(source)
-        self.logger.info(f"Data read successfully from {source} with {df.shape[0]} records")
         return df
