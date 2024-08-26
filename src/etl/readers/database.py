03fd8fe7af7e9eee75d64753b068ab435c7b3ce7
matheus <matheuschomem@hotmail.com>
2024-08-26 08:04:59 -0300

    refactor: Update superclass initialization in DatabaseReader

--- src/etl/readers/database.py
+++ src/etl/readers/database.py
@@ -8,7 +8,7 @@ from src.shared.database.tables import MySqlTable
 class DatabaseReader(Reader):
 
     def __init__(self) -> None:
-        super().__init__()
+        super().__init__(class_name=self.__class__.__name__)
 
     def read_dataframe(self, source: MySqlTable) -> DataFrame:
         self.logger.info(f"Reading table {source.__tablename__} from Database")
