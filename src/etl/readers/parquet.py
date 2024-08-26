edfe8277f413a82c451776c1e35d0bbf989252fd
matheus <matheuschomem@hotmail.com>
2024-08-26 08:05:19 -0300

    refactor: Update superclass initialization in DatabaseReader

--- src/etl/readers/parquet.py
+++ src/etl/readers/parquet.py
@@ -6,7 +6,7 @@ from src.etl.definitions import Path, Reader
 class ParquetReader(Reader):
 
     def __init__(self) -> None:
-        super().__init__()
+        super().__init__(class_name=self.__class__.__name__)
 
     def read_dataframe(self, source: Path) -> DataFrame:
         self.logger.info(f"Reading data from {source} using Parquet format")
