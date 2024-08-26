6d172d8fc41fdaae65e26356bb43fdeb5ebbbdd0
matheus <matheuschomem@hotmail.com>
2024-08-26 08:04:16 -0300

    refactor: Improve column formatting in CleanerFormatter

--- src/etl/engines/cleaner/tools.py
+++ src/etl/engines/cleaner/tools.py
@@ -1,18 +1,20 @@
+from typing import Literal, Tuple
+
 from polars import DataFrame, col
-from typing import Tuple, Literal
-from src.shared.logger import LoggingManager
 
-class CleanerTools:
+from src.etl.definitions import Engine
 
-    def __init__(self, logger_manager=LoggingManager()) -> None:
-        logger_manager.set_class_name(self.__class__.__name__)
-        self.logger = logger_manager.get_logger()
+
+class CleanerTools(Engine):
+
+    def __init__(self) -> None:
+        super().__init__(class_name=self.__class__.__name__)
 
     def unnest_expanded_data(self, df: DataFrame, json_column: str) -> DataFrame:
         self.logger.info(f"Unnesting expanded data from column: {json_column}")
         df_expanded = df.with_columns(col(json_column).str.json_decode().alias("data_expanded")).unnest("data_expanded")
         return df_expanded
-    
+
     def drop_columns(self, df: DataFrame, columns: list) -> DataFrame:
         self.logger.info(f"Dropping columns: {columns}")
         df = df.drop(columns)
@@ -20,7 +22,7 @@ class CleanerTools:
 
     def get_datetime_cols(self, source_type: Literal["morning", "night"]) -> Tuple[str, str, str]:
         return f"date_header_{source_type}Date", f"time_header_{source_type}Time", f"datetime_header_{source_type}"
-    
+
     def partitionate_dataframe(self, df: DataFrame, partition_cols: list) -> DataFrame:
         self.logger.info(f"Partitionating dataframe by columns: {partition_cols}")
         df = df.partition_by(partition_cols)
