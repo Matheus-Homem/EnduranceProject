2f3de1e328cff4c1d23bf5ce475f966f2e53efb6
matheus <matheuschomem@hotmail.com>
2024-08-26 08:06:13 -0300

    refactor: Add LoggingManager for centralized logging configuration

---
+++ src/shared/logging/manager.py
@@ -0,0 +1,38 @@
+import logging
+from src.shared.logging.custom_formattter import CustomColoredFormatter
+import colorlog
+
+SUCCESS_LEVEL_NUM = 25
+logging.addLevelName(SUCCESS_LEVEL_NUM, "SUCCESS")
+
+def success(self, message, *args, **kwargs):
+    if self.isEnabledFor(SUCCESS_LEVEL_NUM):
+        self._log(SUCCESS_LEVEL_NUM, message, args, **kwargs)
+
+logging.Logger.success = success
+
+class LoggingManager:
+    def __init__(self):
+        logger = logging.getLogger()
+        logger.setLevel(logging.DEBUG)
+        
+        for handler in logger.handlers[:]:
+            logger.removeHandler(handler)
+        
+        self._add_console_handler(logger) 
+
+    def _add_console_handler(self, logger: logging.Logger) -> None:
+        formatter = CustomColoredFormatter(
+            "%(log_color)s%(asctime)s - %(pathname)s - %(funcName)s - %(levelname)s - %(message)s",
+            log_colors={
+                "DEBUG": "purple",
+                "INFO": "cyan",
+                "SUCCESS": "green",
+                "WARNING": "yellow",
+                "ERROR": "red",
+                "CRITICAL": "bold_red",
+            },
+        )
+        console_handler = colorlog.StreamHandler()
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
