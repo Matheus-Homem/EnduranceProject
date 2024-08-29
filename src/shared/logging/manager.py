8744a79a598637d751289ec11ae7e77dd807c66b
matheus <matheuschomem@hotmail.com>
2024-08-29 08:07:51 -0300

    chore: black

--- src/shared/logging/manager.py
+++ src/shared/logging/manager.py
@@ -1,25 +1,30 @@
 import logging
-from src.shared.logging.custom_formattter import CustomColoredFormatter
+
 import colorlog
+
+from src.shared.logging.custom_formattter import CustomColoredFormatter
 
 SUCCESS_LEVEL_NUM = 25
 logging.addLevelName(SUCCESS_LEVEL_NUM, "SUCCESS")
+
 
 def success(self, message, *args, **kwargs):
     if self.isEnabledFor(SUCCESS_LEVEL_NUM):
         self._log(SUCCESS_LEVEL_NUM, message, args, **kwargs)
 
+
 logging.Logger.success = success
+
 
 class LoggingManager:
     def __init__(self):
         logger = logging.getLogger()
         logger.setLevel(logging.DEBUG)
-        
+
         for handler in logger.handlers[:]:
             logger.removeHandler(handler)
-        
-        self._add_console_handler(logger) 
+
+        self._add_console_handler(logger)
 
     def _add_console_handler(self, logger: logging.Logger) -> None:
         formatter = CustomColoredFormatter(
