3b2f3f2b0a61eff4438af0925ba07b5f2be46f6d
matheus <matheuschomem@hotmail.com>
2024-09-13 07:21:47 -0300

    feat: Update logging manager to filter out empty log messages

--- src/shared/logging/custom_formattter.py
+++ src/shared/logging/custom_formattter.py
@@ -1,14 +1,15 @@
 import os
-
 import colorlog
-
 
 class CustomColoredFormatter(colorlog.ColoredFormatter):
     def format(self, record):
         src_path = os.path.join("src", "")
         src_index = record.pathname.lower().find(src_path)
-        if src_index != -1:
-            record.pathname = record.pathname[src_index:]
+        
+        if src_index == -1:
+            return ""
+        
+        record.pathname = record.pathname[src_index:]
 
         if hasattr(record, "funcName") and hasattr(record, "module"):
             class_name = record.name
