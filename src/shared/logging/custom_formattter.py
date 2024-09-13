c1b5d88500c3e2790d926e4bec1c69e0810b85aa
matheus <matheuschomem@hotmail.com>
2024-09-13 14:11:27 -0300

    refactor: Improve formatting and organization in custom_formatter.py

--- src/shared/logging/custom_formattter.py
+++ src/shared/logging/custom_formattter.py
@@ -1,14 +1,16 @@
 import os
+
 import colorlog
+
 
 class CustomColoredFormatter(colorlog.ColoredFormatter):
     def format(self, record):
         src_path = os.path.join("src", "")
         src_index = record.pathname.lower().find(src_path)
-        
+
         if src_index == -1:
             return ""
-        
+
         record.pathname = record.pathname[src_index:]
 
         if hasattr(record, "funcName") and hasattr(record, "module"):
