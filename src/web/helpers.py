962fa63ce4af07d5690830f3fe0f12ca2fcaee3d
matheus <matheuschomem@hotmail.com>
2024-09-13 08:30:02 -0300

    refactor: Add date conversion function to helpers.py

--- src/web/helpers.py
+++ src/web/helpers.py
@@ -1,10 +1,15 @@
 from src.shared.utilities.text import TextUtilities
+from datetime import date
 
 
 def clean_and_serialize_dict(data: dict) -> str:
     cleaned_dict = {TextUtilities.clean_string(key): TextUtilities.clean_string(str(value)) for key, value in data.items()}
     serialized_dict = TextUtilities.serialize_dict(cleaned_dict)
     return serialized_dict
+
+    
+def convert_input_date(date_to_convert: str) -> date:
+    return date.fromisoformat(date_to_convert)
 
 
 def print_green(text: str):
