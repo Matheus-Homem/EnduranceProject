a62543a8a6294962545eaef114086393a2662c88
matheus <matheuschomem@hotmail.com>
2024-09-18 13:51:38 -0300

    refactor: rename test_logging_manager.py and improve LoggingManager tests

--- tests/shared/logging/test_logging_manager.py
+++ tests/shared/logging/test_logging_manager.py
@@ -2,10 +2,7 @@ import logging
 import unittest
 from unittest.mock import MagicMock, patch
 
-import colorlog
-
-from src.shared.logging.custom_formatter import CustomColoredFormatter
-from src.shared.logging.manager import LoggingManager
+from src.shared.logging.manager import SUCCESS_LEVEL_NUM, LoggingManager
 
 
 class TestLoggingManager(unittest.TestCase):
@@ -13,7 +10,6 @@ class TestLoggingManager(unittest.TestCase):
     @patch("src.shared.logging.manager.colorlog.StreamHandler")
     @patch("src.shared.logging.manager.CustomColoredFormatter")
     def test_add_console_handler(self, mock_colored_formatter, mock_stream_handler):
-        mock_logger = MagicMock(spec=logging.Logger)
 
         LoggingManager()
 
@@ -32,11 +28,9 @@ class TestLoggingManager(unittest.TestCase):
         mock_stream_handler_instance = mock_stream_handler.return_value
         mock_stream_handler_instance.setFormatter.assert_called_once_with(mock_colored_formatter.return_value)
 
-        # mock_logger.addHandler.assert_called_once_with(mock_stream_handler_instance)
-
     @patch("src.shared.logging.manager.logging.getLogger")
     @patch.object(LoggingManager, "_add_console_handler")
-    def test_initialize_logger_adds_handlers(self, mock_add_console_handler, mock_get_logger):
+    def test_initialize_logger(self, mock_add_console_handler, mock_get_logger):
         mock_logger = MagicMock()
         mock_logger.hasHandlers.return_value = False
         mock_get_logger.return_value = mock_logger
@@ -45,17 +39,6 @@ class TestLoggingManager(unittest.TestCase):
 
         mock_add_console_handler.assert_called_once_with(mock_logger)
 
-    # @patch("src.shared.logging.manager.logging.getLogger")
-    # @patch.object(LoggingManager, "_add_console_handler")
-    # def test_initialize_logger_does_not_add_handlers(self, mock_add_console_handler, mock_get_logger):
-    #     mock_logger = MagicMock()
-    #     mock_logger.hasHandlers.return_value = True
-    #     mock_get_logger.return_value = mock_logger
-
-    #     LoggingManager()
-
-    #     self.assertEqual(len(mock_logger.handlers), )
-
     def test_success_logging(self):
         LoggingManager()
         logger = logging.getLogger()
@@ -63,6 +46,15 @@ class TestLoggingManager(unittest.TestCase):
         with patch.object(logger, "success") as mock_success:
             logger.success("This is a success message")
             mock_success.assert_called_once_with("This is a success message")
+
+    def test_success_level(self):
+        self.assertEqual(logging.getLevelName(SUCCESS_LEVEL_NUM), "SUCCESS")
+
+    def test_success_method(self):
+        logger = logging.getLogger()
+        with patch.object(logger, "_log") as mock_log:
+            logger.success("Test success message")
+            mock_log.assert_called_once_with(SUCCESS_LEVEL_NUM, "Test success message", ())
 
 
 if __name__ == "__main__":
