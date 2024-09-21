265c39ba22ef8685f98c3397e6ac447a4f817550
matheus <matheuschomem@hotmail.com>
2024-09-21 14:44:50 -0300

    refactor: remove WebUtils class and its methods for cleaner code structure

--- src/web/helpers.py
+++ src/web/helpers.py
@@ -1,37 +1,3 @@
-import re
-from datetime import date
-
-from src.shared.utilities.text import TextUtilities
-
-
-class WebUtils:
-
-    @staticmethod
-    def clean_and_serialize_dict(data: dict) -> str:
-        cleaned_dict = {TextUtilities.clean_string(key): TextUtilities.clean_string(str(value)) for key, value in data.items()}
-        serialized_dict = TextUtilities.serialize_dict(cleaned_dict)
-        return serialized_dict
-
-    @staticmethod
-    def convert_input_date(date_to_convert: str) -> date:
-        return date.fromisoformat(date_to_convert)
-
-    @staticmethod
-    def is_valid_password(password) -> bool:
-        if len(password) < 8:
-            return False
-        if not re.search(r"\d", password):
-            return False
-        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
-            return False
-        return True
-
-    @staticmethod
-    def is_valid_email(email) -> bool:
-        email_regex = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-        return re.match(email_regex, email) is not None
-
-
 class SimpleMessagePrinter:
 
     @staticmethod
