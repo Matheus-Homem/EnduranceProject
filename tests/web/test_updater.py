8573aa2f615e33d8dd10983d636d0c2085410672
matheus <matheuschomem@hotmail.com>
2024-09-21 14:46:29 -0300

    test: add unit tests for StringUtils and ElementSchemasController

---
+++ tests/web/test_updater.py
@@ -0,0 +1,25 @@
+from src.web.updater import ElementSchemasController
+import unittest
+
+class TestElementSchemasController(unittest.TestCase):
+    def test_read_html_files(self):
+        directory = 'src/web/templates/core'
+        controller = ElementSchemasController()
+
+        # Act
+        result = controller.read_html_files(directory)
+
+        print(list(result.keys()))
+        assert result.get('alchemist').get("fields") == ['bool_anger','bool_anxiety','bool_disheartenment','bool_fear','bool_frustration','bool_sadness','date_input','string_vent']
+
+    def test_update_element_schemas(self):
+        controller = ElementSchemasController()
+
+        # Act
+        result = controller.update_element_schemas()
+
+        assert result == None
+    
+
+if __name__ == '__main__':
+    unittest.main()
