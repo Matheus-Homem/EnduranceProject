fa3f7f149abffd4b2bd51f920be17da9f814ed9b
matheus <matheuschomem@hotmail.com>
2024-09-22 12:16:56 -0300

    refactor: enhance MySqlTable with unique constraint handling and schema hash generation

--- src/shared/database/tables.py
+++ src/shared/database/tables.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from sqlalchemy import (
     Column,
     Date,
@@ -10,15 +12,27 @@ from sqlalchemy import (
     UniqueConstraint,
 )
 from sqlalchemy.orm import declarative_base
-from src.shared.utils import DateUtils
+
+from src.shared.utils import DateUtils, HashUtils
 
 Base = declarative_base(metadata=MetaData())
 
+
 class MySqlTable(Base):
     __abstract__ = True
+    __unique_constraint_name__ = None
+
+    @classmethod
+    def get_unique_constraint_name(cls) -> str:
+        return cls.__unique_constraint_name__
+
+    @staticmethod
+    def get_schema_hash(schema_fields: List[str]) -> str:
+        fields_as_string = ",".join(schema_fields)
+        return HashUtils.string_to_sha256(fields_as_string)
 
 
-class User(MySqlTable):
+class Users(MySqlTable):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -38,16 +52,12 @@ class ElementEntries(MySqlTable):
     element_category = Column(String(255), nullable=False)
     element_name = Column(String(255), nullable=False)
     element_string = Column(Text, nullable=False)
-    schema_version = Column(Integer, nullable=False)
+    schema_hash = Column(String(64), nullable=False)
     op = Column(Enum("c", "u", "d"), nullable=False, default="c")
     created_at = Column(DateTime, nullable=False, default=DateUtils.current_brasilia_sp_time)
     updated_at = Column(DateTime, nullable=False, default=DateUtils.current_brasilia_sp_time, onupdate=DateUtils.current_brasilia_sp_time)
 
     __table_args__ = (UniqueConstraint("date", "user_id", "element_category", "element_name", name=__unique_constraint_name__),)
-
-    @classmethod
-    def get_unique_constraint_name(cls):
-        return cls.__unique_constraint_name__
 
 
 class ElementSchemas(MySqlTable):
@@ -57,14 +67,12 @@ class ElementSchemas(MySqlTable):
     id = Column(Integer, primary_key=True, autoincrement=True)
     element_category = Column(String(255), nullable=False)
     element_name = Column(String(255), nullable=False)
-    schema_version = Column(String(64), nullable=False)
-    schema_definition = Column(Text, nullable=False)
+    schema_version = Column(Integer, nullable=False)
+    schema_hash = Column(String(64), nullable=False)
+    schema_fields = Column(Text, nullable=False)
+    schema_dtypes = Column(Text, nullable=False)
     op = Column(Enum("c", "u", "d"), nullable=False, default="c")
     created_at = Column(DateTime, nullable=False, default=DateUtils.current_brasilia_sp_time)
     updated_at = Column(DateTime, nullable=False, default=DateUtils.current_brasilia_sp_time, onupdate=DateUtils.current_brasilia_sp_time)
 
     __table_args__ = (UniqueConstraint("element_category", "element_name", "schema_version", name=__unique_constraint_name__),)
-
-    @classmethod
-    def get_unique_constraint_name(cls):
-        return cls.__unique_constraint_name__
