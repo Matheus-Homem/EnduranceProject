583a934c4c36a494984ccdf0f5f89bf4747353c0
matheus <matheuschomem@hotmail.com>
2024-09-23 15:14:26 -0300

    refactor: update test_utils to use DictUtils and HashUtils, and enhance validation tests

--- tests/shared/test_utils.py
+++ tests/shared/test_utils.py
@@ -1,16 +1,24 @@
+import hashlib
 import unittest
+from datetime import date, datetime
 
-from src.shared.utils import DateUtils, DictUtils, StringUtils, ValidationUtils
+from src.shared.utils import (
+    DateUtils,
+    DictUtils,
+    HashUtils,
+    StringUtils,
+    ValidationUtils,
+)
 
 
 class TestStringUtils(unittest.TestCase):
 
     def test_remove_newlines(self):
-        self.assertEqual(StringUtils.remove_newlines("Hello\r\nWorld"), "HelloWorld")
-        self.assertEqual(StringUtils.remove_newlines("NoNewLine"), "NoNewLine")
-        self.assertEqual(StringUtils.remove_newlines("Line1\r\n Line2\r\n"), "Line1 Line2")
-        self.assertEqual(StringUtils.remove_newlines(" "), " ")
-        self.assertEqual(StringUtils.remove_newlines("\r\n"), "")
+        self.assertEqual(StringUtils.remove_linebreaks("Hello\r\nWorld"), "HelloWorld")
+        self.assertEqual(StringUtils.remove_linebreaks("NoNewLine"), "NoNewLine")
+        self.assertEqual(StringUtils.remove_linebreaks("Line1\r\n Line2\r\n"), "Line1 Line2")
+        self.assertEqual(StringUtils.remove_linebreaks(" "), " ")
+        self.assertEqual(StringUtils.remove_linebreaks("\r\n"), "")
 
     def test_remove_special_characters(self):
         cleaned_string = StringUtils.remove_special_characters("áàâãéêíóôõúüçÁÀÂÃÉÊÍÓÔÕÚÜÇ")
@@ -22,6 +30,9 @@ class TestStringUtils(unittest.TestCase):
     def test_clean_string(self):
         self.assertEqual(StringUtils.clean_string("João\r\n Dançarino"), "Joao Dancarino")
 
+
+class TestDictUtils(unittest.TestCase):
+
     def test_serialize_dict(self):
         data = {
             "name": "JOAO DANCARINO",
@@ -30,11 +41,53 @@ class TestStringUtils(unittest.TestCase):
             "number": "123",
         }
         expected_output = '{"name": "JOAO DANCARINO", "email": "john.doe@example.com", "address": "Rua do Acai", "number": "123"}'
-        self.assertEqual(StringUtils.serialize_dict(data), expected_output)
+        self.assertEqual(DictUtils.serialize_dict(data), expected_output)
 
         data_empty = {}
         expected_output_empty = "{}"
-        self.assertEqual(StringUtils.serialize_dict(data_empty), expected_output_empty)
+        self.assertEqual(DictUtils.serialize_dict(data_empty), expected_output_empty)
+
+    def test_clean_and_serialize_dict(self):
+        input_dict = {"Café\n": "Morn\ning"}
+        expected_output = '{"Cafe": "Morning"}'
+        self.assertEqual(DictUtils.clean_and_serialize_dict(input_dict), expected_output)
+
+
+class TestValidationUtils(unittest.TestCase):
+
+    def test_is_valid_password(self):
+        self.assertTrue(ValidationUtils.is_valid_password("Password1!"))
+        self.assertFalse(ValidationUtils.is_valid_password("short"))
+        self.assertFalse(ValidationUtils.is_valid_password("n0specialchar"))
+        self.assertFalse(ValidationUtils.is_valid_password("NoNumber!"))
+
+    def test_is_valid_email(self):
+        self.assertTrue(ValidationUtils.is_valid_email("test@example.com"))
+        self.assertFalse(ValidationUtils.is_valid_email("invalid-email"))
+
+    def test_is_list_of_strings(self):
+        self.assertTrue(ValidationUtils.is_list_of_strings(["a", "b", "c"]))
+        self.assertFalse(ValidationUtils.is_list_of_strings(["a", 1, "c"]))
+        self.assertFalse(ValidationUtils.is_list_of_strings("not a list"))
+        self.assertFalse(ValidationUtils.is_list_of_strings([1, 2, 3]))
+
+
+class TestDateUtils(unittest.TestCase):
+
+    def test_convert_date_input(self):
+        self.assertEqual(DateUtils.convert_date_input("2023-01-01"), date(2023, 1, 1))
+
+    def test_current_brasilia_sp_time(self):
+        current_time = DateUtils.current_brasilia_sp_time()
+        self.assertIsInstance(current_time, datetime)
+        self.assertEqual(current_time.tzinfo.zone, "America/Sao_Paulo")
+
+
+class TestHashUtils(unittest.TestCase):
+
+    def test_string_to_sha256(self):
+        expected_result = hashlib.sha256("test".encode("utf-8")).hexdigest()
+        self.assertEqual(HashUtils.string_to_sha256("test"), expected_result)
 
 
 if __name__ == "__main__":
