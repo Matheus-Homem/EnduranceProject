f5b8058897bb9590a53357c0d2f3f04993511165
matheus <matheuschomem@hotmail.com>
2024-09-29 07:08:25 -0300

    feat: add daily_control table and update schema for element_entries and element_schemas

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -1,30 +1,42 @@
+CREATE TABLE `daily_control` (
+  `entry_date` date NOT NULL,
+  `user_id` varchar(36) NOT NULL,
+  `element_category` varchar(255) NOT NULL,
+  `element_name` varchar(255) NOT NULL,
+  `has_data` tinyint(1) NOT NULL,
+  PRIMARY KEY (`entry_date`,`user_id`,`element_category`,`element_name`),
+  UNIQUE KEY `_control_uc` (`entry_date`,`user_id`,`element_category`,`element_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
 CREATE TABLE `element_entries` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `date` date NOT NULL,
+  `entry_date` date NOT NULL,
   `user_id` varchar(36) NOT NULL,
   `element_category` varchar(255) NOT NULL,
   `element_name` varchar(255) NOT NULL,
   `element_string` text NOT NULL,
-  `schema_version` int NOT NULL,
+  `schema_encoded` text NOT NULL,
   `op` enum('c','u','d') NOT NULL DEFAULT 'c',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `_entry_uc` (`date`,`user_id`,`element_category`,`element_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb3;
+  UNIQUE KEY `_entry_uc` (`entry_date`,`user_id`,`element_category`,`element_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8mb3;
 
 CREATE TABLE `element_schemas` (
   `id` int NOT NULL AUTO_INCREMENT,
   `element_category` varchar(255) NOT NULL,
   `element_name` varchar(255) NOT NULL,
-  `schema_version` varchar(64) NOT NULL,
-  `schema_definition` text NOT NULL,
+  `schema_version` int NOT NULL,
+  `schema_encoded` text NOT NULL,
+  `schema_fields` text NOT NULL,
+  `schema_dtypes` text NOT NULL,
   `op` enum('c','u','d') NOT NULL DEFAULT 'c',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `_schema_uc` (`element_category`,`element_name`,`schema_version`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb3;
 
 CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
