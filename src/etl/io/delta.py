a0df18ff37bc0fef3ce915423fc6a8c61fd9b4f9
matheus <matheuschomem@hotmail.com>
2024-10-04 06:40:59 -0300

    feat: enhance DeltaHandler initialization and improve logging for read/write operations

--- src/etl/io/delta.py
+++ src/etl/io/delta.py
@@ -7,10 +7,12 @@ from src.etl.ports import IOHandler, PandasDF
 class DeltaHandler(IOHandler):
 
     def __init__(self, layer: Layer):
-        super().__init__(layer)
+        super().__init__(layer=layer, class_name=__class__.__name__)
 
     def read(self) -> PandasDF:
+        self.logger.info(f"Reading Delta Lake file from {self.path}")
         return DeltaTable(self.path).to_pandas()
 
     def write(self, table: PandasDF) -> None:
-        write_deltalake(self.path, table, mode="overwrite")
+        self.logger.info(f"Writing Delta Lake file to {self.path}")
+        write_deltalake(self.path, table, mode="overwrite", schema_mode="overwrite")
