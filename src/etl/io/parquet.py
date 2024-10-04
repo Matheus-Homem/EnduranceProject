6d8fdf7029fe70091e3cfc6e260289c5bfc0274e
matheus <matheuschomem@hotmail.com>
2024-10-04 06:41:06 -0300

    feat: enhance ParquetHandler initialization and improve logging for read/write operations

--- src/etl/io/parquet.py
+++ src/etl/io/parquet.py
@@ -5,10 +5,13 @@ from src.etl.ports import IOHandler, PandasDF
 class ParquetHandler(IOHandler):
 
     def __init__(self, layer: Layer):
-        super().__init__(layer)
+        super().__init__(class_name=__class__.__name__, layer=layer, format="parquet")
 
     def read(self) -> PandasDF:
+        self.logger.info(f"Reading Parquet file from {self.path}")
         return self.pd.read_parquet(self.path)
 
     def write(self, table: PandasDF) -> None:
-        table.to_parquet(self.path, index=False)
+        self.logger.info(f"Writing Parquet file to {self.path}.parquet")
+        parquet_path = self.path + ".parquet"
+        table.to_parquet(parquet_path, index=False)
