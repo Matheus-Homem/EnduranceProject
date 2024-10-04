03988b1e9bf4a1146052aba6122c218d5fd461a0
matheus <matheuschomem@hotmail.com>
2024-10-04 06:40:28 -0300

    feat: initialize CleaningEngine with class name and add logging for data cleaning process

--- src/etl/engines/cleaning.py
+++ src/etl/engines/cleaning.py
@@ -4,7 +4,8 @@ from src.etl.ports import Engine, PandasDF
 class CleaningEngine(Engine):
 
     def __init__(self):
-        super().__init__()
+        super().__init__(class_name=__class__.__name__)
 
     def process(self, table: PandasDF) -> PandasDF:
-        pass
+        self.logger.info("Starting data cleaning process")
+        return table
