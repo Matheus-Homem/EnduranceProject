eb23013ebd24c94ae2b3cf2196372c605fe5cb7f
matheus <matheuschomem@hotmail.com>
2024-10-04 06:40:51 -0300

    feat: enhance DatabaseHandler initialization and improve logging for data reading

--- src/etl/io/database.py
+++ src/etl/io/database.py
@@ -8,12 +8,12 @@ from src.shared.credentials import PRD
 class DatabaseHandler(IOHandler):
 
     def __init__(self, layer: Layer):
-        super().__init__(layer)
+        super().__init__(layer=layer, class_name=__class__.__name__)
 
     def read(self) -> DatabaseDF:
         if self.layer != Layer.DATABASE:
             raise ValueError("Layer not supported: DatabaseHandler only supports Layer.DATABASE")
-        self.logger.info(f"Reading data from {self.path}")
+        self.logger.info(f"Reading data from database connection")
         with DatabaseExecutorBuilder(use_production_db=PRD) as executor:
             entries = executor.select(ElementEntries)
         return entries
