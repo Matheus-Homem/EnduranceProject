c73a477297b42cabffd00e4649800641d3581223
matheus <matheuschomem@hotmail.com>
2024-10-05 09:07:52 -0300

    feat: update DatabaseHandler to support dynamic table names and improve read method

--- src/etl/io/database.py
+++ src/etl/io/database.py
@@ -1,47 +1,18 @@
 from src.database.connection.builder import DatabaseExecutorBuilder
-from src.database.tables import ElementEntries
-from src.etl.definitions import Layer
-from src.etl.ports import DatabaseDF, IOHandler
+from src.etl.core.definitions import DatabaseDF, IOHandler, Layer, TableName
 from src.shared.credentials import PRD
 
 
 class DatabaseHandler(IOHandler):
 
     def __init__(self, layer: Layer):
-        super().__init__(layer=layer, class_name=__class__.__name__)
+        super().__init__(class_name=__class__.__name__, supported_layers=[Layer.DATABASE], layer=layer)
 
-    def read(self) -> DatabaseDF:
-        if self.layer != Layer.DATABASE:
-            raise ValueError("Layer not supported: DatabaseHandler only supports Layer.DATABASE")
+    def read(self, table_name: TableName = TableName("element_entries")) -> DatabaseDF:
         self.logger.info(f"Reading data from database connection")
+        mysql_table = self.generate_path(table_name)
         with DatabaseExecutorBuilder(use_production_db=PRD) as executor:
-            entries = executor.select(ElementEntries)
-        return entries
+            return executor.select(mysql_table)
 
     def write(self, table):
         raise NotImplementedError("DatabaseReader does not support write operations")
-
-
-# from contextlib import contextmanager
-# from typing import Generator
-# import logging
-
-# @contextmanager
-# def log_operation(logger: logging.Logger, message: str) -> Generator[None, None, None]:
-#     logger.info(f"Starting: {message}")
-#     try:
-#         yield
-#     finally:
-#         logger.info(f"Finished: {message}")
-
-# def read(self) -> DatabaseDF:
-#     if self.layer != Layer.DATABASE:
-#         raise ValueError("Layer not supported. DatabaseHandler only supports Layer.DATABASE")
-
-#     self.logger.info(f"Reading data from {self.path}")
-
-#     with log_operation(self.logger, f"Reading data from {self.path}"):
-#         with DatabaseExecutorBuilder(use_production_db=PRD) as executor:
-#             entries = executor.select(ElementEntries)
-
-#     return entries
