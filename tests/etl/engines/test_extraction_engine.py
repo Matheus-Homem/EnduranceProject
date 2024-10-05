f24bb1ba1059a73fd5ad07b9549852ea6e1a3656
matheus <matheuschomem@hotmail.com>
2024-10-05 09:10:02 -0300

    test: update ExtractionEngine test to use list for input data and display DataFrame

--- tests/etl/engines/test_extraction_engine.py
+++ tests/etl/engines/test_extraction_engine.py
@@ -1,6 +1,7 @@
 import unittest
 
 import pandas as pd
+from IPython.display import display
 
 from src.etl.engines.extraction import ExtractionEngine
 
@@ -11,21 +12,23 @@ class TestExtractionEngine(unittest.TestCase):
         self.engine = ExtractionEngine()
 
     def test_process(self):
-        data = {
-            "id": 1,
-            "entry_date": pd.Timestamp("2021-01-01"),
-            "user_id": "0",
-            "element_category": "test_category",
-            "element_name": "test_name",
-            "element_string": "{'nao':'nao'}",
-            "schema_encoded": "1",
-            "op": "u",
-            "created_at": pd.Timestamp("2024-09-20 06:14:57"),
-            "updated_at": pd.Timestamp("2024-09-20 06:24:51"),
-        }
+        data = [
+            {
+                "id": 1,
+                "entry_date": pd.Timestamp("2021-01-01"),
+                "user_id": "0",
+                "element_category": "test_category",
+                "element_name": "test_name",
+                "element_string": "{'nao':'nao'}",
+                "schema_encoded": "1",
+                "op": "u",
+                "created_at": pd.Timestamp("2024-09-20 06:14:57"),
+                "updated_at": pd.Timestamp("2024-09-20 06:24:51"),
+            }
+        ]
 
-        expected_df = pd.DataFrame([data])
-        expected_df.head()
+        expected_df = pd.DataFrame(data)
+        display(expected_df.head())
 
         result_df = self.engine.process(data)
 
