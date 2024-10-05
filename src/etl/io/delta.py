4e3f84a98ae58ab3f02c6cf14c465d67ecf4a7a3
matheus <matheuschomem@hotmail.com>
2024-10-05 08:57:50 -0300

    feat: update DeltaHandler to support dynamic table names for read/write operations

--- src/etl/io/delta.py
+++ src/etl/io/delta.py
@@ -1,18 +1,19 @@
 from deltalake import DeltaTable, write_deltalake
 
-from src.etl.definitions import Layer
-from src.etl.ports import IOHandler, PandasDF
+from src.etl.core.definitions import IOHandler, Layer, PandasDF, TableName
 
 
 class DeltaHandler(IOHandler):
 
     def __init__(self, layer: Layer):
-        super().__init__(layer=layer, class_name=__class__.__name__)
+        super().__init__(class_name=__class__.__name__, supported_layers=[Layer.SILVER, Layer.GOLD], layer=layer)
 
-    def read(self) -> PandasDF:
-        self.logger.info(f"Reading Delta Lake file from {self.path}")
-        return DeltaTable(self.path).to_pandas()
+    def read(self, table_name: TableName) -> PandasDF:
+        path = self.generate_path(table_name=table_name)
+        self.logger.info(f"Reading Delta Lake file from {path}")
+        return DeltaTable(path).to_pandas()
 
-    def write(self, table: PandasDF) -> None:
-        self.logger.info(f"Writing Delta Lake file to {self.path}")
-        write_deltalake(self.path, table, mode="overwrite", schema_mode="overwrite")
+    def write(self, table: PandasDF, table_name: TableName) -> None:
+        path = self.generate_path(table_name=table_name)
+        self.logger.info(f"Writing Delta Lake file to {path}")
+        write_deltalake(path, table, mode="overwrite", schema_mode="overwrite")
