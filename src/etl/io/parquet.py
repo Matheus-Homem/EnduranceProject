a1a6a8a3bd30762b25ed71c0e6a8acc8e34def5b
matheus <matheuschomem@hotmail.com>
2024-10-05 08:57:38 -0300

    feat: update ParquetHandler to support dynamic table names for read/write operations

--- src/etl/io/parquet.py
+++ src/etl/io/parquet.py
@@ -1,17 +1,17 @@
-from src.etl.definitions import Layer
-from src.etl.ports import IOHandler, PandasDF
+from src.etl.core.definitions import IOHandler, Layer, PandasDF, TableName
 
 
 class ParquetHandler(IOHandler):
 
     def __init__(self, layer: Layer):
-        super().__init__(class_name=__class__.__name__, layer=layer, format="parquet")
+        super().__init__(class_name=__class__.__name__, supported_layers=[Layer.BRONZE], layer=layer)
 
-    def read(self) -> PandasDF:
-        self.logger.info(f"Reading Parquet file from {self.path}")
-        return self.pd.read_parquet(self.path)
+    def read(self, table_name: TableName) -> PandasDF:
+        path = self.generate_path(table_name=table_name)
+        self.logger.info(f"Reading Parquet file from {repr(path)}")
+        return self.pd.read_parquet(path)
 
-    def write(self, table: PandasDF) -> None:
-        self.logger.info(f"Writing Parquet file to {self.path}.parquet")
-        parquet_path = self.path + ".parquet"
-        table.to_parquet(parquet_path, index=False)
+    def write(self, table: PandasDF, table_name: TableName) -> None:
+        path = self.generate_path(table_name=table_name)
+        self.logger.info(f"Writing Parquet file to {repr(path)}")
+        table.to_parquet(path, index=False)
