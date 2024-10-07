a8763ecf448fd63794c52ac4a0ff5c3add5f7c4b
matheus <matheuschomem@hotmail.com>
2024-10-07 05:39:12 -0300

    refactor: update ParquetHandler read and write methods to use default table name and dataframe parameter

--- src/etl/io/parquet.py
+++ src/etl/io/parquet.py
@@ -6,12 +6,12 @@ class ParquetHandler(IOHandler):
     def __init__(self, layer: Layer):
         super().__init__(class_name=__class__.__name__, supported_layers=[Layer.BRONZE], layer=layer)
 
-    def read(self, table_name: TableName) -> PandasDF:
+    def read(self, table_name: TableName = TableName("entries")) -> PandasDF:
         path = self.generate_path(table_name=table_name)
         self.logger.info(f"Reading Parquet file from {repr(path)}")
         return self.pd.read_parquet(path)
 
-    def write(self, table: PandasDF, table_name: TableName) -> None:
+    def write(self, dataframe: PandasDF, table_name: TableName = TableName("entries")) -> None:
         path = self.generate_path(table_name=table_name)
         self.logger.info(f"Writing Parquet file to {repr(path)}")
-        table.to_parquet(path, index=False)
+        dataframe.to_parquet(path, index=False)
