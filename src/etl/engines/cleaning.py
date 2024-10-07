220360893012f3885a134dcacd72995f09c999e9
matheus <matheuschomem@hotmail.com>
2024-10-07 09:42:06 -0300

    refactor: enhance CleaningEngine with column splitting and table name retrieval methods

--- src/etl/engines/cleaning.py
+++ src/etl/engines/cleaning.py
@@ -1,11 +1,26 @@
-from src.etl.core.definitions import Engine, PandasDF
+from typing import List
+
+from src.etl.core.definitions import Engine, PandasDF, TableName
 
 
 class CleaningEngine(Engine):
 
-    def __init__(self):
+    def __init__(self, column_to_split: str = "element_name"):
         super().__init__(class_name=__class__.__name__)
+        self.column_to_split = column_to_split
 
-    def process(self, table: PandasDF) -> PandasDF:
+    def need_split(self) -> bool:
+        return True
+
+    def process(self, dataframe: PandasDF) -> PandasDF:
         self.logger.info("Starting data cleaning process")
-        return table
+        return dataframe
+
+    def split(self, dataframe: PandasDF) -> List[PandasDF]:
+        self.logger.info("Starting data split process")
+        subsets = dataframe[self.column_to_split].unique()
+        return [dataframe[dataframe[self.column_to_split] == subset] for subset in subsets]
+
+    def get_table_name(self, dataframe: PandasDF) -> TableName:
+        table_name = dataframe[self.column_to_split].unique()[0]
+        return TableName(table_name)
