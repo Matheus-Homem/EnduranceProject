18a184c833ecbabcbdf04626ae243bf7ecbb1ed6
matheus <matheuschomem@hotmail.com>
2024-10-08 06:26:17 -0300

    refactor: simplify ExtractionEngine initialization and remove unsupported methods

--- src/etl/engines/extraction.py
+++ src/etl/engines/extraction.py
@@ -1,17 +1,11 @@
-from src.etl.core.definitions import DatabaseDF, Engine, PandasDF, TableName
+from src.etl.core.definitions import DatabaseDF, Engine, PandasDF
 
 
 class ExtractionEngine(Engine):
 
     def __init__(self):
-        super().__init__(class_name=__class__.__name__, need_split=False)
+        super().__init__(class_name=__class__.__name__)
 
     def process(self, dataframe: DatabaseDF) -> PandasDF:
         self.logger.info("Starting data processing for extraction tables")
         return self.pd.DataFrame(dataframe)
-
-    def split(self, dataframe: PandasDF) -> PandasDF:
-        NotImplementedError("ExtractionEngine does not support split operations")
-
-    def get_table_name(self, dataframe: PandasDF) -> TableName:
-        NotImplementedError("ExtractionEngine does not support get_table_name operations")
