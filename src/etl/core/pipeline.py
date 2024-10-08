5042cdf305fcd484b85cab4e0c2df5937b328d37
matheus <matheuschomem@hotmail.com>
2024-10-08 06:25:58 -0300

    refactor: rename processing methods in Pipeline class for clarity and consistency

--- src/etl/core/pipeline.py
+++ src/etl/core/pipeline.py
@@ -21,16 +21,17 @@ class Pipeline:
         data = self.reader.read(table_name=table_to_read)
 
         if self.engine.should_split_data():
-            self._split_and_write_data_subsets(data)
+            self._split_process_and_save(data)
         else:
-            self._process_and_write(data, table_to_write)
+            self._proccess_and_save(data, table_to_write)
 
-    def _process_and_write(self, dataframe: DataFrameType, table_to_write: TableName) -> None:
+    def _proccess_and_save(self, dataframe: DataFrameType, table_to_write: TableName) -> None:
         processed_table = self.engine.process(dataframe)
         self.writer.write(dataframe=processed_table, table_name=table_to_write)
 
-    def _split_and_write_data_subsets(self, data: DataFrameType) -> None:
-        data_subsets = self.engine.split(data)
+    def _split_process_and_save(self, dataframe: DataFrameType) -> None:
+        data_subsets = self.engine.splitter.split(dataframe=dataframe)
         for subset in data_subsets:
-            table_name = self.engine.get_table_name(subset)
-            self._process_and_write(dataframe=subset, table_to_write=table_name)
+            table_to_write = self.engine.splitter.get_table_name(subset)
+            processed_table = self.engine.process(subset)
+            self.writer.write(dataframe=processed_table, table_name=table_to_write)
