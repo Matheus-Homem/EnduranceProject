f3e72980010b2d40012309a440c27987373641a8
matheus <matheuschomem@hotmail.com>
2024-10-09 15:27:53 -0300

    refactor: replace CastingTool with SchemaTool in CleaningEngine and streamline data processing methods

--- src/etl/engines/cleaning.py
+++ src/etl/engines/cleaning.py
@@ -2,7 +2,7 @@ from json import loads as json_loads
 from typing import List
 
 from src.etl.core.definitions import Engine, PandasDF, Splitter, Tool
-from src.etl.engines.tools.casting import CastingTool
+from src.etl.engines.tools.schema import SchemaTool
 from src.etl.engines.tools.splitter import CleaningSplitter
 
 
@@ -10,35 +10,30 @@ class CleaningEngine(Engine):
 
     def __init__(
         self,
-        tool: Tool = CastingTool(),
+        tool: Tool = SchemaTool(),
         splitter: Splitter = CleaningSplitter(),
     ):
         super().__init__(class_name=__class__.__name__, tool=tool, splitter=splitter)
 
-    def _reorder_columns(self, dataframe: PandasDF, start_columns: List[str], end_columns: List[str]) -> PandasDF:
-        variable_columns = [col for col in dataframe.columns if col not in start_columns + end_columns]
-        new_column_order = start_columns + variable_columns + end_columns
+    def _explode_json_column(self, dataframe: PandasDF, field_name: str) -> PandasDF:
+        base_dataframe = dataframe.drop([field_name], axis=1)
+        exploded_json_columns = dataframe[field_name].apply(json_loads).apply(self.pd.Series)
+        return self.pd.concat([base_dataframe, exploded_json_columns], axis=1)
+
+    def _reorder_columns(self, dataframe: PandasDF, start_cols: List[str], end_cols: List[str]) -> PandasDF:
+        variable_columns = [col for col in dataframe.columns if col not in start_cols + end_cols]
+        new_column_order = start_cols + variable_columns + end_cols
         return dataframe[new_column_order]
 
-    def _cast_columns(self, dataframe: PandasDF) -> PandasDF:
-        return self.tool.apply(dataframe)
-
-    def _drop_columns(self, dataframe: PandasDF) -> PandasDF:
-        return dataframe.drop(["id", "entry_date", "op"], axis=1)
-
-    def _explode_json_column(self, dataframe: PandasDF, field_name: str) -> PandasDF:
-        df1 = dataframe.drop([field_name], axis=1)
-        df2 = dataframe[field_name].apply(json_loads).apply(self.pd.Series)
-        return self.pd.concat([df1, df2], axis=1)
+    def _drop_columns(self, dataframe: PandasDF, columns: List[str]) -> PandasDF:
+        return dataframe.drop(columns, axis=1)
 
     def process(self, dataframe: PandasDF) -> PandasDF:
         self.logger.info("Starting data cleaning process")
-        df_exploded = self._explode_json_column(dataframe, field_name="element_string")
-        df_ordered = self._reorder_columns(
-            df_exploded,
-            start_columns=["date_input", "element_category", "element_name"],
-            end_columns=["user_id", "schema_encoded", "created_at", "updated_at"],
+        return (
+            dataframe
+            .pipe(self._explode_json_column, field_name="element_string")
+            .pipe(self._reorder_columns, start_cols=["date_input", "element_category", "element_name"], end_cols=["user_id", "schema_encoded", "created_at", "updated_at"])
+            .pipe(self.tool.apply)
+            .pipe(self._drop_columns, columns=["id", "entry_date", "op", "schema_dtypes", "schema_fields"])
         )
-        df_casted = self._cast_columns(df_ordered)
-        df_cleaned = self._drop_columns(df_casted)
-        return df_cleaned
