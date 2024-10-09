c87045adefc92b8c00959eea05dc9fa00debc281
matheus <matheuschomem@hotmail.com>
2024-10-09 15:28:05 -0300

    refactor: enhance CastingStrategyFactory with additional casting strategies and improve null handling in casting methods

--- src/etl/engines/tools/casting.py
+++ src/etl/engines/tools/casting.py
@@ -1,50 +1,78 @@
-from json import loads as json_loads
-from typing import Dict
-
-from src.etl.core.definitions import CastingStrategy, PandasDF, Tool
-
-
-class CastingTool(Tool):
-    def _read_schema_dataframe(self, schema_path: str) -> PandasDF:
-        df_schema = self.pd.read_parquet(schema_path)
-        df_schema_filtered = df_schema[["schema_encoded", "element_category", "element_name", "schema_dtypes", "schema_fields"]]
-        return df_schema_filtered
-
-    def _get_dtype_dict(self, df_schema: PandasDF, schema_encoded: str) -> Dict[str, str]:
-        try:
-            schema = df_schema[(df_schema["schema_encoded"] == schema_encoded)]
-            return json_loads(schema["schema_dtypes"].iloc[0])
-        except Exception as e:
-            raise Exception(f"Error while getting schema dtype dictionary: {e}")
-
-    def apply(self, dataframe: PandasDF) -> PandasDF:
-        self.schema = self._read_schema_dataframe("data/bronze/schemas.parquet")
-        df_merged = self.pd.merge(dataframe, self.schema, on=["schema_encoded", "element_category", "element_name"], how="inner")
-
-        for schema_code in df_merged["schema_encoded"].unique():
-            schema = self._get_dtype_dict(df_merged, schema_code)
-            for column, dtype in schema.items():
-                strategy = CastingStrategyFactory.get_strategy(dtype)
-                strategy.cast(df_merged[column], pandas=self.pd)
-
-        return df_merged
+from datetime import datetime
+from src.etl.core.definitions import CastingStrategy, DataType, PandasDF
 
 
 class CastingStrategyFactory:
     @staticmethod
-    def get_strategy(dtype: str) -> CastingStrategy:
-        if dtype == "date":
+    def get_strategy(column_type: str) -> CastingStrategy:
+        dtype = DataType(column_type)
+
+        if dtype == DataType.DATE:
             return DateCastingStrategy()
-        elif dtype == "bool":
+        if dtype == DataType.BOOL:
             return BoolCastingStrategy()
+        if dtype == DataType.DOUBLE:
+            return DoubleCastingStrategy()
+        if dtype == DataType.DURATION_HHMMSS:
+            return DurationHHMMSSCastingStrategy()
+        if dtype == DataType.DURATION_HHMM:
+            return DurationHHMMCastingStrategy()
+        if dtype == DataType.INTEGER:
+            return IntegerCastingStrategy()
+        if dtype == DataType.ORDINAL:
+            return OrdinalCastingStrategy()
+        if dtype == DataType.STRING:
+            return StringCastingStrategy()
+        if dtype == DataType.TIMESTAMP:
+            return TimestampCastingStrategy()
         return None
 
 
 class DateCastingStrategy(CastingStrategy):
-    def cast(self, col: PandasDF, pandas) -> PandasDF:
-        return col.apply(lambda x: pandas.to_datetime(x).date() if pandas.notnull(x) else None)
+    def cast(self, col: PandasDF) -> PandasDF:
+        col = col.fillna(value="1900-01-01")
+        return col.apply(lambda x: datetime.strptime(x, "%Y-%m-%d").date() if x else None)
 
 
 class BoolCastingStrategy(CastingStrategy):
     def cast(self, col: PandasDF) -> PandasDF:
-        return col.apply(lambda x: True if x == "True" else False if x == "False" else None)
+        return col.apply(lambda x: True if x == "True" else False if x == "False" else False)
+
+
+class DoubleCastingStrategy(CastingStrategy):
+    def cast(self, col: PandasDF) -> PandasDF:
+        return col.apply(lambda x: float(x) if x else 0)
+
+
+class DurationHHMMSSCastingStrategy(CastingStrategy):
+    def cast(self, col: PandasDF) -> PandasDF:
+        col = col.fillna(value="00:00:00")
+        return col.apply(lambda x: datetime.strptime(x, "%H:%M:%S").time() if x else None)
+
+
+class DurationHHMMCastingStrategy(CastingStrategy):
+    def cast(self, col: PandasDF) -> PandasDF:
+        col = col.fillna(value="00:00")
+        return col.apply(lambda x: datetime.strptime(x, "%H:%M").time() if x else None)
+
+
+class IntegerCastingStrategy(CastingStrategy):
+    def cast(self, col: PandasDF) -> PandasDF:
+        return col.apply(lambda x: int(x) if x else 0)
+    
+
+class OrdinalCastingStrategy(CastingStrategy):
+    def cast(self, col: PandasDF) -> PandasDF:
+        return col.astype('category')
+
+
+class StringCastingStrategy(CastingStrategy):
+    def cast(self, col: PandasDF) -> PandasDF:
+        col = col.fillna("")
+        return col.astype("string")
+
+
+class TimestampCastingStrategy(CastingStrategy):
+    def cast(self, col: PandasDF) -> PandasDF:
+        col = col.fillna(value="1900-01-01 00:00:00")
+        return col.apply(lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S") if x else None)
