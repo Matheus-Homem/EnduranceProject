987b2f3fb3d00f44978fc8e98eaf3f7f06c19db2
matheus <matheuschomem@hotmail.com>
2024-10-15 07:08:31 -0300

    refactor: Update casting module to handle null values and improve data type handling
    - Update SchemaTool to handle null values in casting methods
    - Enhance CastingStrategyFactory with additional casting strategies and improve null handling in casting methods
    - Update casting strategies to use pandas Series and improve data type handling

--- src/etl/engines/tools/casting.py
+++ src/etl/engines/tools/casting.py
@@ -1,5 +1,8 @@
-from datetime import datetime
-from src.etl.core.definitions import CastingStrategy, DataType, PandasDF
+from datetime import datetime, timedelta
+
+from pandas import Series, to_datetime
+
+from src.etl.core.definitions import CastingStrategy, DataType
 
 
 class CastingStrategyFactory:
@@ -29,50 +32,47 @@ class CastingStrategyFactory:
 
 
 class DateCastingStrategy(CastingStrategy):
-    def cast(self, col: PandasDF) -> PandasDF:
-        col = col.fillna(value="1900-01-01")
-        return col.apply(lambda x: datetime.strptime(x, "%Y-%m-%d").date() if x else None)
+    def cast(self, col: Series) -> Series:
+        return to_datetime(col, format="%Y-%m-%d", errors="coerce").dt.date.astype("datetime64[ns]")
 
 
 class BoolCastingStrategy(CastingStrategy):
-    def cast(self, col: PandasDF) -> PandasDF:
-        return col.apply(lambda x: True if x == "True" else False if x == "False" else False)
+    def cast(self, col: Series) -> Series:
+        return col.apply(lambda x: True if x == "True" else False if x == "False" else None).astype("bool")
 
 
 class DoubleCastingStrategy(CastingStrategy):
-    def cast(self, col: PandasDF) -> PandasDF:
-        return col.apply(lambda x: float(x) if x else 0)
+    def cast(self, col: Series) -> Series:
+        return col.astype("float64") / 100
 
 
 class DurationHHMMSSCastingStrategy(CastingStrategy):
-    def cast(self, col: PandasDF) -> PandasDF:
-        col = col.fillna(value="00:00:00")
-        return col.apply(lambda x: datetime.strptime(x, "%H:%M:%S").time() if x else None)
+    def cast(self, col: Series) -> Series:
+        return col.apply(
+            lambda x: int(timedelta(hours=int(x[:-4]), minutes=int(x[-4:-2]), seconds=int(x[-2:])).total_seconds()) if x else None
+        ).astype("Int64")
 
 
 class DurationHHMMCastingStrategy(CastingStrategy):
-    def cast(self, col: PandasDF) -> PandasDF:
-        col = col.fillna(value="00:00")
-        return col.apply(lambda x: datetime.strptime(x, "%H:%M").time() if x else None)
+    def cast(self, col: Series) -> Series:
+        return col.apply(lambda x: int(timedelta(hours=int(x[-4:-2]), minutes=int(x[-2:])).total_seconds()) if x else None).astype("Int64")
 
 
 class IntegerCastingStrategy(CastingStrategy):
-    def cast(self, col: PandasDF) -> PandasDF:
-        return col.apply(lambda x: int(x) if x else 0)
-    
+    def cast(self, col: Series) -> Series:
+        return col.astype("Int64")
+
 
 class OrdinalCastingStrategy(CastingStrategy):
-    def cast(self, col: PandasDF) -> PandasDF:
-        return col.astype('category')
+    def cast(self, col: Series) -> Series:
+        return col.astype("category")
 
 
 class StringCastingStrategy(CastingStrategy):
-    def cast(self, col: PandasDF) -> PandasDF:
-        col = col.fillna("")
+    def cast(self, col: Series) -> Series:
         return col.astype("string")
 
 
 class TimestampCastingStrategy(CastingStrategy):
-    def cast(self, col: PandasDF) -> PandasDF:
-        col = col.fillna(value="1900-01-01 00:00:00")
-        return col.apply(lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S") if x else None)
+    def cast(self, col: Series) -> Series:
+        return col.apply(lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S") if x else None).astype("datetime64[ns]")
