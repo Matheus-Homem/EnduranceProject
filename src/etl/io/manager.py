04102d2ac34a16c3b9ee831ba63afc55a175d6fb
matheus <matheuschomem@hotmail.com>
2024-10-23 07:57:25 -0300

    refactor: IOManager to use instance methods for getting handlers

--- src/etl/io/manager.py
+++ src/etl/io/manager.py
@@ -1,25 +1,7 @@
-from src.etl.core.definitions import DataFrameType, Format, IOHandler, Layer, TableName
+from src.etl.core.definitions import Format, IOHandler, Layer
 from src.etl.io.database import DatabaseHandler
 from src.etl.io.delta import DeltaHandler
 from src.etl.io.parquet import ParquetHandler
-
-
-class IOHandlerFactory:
-
-    @staticmethod
-    def get_handler(layer: Layer, format: Format) -> IOHandler:
-
-        if format == Format.JDBC:
-            return DatabaseHandler(layer=layer)
-
-        if format == Format.PARQUET:
-            return ParquetHandler(layer=layer)
-
-        if format == Format.DELTA:
-            return DeltaHandler(layer=layer)
-
-        else:
-            raise ValueError(f"Unsupported format: {format}")
 
 
 class IOManager:
@@ -29,10 +11,19 @@ class IOManager:
         layer: Layer,
         format: Format,
     ):
-        self.handler = IOHandlerFactory.get_handler(layer, format)
+        self.layer = layer
+        self.format = format
 
-    def read(self, table_name: TableName) -> DataFrameType:
-        return self.handler.read(table_name=table_name)
+    def get_handler(self) -> IOHandler:
 
-    def write(self, dataframe: DataFrameType, table_name: TableName) -> None:
-        self.handler.write(dataframe=dataframe, table_name=table_name)
+        if self.format == Format.JDBC:
+            return DatabaseHandler(layer=self.layer)
+
+        if self.format == Format.PARQUET:
+            return ParquetHandler(layer=self.layer)
+
+        if self.format == Format.DELTA:
+            return DeltaHandler(layer=self.layer)
+
+        else:
+            raise ValueError(f"Unsupported format: {self.format}")
