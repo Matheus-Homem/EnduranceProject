96399ab6131baf3e1339ab8654d19c44abe16f72
matheus <matheuschomem@hotmail.com>
2024-10-23 07:56:33 -0300

    refactor: Pipeline class to handle different engine types in execute method

--- src/etl/core/pipeline.py
+++ src/etl/core/pipeline.py
@@ -1,6 +1,6 @@
 import logging
 
-from src.etl.core.definitions import DataFrameType, Engine, TableName
+from src.etl.core.definitions import Engine, EngineType, TableName
 from src.etl.io.manager import IOManager
 
 
@@ -13,33 +13,27 @@ class Pipeline:
         writer: IOManager,
     ):
         self.logger = logging.getLogger(__class__.__name__)
-        self.reader = reader
+        self.reader = reader.get_handler()
         self.engine = engine
-        self.writer = writer
-        self._set_pipeline_type(engine.__class__.__name__)
-
-    def _set_pipeline_type(self, engine_class_name: str) -> str:
-        self.pipeline_type = engine_class_name.replace("Engine", "").upper()
-
-    def _proccess_and_save(self, dataframe: DataFrameType, table_to_write: TableName) -> None:
-        processed_table = self.engine.process(dataframe)
-        self.writer.write(dataframe=processed_table, table_name=table_to_write)
-
-    def _split_process_and_save(self, dataframe: DataFrameType) -> None:
-        data_subsets = self.engine.splitter.split(dataframe=dataframe)
-        for subset in data_subsets:
-            table_to_write = self.engine.splitter.get_table_name(subset)
-            processed_table = self.engine.process(subset)
-            self.writer.write(dataframe=processed_table, table_name=table_to_write)
+        self.writer = writer.get_handler()
 
     def execute(self, table_to_read: TableName = None, table_to_write: TableName = None) -> None:
-        self.logger.info(f"Starting pipeline execution for {self.pipeline_type} layer")
+        self.logger.info(f"Starting pipeline execution for {self.engine.type.value.upper()} process")
 
-        data = self.reader.read(table_name=table_to_read)
+        if self.engine.type == EngineType.EXTRACTION:
+            df = self.reader.read(table_name=table_to_read)
+            df = self.engine.process(df)
+            self.writer.write(dataframe=df, table_name=table_to_write)
 
-        if self.engine.should_split_data():
-            self._split_process_and_save(data)
-        else:
-            self._proccess_and_save(data, table_to_write)
+        if self.engine.type == EngineType.CLEANING:
+            df = self.reader.read(table_name=table_to_read)
+            data_subsets = self.engine.splitter.split(dataframe=df)
+            for subset in data_subsets:
+                table_to_write = self.engine.splitter.get_table_name(subset)
+                processed_table = self.engine.process(subset)
+                self.writer.write(dataframe=processed_table, table_name=table_to_write)
 
-        self.logger.info(f"Pipeline execution for {self.pipeline_type} layer finished")
+        if self.engine.type == EngineType.REFINEMENT:
+            pass
+
+        self.logger.info(f"Pipeline execution for {self.engine.type.value.upper()} process finished")
