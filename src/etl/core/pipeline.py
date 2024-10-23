d2648951477e9756b051b4b55941b019511ea810
matheus <matheuschomem@hotmail.com>
2024-10-23 19:48:10 -0300

    Refactor Pipeline class to include a __refine method for handling DeltaHandler instances

--- src/etl/core/pipeline.py
+++ src/etl/core/pipeline.py
@@ -3,6 +3,7 @@ import logging
 from src.etl.core.definitions import Engine, EngineType, TableName
 from src.etl.engines.tools.utils import get_subset_table_name, split_dataframe
 from src.etl.io.manager import IOManager
+from src.etl.io.delta import DeltaHandler
 
 
 class Pipeline:
@@ -32,6 +33,16 @@ class Pipeline:
             processed_table = self.engine.process(subset)
             self.writer.write(dataframe=processed_table, table_name=subset_table_name)
 
+    def __refine(self) -> None:
+        if isinstance(self.reader, DeltaHandler):
+            list_of_tables = self.reader.list_delta_tables()
+            for table in list_of_tables:
+                df = self.reader.read(table_name=table)
+                df = self.engine.process(df)
+                self.writer.write(dataframe=df, table_name=table)
+        else:
+            self.logger.error("Reader is not an instance of DeltaHandler for REFINEMENT process")
+
     def execute(self, table_to_read: TableName = None, table_to_write: TableName = None) -> None:
         self.logger.info(f"Starting pipeline execution for {self.engine.type.value.upper()} process")
 
@@ -42,11 +53,6 @@ class Pipeline:
             self.__clean(table_to_read=table_to_read)
 
         if self.engine.type == EngineType.REFINEMENT:
-            list_of_tables = self.engine.tool.apply()
-            for table in list_of_tables:
-                table_name = table.split("/")[0]
-                df = self.reader.read(table_name=table_name)
-                df = self.engine.process(df)
-                self.writer.write(dataframe=df, table_name=table_name)
+            self.__refine()
 
         self.logger.info(f"Pipeline execution for {self.engine.type.value.upper()} process finished")
