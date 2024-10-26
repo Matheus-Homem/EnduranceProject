0b764800c51e68eed1a512db3e5f738613adffb2
matheus <matheuschomem@hotmail.com>
2024-10-26 07:03:02 -0300

    Add cleaning, extraction, and io modules to core package

---
+++ src/etl/core/io/__init__.py
@@ -0,0 +1,2 @@
+from .delta import DeltaHandler as DeltaHandlerType
+from .manager import IOManager
--- src/etl/engines/__init__.py
+++
@@ -1,3 +0,0 @@
-from .cleaning import CleaningEngine
-from .extraction import ExtractionEngine
-from .refinement import RefinementEngine
