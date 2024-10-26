3b070f1bc49891f3e348b81ef8384c97ecd47091
matheus <matheuschomem@hotmail.com>
2024-10-26 06:32:51 -0300

    refactor: refinement engine in src/etl/engines/refinement.py

--- src/etl/engines/refinement.py
+++ src/etl/engines/refinement.py
@@ -8,20 +8,6 @@ class RefinementEngine(Engine):
     def __init__(self):
         super().__init__(class_name=__class__.__name__, type=EngineType.REFINEMENT)
 
-    # def _melt_dataframe(
-    #     self,
-    #     table: PandasDF,
-    #     bool_cols: List[str],
-    #     id_cols: List[str] = ['element_category', 'element_name', 'user_id', 'date_input'],
-    #     activity_content_col: str = None,
-    # ) -> PandasDF:
-    #     return table.melt(
-    #         id_vars=id_cols + activity_content_col,
-    #         value_vars=bool_cols,
-    #         var_name="column_name",
-    #         value_name="column_value"
-    #     )
-
     def _build_summary_statistics(self, cleaned_table: PandasDF) -> Optional[PandasDF]:
         pass
 
