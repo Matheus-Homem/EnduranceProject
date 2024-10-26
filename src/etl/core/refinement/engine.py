041c606b20bff7ae5f40bb2826677c4477ffb5f3
matheus <matheuschomem@hotmail.com>
2024-10-26 08:19:43 -0300

    Refactor RefinementEngine to use specific DataFrameTransformer classes

--- src/etl/core/refinement/engine.py
+++ src/etl/core/refinement/engine.py
@@ -1,34 +1,27 @@
-from typing import List, Optional
+from typing import List
 
 from src.etl.core.definitions import Engine, EngineType, PandasDF
-
-# from src.etl.core.refinement.groupers.summary import SummaryDataFrameTransformer
-# from src.etl.core.refinement.groupers.monthly import MonthlyDataFrameTransformer
-# from src.etl.core.refinement.groupers.weekly import WeeklyDataFrameTransformer
+from src.etl.core.refinement.groupers.monthly import MonthlyDataFrameTransformer
+from src.etl.core.refinement.groupers.summary import SummaryDataFrameTransformer
+from src.etl.core.refinement.groupers.weekly import WeeklyDataFrameTransformer
 
 
 class RefinementEngine(Engine):
 
     def __init__(
         self,
-        #         summary_transformer: SummaryDataFrameTransformer = SummaryDataFrameTransformer(),
-        #         monthly_transformer: MonthlyDataFrameTransformer = MonthlyDataFrameTransformer(),
-        #         weekly_transformer: WeeklyDataFrameTransformer = WeeklyDataFrameTransformer(),
+        summary_transformer: SummaryDataFrameTransformer = SummaryDataFrameTransformer(),
+        monthly_transformer: MonthlyDataFrameTransformer = MonthlyDataFrameTransformer(),
+        weekly_transformer: WeeklyDataFrameTransformer = WeeklyDataFrameTransformer(),
     ):
         super().__init__(class_name=__class__.__name__, type=EngineType.REFINEMENT)
-
-    def _build_summary_statistics(self, cleaned_table: PandasDF) -> Optional[PandasDF]:
-        pass
-
-    def _build_monthly_statistics(self, cleaned_table: PandasDF) -> Optional[PandasDF]:
-        pass
-
-    def _build_weekly_statistics(self, cleaned_table: PandasDF) -> Optional[PandasDF]:
-        pass
+        self.summary_transformer = summary_transformer
+        self.monthly_transformer = monthly_transformer
+        self.weekly_transformer = weekly_transformer
 
     def process(self, table: PandasDF) -> List[PandasDF]:
         self.logger.info("Starting data refinement process")
-        df_summary = self._build_summary_statistics(cleaned_table=table)
-        df_monthly = self._build_monthly_statistics(cleaned_table=table)
-        df_weekly = self._build_weekly_statistics(cleaned_table=table)
+        df_summary = self.summary_transformer.apply(dataframe=table)
+        df_monthly = self.monthly_transformer.apply(dataframe=table)
+        df_weekly = self.weekly_transformer.apply(dataframe=table)
         return [df for df in [df_summary, df_monthly, df_weekly] if df is not None]
