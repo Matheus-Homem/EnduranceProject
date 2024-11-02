67021a39d305b8641125e7bca4a6c1a7befa209b
matheus <matheuschomem@hotmail.com>
2024-11-02 17:34:56 -0300

    refactor: SummaryDataFrameTransformer to include SummaryDataFrameAggregator class and filter value columns by variant

--- src/etl/core/refinement/summary/transformer.py
+++ src/etl/core/refinement/summary/transformer.py
@@ -1,8 +1,7 @@
-from typing import List, Literal, Optional, Union
-
-from pandas import Series, Timedelta, Timestamp, to_datetime
+from typing import List, Optional
 
 from src.etl.core.definitions import PandasDF, Transformer
+from src.etl.core.refinement.summary.aggregator import SummaryDataFrameAggregator
 from src.etl.core.refinement.summary.melter import SummaryDataFrameMelter
 
 
@@ -17,167 +16,7 @@ class SummaryDataFrameTransformer(Transformer):
         self.habit_detail_dict = {
             "navigator": "book",
         }
-
-    @staticmethod
-    def _calculate_date_aggregation(
-        dates: Series,
-        aggregate_by: Literal["count", "min", "max"],
-    ) -> Union[int, Timestamp, None]:
-        if not (dates != Timestamp(0)).any():
-            return None
-        elif aggregate_by == "count":
-            return dates.count()
-        elif aggregate_by == "min":
-            return dates.min()
-        elif aggregate_by == "max":
-            return dates.max()
-
-    @staticmethod
-    def _calculate_longest_streak(dates: Series) -> int:
-        if dates.empty:
-            return 0
-        dates = dates.sort_values()
-        streaks = (dates.diff() != Timedelta(days=1)).cumsum()
-        return streaks.value_counts().max()
-
-    @staticmethod
-    def _calculate_longest_gap(dates: Series) -> int:
-        if dates.empty:
-            return 0
-        dates = dates.sort_values()
-        gaps = dates.diff().dt.days[1:]
-        return gaps.max() if not gaps.empty else 0
-
-    def _diplomat(
-        self,
-        cleaned_dataframe: PandasDF,
-        id_vars: List[str] = ["element_category", "element_name", "user_id", "date_input"],
-        value_vars: List[str] = ["animals", "family", "friends", "society", "contacts", "coworkers"],
-        var_name: str = "habit_action",
-        habit_detail_col: Optional[str] = None,
-    ) -> PandasDF:
-        df_right = cleaned_dataframe.melt(
-            id_vars=[id_vars, habit_detail_col],
-            value_vars=value_vars,
-            var_name=var_name,
-            value_name="value",
-        )
-
-        df_right["habit_action"] = df_right["habit_action"].str.replace("_0", "")
-        df_right["habit_group"] = "relate"
-
-        df_right = (
-            df_right.groupby(["user_id", "element_category", "element_name", "habit_action", "habit_group"])
-            .agg(
-                total=("date_input", lambda x: SummaryDataFrameTransformer._calculate_date_aggregation(x[df_right["value"] > 0], "count")),
-                first_date=("date_input", lambda x: SummaryDataFrameTransformer._calculate_date_aggregation(x[df_right["value"] > 0], "min")),
-                last_date=("date_input", lambda x: SummaryDataFrameTransformer._calculate_date_aggregation(x[df_right["value"] > 0], "max")),
-                longest_streak=("date_input", lambda x: SummaryDataFrameTransformer._calculate_longest_streak(x[df_right["value"] > 0])),
-                longest_gap=("date_input", lambda x: SummaryDataFrameTransformer._calculate_longest_gap(x[df_right["value"] > 0])),
-            )
-            .reset_index()
-        )
-
-        df_final = df_right
-        df_final["days_since_last"] = (to_datetime("today") - df_final["last_date"]).dt.days
-        df_final["habit_detail"] = None
-        df_final = df_final[
-            [
-                "element_category",
-                "element_name",
-                "user_id",
-                "habit_group",
-                "habit_action",
-                "habit_detail",
-                "total",
-                "first_date",
-                "last_date",
-                "days_since_last",
-                "longest_streak",
-                "longest_gap",
-            ]
-        ]
-        df_final = df_final.sort_values(by=["element_category", "element_name", "user_id", "habit_group", "habit_action", "habit_detail"])
-
-        return df_final
-
-    def _navigator(self):
-
-        df_right = df_navigator.melt(
-            id_vars=["element_category", "element_name", "user_id", "book_0", "date_input"],
-            value_vars=["read_0", "listen_0", "notes_0"],
-            var_name="habit_action",
-            value_name="value",
-        )
-
-        df_right["habit_action"] = df_right["habit_action"].str.replace("_0", "")
-        df_right["habit_group"] = "book"
-
-        def count_with_none(series):
-            if not series.any():
-                return 0
-            else:
-                return series.count()
-
-        def min_with_none(series):
-            if not series.any():
-                return None
-            else:
-                return series.min()
-
-        def max_with_none(series):
-            if not series.any():
-                return None
-            else:
-                return series.max()
-
-        def calculate_longest_streak(dates):
-            if dates.empty:
-                return 0
-            dates = dates.sort_values()
-            streaks = (dates.diff() != pd.Timedelta(days=1)).cumsum()
-            return streaks.value_counts().max()
-
-        def calculate_longest_gap(dates):
-            if dates.empty:
-                return 0
-            dates = dates.sort_values()
-            gaps = dates.diff().dt.days[1:]
-            return gaps.max() if not gaps.empty else 0
-
-        df_right = (
-            df_right.groupby(["user_id", "element_category", "element_name", "book_0", "habit_action", "habit_group"])
-            .agg(
-                total=("date_input", lambda x: count_with_none(x[df_right["value"] > 0])),
-                first_date=("date_input", lambda x: min_with_none(x[df_right["value"] > 0])),
-                last_date=("date_input", lambda x: max_with_none(x[df_right["value"] > 0])),
-                longest_streak=("date_input", lambda x: calculate_longest_streak(x[df_right["value"] > 0])),
-                longest_gap=("date_input", lambda x: calculate_longest_gap(x[df_right["value"] > 0])),
-            )
-            .reset_index()
-        )
-
-        df_final = df_right.rename(columns={"book_0": "habit_detail"})
-        df_final["days_since_last"] = (pd.to_datetime("today") - df_final["last_date"]).dt.days
-        df_final = df_final[
-            [
-                "element_category",
-                "element_name",
-                "user_id",
-                "habit_group",
-                "habit_action",
-                "habit_detail",
-                "total",
-                "first_date",
-                "last_date",
-                "days_since_last",
-                "longest_streak",
-                "longest_gap",
-            ]
-        ]
-        df_final = df_final.sort_values(by=["element_category", "element_name", "user_id", "habit_group", "habit_action", "habit_detail"])
-
-        return df_final
+        self.default_columns = ["element_category", "element_name", "user_id", "date_input", "schema_encoded", "created_at", "updated_at"]
 
     def _get_habit_detail_col(
         self,
@@ -185,17 +24,45 @@ class SummaryDataFrameTransformer(Transformer):
     ) -> Optional[str]:
         return self.habit_detail_dict.get(dataframe["element_name"].unique()[0])
 
-    def _set_habit_group(self, dataframe: PandasDF) -> PandasDF:
-        pass
+    def _get_value_columns(self, dataframe: PandasDF, detail_column: str) -> List[str]:
+        if detail_column:
+            return [col for col in dataframe.columns if col not in self.default_columns and not col.startswith(detail_column)]
+        return [col for col in dataframe.columns if col not in self.default_columns]
 
-    def _group_dataframe(self, dataframe: PandasDF) -> PandasDF:
-        pass
+    def _set_habit_group(self, dataframe: PandasDF) -> PandasDF:
+        element_name = dataframe["element_name"].unique()[0]
+        habit_group_dict = {
+            "navigator": "book",
+            "diplomat": "relate",
+        }
+
+        dataframe["habit_group"] = habit_group_dict.get(element_name)
+        return dataframe
+
+    def _calculate_summary_fields(
+        self,
+        dataframe: PandasDF,
+        group_columns: List[str] = ["user_id", "element_category", "element_name", "habit_detail", "habit_action", "habit_group"],
+    ) -> PandasDF:
+        valid_group_columns = [col for col in group_columns if not dataframe[col].isnull().all()]
+        df_grouped = (
+            dataframe.groupby(valid_group_columns)
+            .agg(
+                total=("date_input", lambda x: SummaryDataFrameAggregator.calculate_date_aggregation(x[dataframe["value"] > 0], "count")),
+                first_date=("date_input", lambda x: SummaryDataFrameAggregator.calculate_date_aggregation(x[dataframe["value"] > 0], "min")),
+                last_date=("date_input", lambda x: SummaryDataFrameAggregator.calculate_date_aggregation(x[dataframe["value"] > 0], "max")),
+                longest_streak=("date_input", lambda x: SummaryDataFrameAggregator.calculate_longest_streak(x[dataframe["value"] > 0])),
+                longest_gap=("date_input", lambda x: SummaryDataFrameAggregator.calculate_longest_gap(x[dataframe["value"] > 0])),
+            )
+            .reset_index()
+        )
+        if "habit_detail" not in df_grouped.columns:
+            df_grouped["habit_detail"] = None
+        return df_grouped
 
     def _add_fields(self, dataframe: PandasDF) -> PandasDF:
-        pass
-
-    def _set_habit_detail(self, dataframe: PandasDF) -> PandasDF:
-        pass
+        dataframe["days_since_last"] = (self.pd.to_datetime("today") - dataframe["last_date"]).dt.days
+        return dataframe
 
     def _reshape_dataframe(self, dataframe: PandasDF) -> PandasDF:
         return dataframe[
@@ -225,12 +92,12 @@ class SummaryDataFrameTransformer(Transformer):
         )
 
     def apply(self, dataframe: PandasDF) -> PandasDF:
-        habit_detail_col = self._get_habit_detail_col(dataframe)
+        detail_column = self._get_habit_detail_col(dataframe)
+        value_columns = self._get_value_columns(dataframe, detail_column)
         return (
-            dataframe.pipe(self.melter.apply, habit_detail_col=habit_detail_col)
+            dataframe.pipe(self.melter.apply, detail=detail_column, value_vars=value_columns)
             .pipe(self._set_habit_group)
-            .pipe(self._group_dataframe)
+            .pipe(self._calculate_summary_fields)
             .pipe(self._add_fields)
-            .pipe(self._set_habit_detail)
             .pipe(self._reshape_dataframe)
         )
