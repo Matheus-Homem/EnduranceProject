bfc613be418dd1e3300449ec3de8873d05620198
matheus <matheuschomem@hotmail.com>
2024-11-05 08:59:22 -0300

    refactor: Consolidate habit group logic and enhance column definitions in SummaryDataFrameTransformer

--- src/etl/core/refinement/summary/transformer.py
+++ src/etl/core/refinement/summary/transformer.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional
+from typing import Dict, List
 
 from src.etl.core.definitions import PandasDF, Transformer
 from src.etl.core.refinement.summary.aggregator import SummaryDataFrameAggregator
@@ -6,6 +6,65 @@ from src.etl.core.refinement.summary.melter import SummaryDataFrameMelter
 
 
 class SummaryDataFrameTransformer(Transformer):
+    default_columns = [
+        "element_category",
+        "element_name",
+        "user_id",
+        "date_input",
+        "schema_encoded",
+        "created_at",
+        "updated_at",
+    ]
+
+    ordened_columns = [
+        "element_category",
+        "element_name",
+        "user_id",
+        "habit_group",
+        "habit_action",
+        "habit_detail",
+        "total",
+        "first_date",
+        "last_date",
+        "days_since_last",
+        "longest_streak",
+        "longest_gap",
+    ]
+
+    dtype_dict = {
+        "element_category": "string",
+        "element_name": "string",
+        "user_id": "string",
+        "habit_group": "string",
+        "habit_action": "string",
+        "habit_detail": "string",
+        "total": "Int64",
+        "first_date": "datetime64[ns]",
+        "last_date": "datetime64[ns]",
+        "days_since_last": "Int64",
+        "longest_streak": "Int64",
+        "longest_gap": "Int64",
+    }
+
+    columns_to_sort = [
+        "element_category",
+        "element_name",
+        "user_id",
+        "habit_group",
+        "habit_action",
+        "habit_detail",
+    ]
+
+    habit_group_dict = {
+        "navigator": "book",
+        "alchemist": "emotion",
+        "sponsor": "duty",
+        "patron": "study",
+        "athlete": "running",
+        "diplomat": "relate",
+        "citizen": "group",
+        "oracle": "overhaul",
+    }
 
     def __init__(
         self,
@@ -13,86 +72,18 @@ class SummaryDataFrameTransformer(Transformer):
     ):
         super().__init__(class_name=__class__.__name__)
         self.melter = melter
-        self.habit_detail_dict = {
-            "navigator": "book",
-        }
-        self.default_columns = [
-            "element_category",
-            "element_name",
-            "user_id",
-            "date_input",
-            "schema_encoded",
-            "created_at",
-            "updated_at",
-        ]
-
-        self.ordened_columns = [
-            "element_category",
-            "element_name",
-            "user_id",
-            "habit_group",
-            "habit_action",
-            "habit_detail",
-            "total",
-            "first_date",
-            "last_date",
-            "days_since_last",
-            "longest_streak",
-            "longest_gap",
-        ]
-
-        self.dtype_dict = {
-            "element_category": "string",
-            "element_name": "string",
-            "user_id": "string",
-            "habit_group": "string",
-            "habit_action": "string",
-            "habit_detail": "string",
-            "total": "Int64",
-            "first_date": "datetime64[ns]",
-            "last_date": "datetime64[ns]",
-            "days_since_last": "Int64",
-            "longest_streak": "Int64",
-            "longest_gap": "Int64",
-        }
-
-        self.columns_to_sort = [
-            "element_category",
-            "element_name",
-            "user_id",
-            "habit_group",
-            "habit_action",
-            "habit_detail",
-        ]
-
-    def _get_habit_detail_col(
-        self,
-        dataframe: PandasDF,
-    ) -> Optional[str]:
-        return self.habit_detail_dict.get(dataframe["element_name"].unique()[0])
 
     def _get_value_columns(self, dataframe: PandasDF) -> List[str]:
         return [col for col in dataframe.columns if col not in self.default_columns and dataframe[col].dtype == "bool"]
 
-    def _set_habit_group(self, dataframe: PandasDF) -> PandasDF:
+    def _set_habit_group(self, dataframe: PandasDF, group_dict: Dict[str, str]) -> PandasDF:
         element_name = dataframe["element_name"].unique()[0]
-
-        habit_group_dict = {
-            "navigator": "book",
-            "alchemist": "emotion",
-            "sponsor": "duty",
-            "patron": "study",
-            "athlete": "running",
-            "diplomat": "relate",
-            "citizen": "group",
-            "oracle": "overhaul",
-        }
 
         if element_name == "atharva_bindu":
             reset_actions = [action for action in list(dataframe["habit_action"].unique()) if action.endswith("Reset")]
             dataframe["habit_group"] = dataframe["habit_action"].apply(lambda action: "reset" if action in reset_actions else "outlook")
         else:
-            dataframe["habit_group"] = habit_group_dict.get(element_name)
+            dataframe["habit_group"] = group_dict.get(element_name)
 
         return dataframe
 
@@ -133,12 +124,13 @@ class SummaryDataFrameTransformer(Transformer):
         return dataframe[column_sequence].astype(dtypes).sort_values(by=sort_keys)
 
     def apply(self, dataframe: PandasDF) -> PandasDF:
-        detail_column = self._get_habit_detail_col(dataframe)
+        detail_column = "book" if dataframe["element_name"].unique()[0] == "navigator" else None
         value_columns = self._get_value_columns(dataframe)
+
         try:
             return (
                 dataframe.pipe(self.melter.apply, detail=detail_column, value_vars=value_columns)
-                .pipe(self._set_habit_group)
+                .pipe(self._set_habit_group, group_dict=self.habit_group_dict)
                 .pipe(self._calculate_summary_fields)
                 .pipe(self._add_fields)
                 .pipe(
