167fc10542f7ab62b0d87c7a988d34552c289663
matheus <matheuschomem@hotmail.com>
2024-11-08 07:25:44 -0300

    feat: Implement refinement type handling in RefinementEngine and add union_dataframes method

--- src/etl/core/refinement/engine.py
+++ src/etl/core/refinement/engine.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from src.etl.core.definitions import Engine, EngineType, PandasDF
+from src.etl.core.definitions import Engine, EngineType, PandasDF, RefinementType
 from src.etl.core.refinement.monthly.transformer import MonthlyDataFrameTransformer
 from src.etl.core.refinement.summary.transformer import SummaryDataFrameTransformer
 from src.etl.core.refinement.weekly.transformer import WeeklyDataFrameTransformer
@@ -18,10 +18,22 @@ class RefinementEngine(Engine):
         self.summary_transformer = summary_transformer
         self.monthly_transformer = monthly_transformer
         self.weekly_transformer = weekly_transformer
+        self.refinement_type = None
 
-    def process(self, table: PandasDF) -> List[PandasDF]:
-        self.logger.info("Starting data refinement process")
-        df_summary = self.summary_transformer.apply(dataframe=table)
-        df_monthly = self.monthly_transformer.apply(dataframe=table)
-        df_weekly = self.weekly_transformer.apply(dataframe=table)
-        return [df for df in [df_summary, df_monthly, df_weekly] if df is not None]
+    def set_refinement_type(self, type: RefinementType) -> None:
+        self.logger.info(f"Setting refinement type to {type.value.upper()}")
+        self.refinement_type = type
+
+    def union_dataframes(self, dataframes: List[PandasDF]) -> PandasDF:
+        return self._pd.concat(dataframes, ignore_index=True).reset_index(drop=True)
+
+    def process(self, table: PandasDF) -> PandasDF:
+
+        if self.refinement_type is None:
+            raise ValueError("Refinement type not set")
+        if self.refinement_type == RefinementType.SUMMARY:
+            return self.summary_transformer.apply(dataframe=table)
+        if self.refinement_type == RefinementType.MONTHLY:
+            return self.monthly_transformer.apply(dataframe=table)
+        if self.refinement_type == RefinementType.WEEKLY:
+            return self.weekly_transformer.apply(dataframe=table)
