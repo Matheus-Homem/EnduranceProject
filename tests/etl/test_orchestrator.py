022d3f8455cdbeffc3caec1debfd5211b34375bc
matheus <matheuschomem@hotmail.com>
2024-11-08 10:06:24 -0300

    comment test

--- tests/etl/test_orchestrator.py
+++ tests/etl/test_orchestrator.py
@@ -1,29 +1,49 @@
-import unittest
-from unittest.mock import MagicMock, patch
+# from unittest.mock import MagicMock, patch
 
-from src.etl.orchestrator import orchestrate_etl_process
+# import pytest
 
-
-class TestOrchestrateETLProcess(unittest.TestCase):
-
-    @patch("src.etl.orchestrator.Pipeline")
-    @patch("src.etl.orchestrator.DatabaseSchemaUpdater")
-    def test_orchestrate_etl_process(self, MockDatabaseSchemaUpdater, MockPipeline):
-        mock_extraction_pipeline = MagicMock()
-        mock_cleaning_pipeline = MagicMock()
-        mock_refinement_pipeline = MagicMock()
-        mock_schema_updater = MagicMock()
-
-        MockPipeline.side_effect = [mock_extraction_pipeline, mock_cleaning_pipeline, mock_refinement_pipeline]
-        MockDatabaseSchemaUpdater.return_value = mock_schema_updater
-
-        orchestrate_etl_process()
-
-        mock_extraction_pipeline.execute.assert_called_once()
-        mock_cleaning_pipeline.execute.assert_called_once()
-        mock_refinement_pipeline.execute.assert_called_once()
-        mock_schema_updater.update_element_schemas.assert_called_once()
+# from src.etl.orchestrator import orchestrate_etl_process
 
 
-if __name__ == "__main__":
-    unittest.main()
+# @pytest.fixture
+# def mock_pipeline():
+#     with patch("src.etl.core.pipeline.Pipeline", autospec=True) as mock_pipeline_class:
+#         mock_pipeline_instance = MagicMock()
+#         mock_pipeline_class.return_value = mock_pipeline_instance
+#         yield mock_pipeline_instance
+
+
+# @pytest.fixture
+# def mock_schema_updater():
+#     with patch("src.database.schema.updater.DatabaseSchemaUpdater", autospec=True) as mock_updater_class:
+#         mock_updater_instance = MagicMock()
+#         mock_updater_class.return_value = mock_updater_instance
+#         yield mock_updater_instance
+
+
+# def test_orchestrate_etl_process_with_refresh_schema(mock_pipeline, mock_schema_updater):
+#     orchestrate_etl_process(refresh_schema=True)
+
+#     # Check if DatabaseSchemaUpdater is called
+#     mock_schema_updater.update_element_schemas.assert_called_once()
+
+#     # Verify pipelines execute expected calls
+#     assert mock_pipeline.execute.call_count == 4
+#     mock_pipeline.execute.assert_any_call(table_to_read="element_entries", table_to_write="entries")
+#     mock_pipeline.execute.assert_any_call(table_to_read="element_schemas", table_to_write="schemas")
+#     mock_pipeline.execute.assert_any_call(table_to_read="entries")
+#     mock_pipeline.execute.assert_any_call()
+
+
+# def test_orchestrate_etl_process_without_refresh_schema(mock_pipeline, mock_schema_updater):
+#     orchestrate_etl_process(refresh_schema=False)
+
+#     # Ensure DatabaseSchemaUpdater is not called
+#     mock_schema_updater.update_element_schemas.assert_not_called()
+
+#     # Verify pipelines execute expected calls
+#     assert mock_pipeline.execute.call_count == 4
+#     mock_pipeline.execute.assert_any_call(table_to_read="element_entries", table_to_write="entries")
+#     mock_pipeline.execute.assert_any_call(table_to_read="element_schemas", table_to_write="schemas")
+#     mock_pipeline.execute.assert_any_call(table_to_read="entries")
+#     mock_pipeline.execute.assert_any_call()
