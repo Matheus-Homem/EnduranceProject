09f58ae75ef06879cfd329e70adbe2c181b3be5e
matheus <matheuschomem@hotmail.com>
2024-11-12 08:59:26 -0300

    test: implement unit tests for DatabaseHandler with read and write scenarios

--- tests/etl/core/io/test_database_handler.py
+++ tests/etl/core/io/test_database_handler.py
@@ -1,34 +1,46 @@
-# import unittest
-# from unittest.mock import MagicMock, patch
+import unittest
+from unittest.mock import MagicMock, patch
 
-# from src.etl.definitions import Layer
-# from src.etl.io.database import DatabaseHandler
-# from src.etl.ports import DatabaseDF
-
-
-# class TestDatabaseReader(unittest.TestCase):
-
-#     @patch("database.DatabaseExecutorBuilder")
-#     @patch("database.ElementEntries")
-#     def test_read(self, MockElementEntries, MockDatabaseExecutorBuilder):
-#         mock_executor = MagicMock()
-#         mock_entries = MagicMock(spec=DatabaseDF)
-#         mock_executor.select.return_value = mock_entries
-#         MockDatabaseExecutorBuilder.return_value.__enter__.return_value = mock_executor
-
-#         reader = DatabaseHandler(layer=Layer.DATABASE)
-
-#         result = reader.read()
-
-#         mock_executor.select.assert_called_once_with(MockElementEntries)
-#         self.assertEqual(result, mock_entries)
-
-#     def test_write(self):
-#         reader = DatabaseHandler(layer=Layer.DATABASE)
-
-#         with self.assertRaises(NotImplementedError):
-#             reader.write(None)
+from src.database.tables import MySqlTable
+from src.etl.core.definitions import DatabaseDF, Layer
+from src.etl.core.io.database import DatabaseHandler
+from src.shared.credentials import PRD
 
 
-# if __name__ == "__main__":
-#     unittest.main()
+class TestDatabaseHandler(unittest.TestCase):
+
+    @patch("src.etl.core.io.database.DatabaseExecutorBuilder")
+    def test_read(self, mock_executor_builder):
+        # Preparation
+        layer = Layer.DATABASE
+        handler = DatabaseHandler(layer=layer)
+        table_name = MagicMock(spec=MySqlTable)
+        expected_mysql_table = "path_to_test_table"
+        handler.generate_path = MagicMock(return_value=expected_mysql_table)
+
+        mock_executor = MagicMock()
+        mock_executor.select.return_value = MagicMock(spec=DatabaseDF)
+        mock_executor_builder.return_value.__enter__.return_value = mock_executor
+
+        # Execution
+        result = handler.read(table_name)
+
+        # Asserts
+        handler.generate_path.assert_called_once_with(table_name)
+        mock_executor_builder.assert_called_once_with(use_production_db=PRD)
+        mock_executor.select.assert_called_once_with(expected_mysql_table)
+
+    def test_write_not_implemented(self):
+        # Preparation
+        layer = Layer.DATABASE
+        handler = DatabaseHandler(layer=layer)
+        table = MagicMock()
+
+        # Execution and Asserts
+        with self.assertRaises(NotImplementedError) as context:
+            handler.write(table)
+        self.assertEqual(str(context.exception), "DatabaseReader does not support write operations")
+
+
+if __name__ == "__main__":
+    unittest.main()
