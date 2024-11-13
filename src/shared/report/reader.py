8334f3d148ed8553b2e1cb4c334fc72fa3ff35ea
matheus <matheuschomem@hotmail.com>
2024-11-13 19:38:43 -0300

    feat: update GoldReader summary method to filter by element and handle NaN values

--- src/shared/report/reader.py
+++ src/shared/report/reader.py
@@ -1,4 +1,5 @@
 from typing import Dict, List
+from math import isnan
 
 from src.etl.core.definitions import Format, Layer
 from src.etl.core.io.manager import IOManager
@@ -7,12 +8,11 @@ from src.etl.core.io.manager import IOManager
 class GoldReader:
 
     @staticmethod
-    def summary(habit_action: str = None) -> List[Dict[str, str]]:
+    def summary(element: str) -> List[Dict[str, str]]:
         gold_handler = IOManager(layer=Layer.GOLD, format=Format.DELTA).get_handler()
-        df_sorted = gold_handler.read("summary").sort_values(by="last_date", ascending=False)
-        if habit_action:
-            df_sorted = df_sorted[df_sorted["habit_action"] == habit_action]
-        return convert_all_float_to_int(df_sorted.to_dict(orient="records"))
+        df_sorted = gold_handler.read("summary").sort_values(by="last_date", ascending=False)    
+        df_filtered = df_sorted[df_sorted["element_name"] == element]
+        return convert_all_float_to_int(df_filtered.to_dict(orient="records"))
 
     @staticmethod
     def monthly() -> List[Dict[str, str]]:
@@ -27,5 +27,5 @@ def convert_all_float_to_int(df_dict: List[Dict[str, str]]) -> List[Dict[str, st
     for record in df_dict:
         for key, value in record.items():
             if isinstance(value, float):
-                record[key] = int(value)
+                record[key] = int(value) if not isnan(value) else None
     return df_dict
