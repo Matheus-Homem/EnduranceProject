818e2dc26ef1172f21446cc325ca260143791a23
matheus <matheuschomem@hotmail.com>
2024-11-21 19:23:29 -0300

    feat: add partitioning support to ParquetHandler write method

--- src/etl/core/io/parquet.py
+++ src/etl/core/io/parquet.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from pandas import read_parquet
 
 from src.etl.core.definitions import IOHandler, Layer, PandasDF, TableName
@@ -13,7 +15,7 @@ class ParquetHandler(IOHandler):
         self.logger.info(f"Reading Parquet file from {repr(path)}")
         return read_parquet(path)
 
-    def write(self, dataframe: PandasDF, table_name: TableName) -> None:
+    def write(self, dataframe: PandasDF, table_name: TableName, partition_cols: List[str] = None) -> None:
         path = self.generate_path(table_name=table_name)
-        self.logger.info(f"Writing Parquet file to {repr(path)}")
-        dataframe.to_parquet(path, index=False)
+        self.logger.info(f"Writing Parquet file to {repr(path)} with partitioning by {partition_cols}")
+        dataframe.to_parquet(path, index=False, partition_cols=partition_cols)
