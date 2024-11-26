51ff63c2aa0dd2740a262e627463a670acff994d
matheus <matheuschomem@hotmail.com>
2024-11-26 09:20:27 -0300

    feat: implement dark mode toggle and persist setting in local storage

--- src/web/static/js/index.js
+++ src/web/static/js/index.js
@@ -14,10 +14,11 @@ closeBtn.addEventListener('click', () => {
 
 darkMode.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode-variables');
+    const isDarkMode = document.body.classList.contains('dark-mode-variables');
     darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
     darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
-})
-
+    localStorage.setItem('darkMode', isDarkMode ? 'enabled' : 'disabled');
+});
 
 Orders.forEach(order => {
     const tr = document.createElement('tr');
